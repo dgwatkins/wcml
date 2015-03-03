8796eae93c7c22ae9a3efa7f27a13dd8e22e03ba
Sergey <sergey.r@icanlocalize.com>
2015-03-03 15:04:55 +0200

    changed tag

--- wpml-woocommerce.php
+++ wpml-woocommerce.php
@@ -5,7 +5,7 @@
   Description: Allows running fully multilingual e-Commerce sites with WooCommerce and WPML. <a href="http://wpml.org/documentation/related-projects/woocommerce-multilingual/">Documentation</a>.
   Author: OnTheGoSystems
   Author URI: http://www.onthegosystems.com/
-  Version: 3.5.2-beta2
+  Version: 3.5.2
 */
 
 
