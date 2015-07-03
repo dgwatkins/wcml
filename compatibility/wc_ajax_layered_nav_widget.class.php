a06b3216e3d5c430decac90ddf9f5e6b8efe94c6
Konrad Karpieszuk <konrad.k@icanlocalize.com>
2015-07-03 12:54:17 +0200

    My changes to Ahmed's changes

--- compatibility/wc_ajax_layered_nav_widget.class.php
+++ compatibility/wc_ajax_layered_nav_widget.class.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- Class for WooCommerce Advanced Ajax Layered Navigation 
+ Class for WooCommerce Advanced Ajax Layered Navigation
  */
 
 class WCML_Ajax_Layered_Nav_Widget {
 	function __construct() {
 		add_filter('wc_ajax_layered_nav_sizeselector_term_id', array($this, 'wc_ajax_layered_nav_sizeselector_term_id'));
-		add_filter('wc_ajax_layered_nav_query_editor', array($this, 'wc_ajax_layered_nav_query_editor_fun'),10,2);
+		add_filter('wc_ajax_layered_nav_query_editor', array($this, 'wc_ajax_layered_nav_query_editor'),10,3);
 	}
 
 	function wc_ajax_layered_nav_sizeselector_term_id($term_id) {
@@ -16,7 +16,7 @@ class WCML_Ajax_Layered_Nav_Widget {
 		return $term_id;
 	}
 
-function wc_ajax_layered_nav_query_editor_fun($attribute,$value){
+function wc_ajax_layered_nav_query_editor($posts, $attribute, $value){
 	$posts = get_posts(
 		array(
 			'post_type' 	=> 'product',
@@ -36,4 +36,3 @@ function wc_ajax_layered_nav_query_editor_fun($attribute,$value){
 	return $posts;
 	}
 }
-
