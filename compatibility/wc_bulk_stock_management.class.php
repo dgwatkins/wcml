a080ba405e07b4cf22e5311d80bad73becbcb2fc
Konrad Karpieszuk <konrad.k@icanlocalize.com>
2015-09-09 10:57:04 +0200

    extension bulk stock management: Synchronise quantities between languages

--- compatibility/wc_bulk_stock_management.class.php
+++ compatibility/wc_bulk_stock_management.class.php
@@ -11,5 +11,24 @@ class WCML_Bulk_Stock_Management {
 			global $sitepress;
 			remove_action('admin_enqueue_scripts', array($sitepress, 'language_filter'));
 		}
+		
+		add_action( 'wc_bulk_stock_after_process_qty', array($this, 'wc_bulk_stock_after_process_qty_action'), 10, 1 );
+	}
+	
+	function wc_bulk_stock_after_process_qty_action($id) {
+		global $sitepress;
+		
+		$new_quantity = get_post_meta($id, '_stock', true);
+		
+		$trid = $sitepress->get_element_trid( $id, 'post_product' );
+		$translations = $sitepress->get_element_translations( $trid, 'post_product' );
+		
+		foreach ($translations as $translation) {
+			if ($translation->element_id == $id) {
+				continue;
+			}
+			update_post_meta($translation->element_id, '_stock', $new_quantity);
+		}
+		
 	}
 }
