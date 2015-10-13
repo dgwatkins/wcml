063f803eeebce9c4f84e1ecc7205940069681c14
sergey.r <sergey.r@icanlocalize.com>
2015-10-14 01:12:04 +0300

    sync gf data wcml-348 Translating cart data for Gravity Forms Product Addons

--- compatibility/gravityforms.class.php
+++ compatibility/gravityforms.class.php
@@ -7,6 +7,8 @@ class WCML_gravityforms{
         add_filter('wcml_multi_currency_is_ajax',array($this,'add_ajax_action'));
 
         add_filter('wcml_exception_duplicate_products_in_cart', array($this, 'addons_duplicate_exception'),10,2);
+
+        add_action( 'wcml_after_duplicate_product_post_meta', array( $this, 'sync_gf_data'), 10, 3 );
     }
     
     function wcml_convert_price($formatted, $unformatted){
@@ -28,6 +30,27 @@ class WCML_gravityforms{
             $exclude = true;
         }
         return $exclude;
+    }
+
+    function sync_gf_data($original_product_id, $trnsl_product_id, $data){
+        $orig_gf = maybe_unserialize( get_post_meta( $original_product_id, '_gravity_form_data' , true ) );
+        $trnsl_gf = maybe_unserialize( get_post_meta( $trnsl_product_id, '_gravity_form_data' , true ) );
+
+        if( !$trnsl_gf ){
+            update_post_meta( $trnsl_product_id, '_gravity_form_data', $orig_gf );
+        }else{
+            $trnsl_gf['id'] = $orig_gf['id'];
+            $trnsl_gf['display_title'] = $orig_gf['display_title'];
+            $trnsl_gf['display_description'] = $orig_gf['display_description'];
+            $trnsl_gf['disable_woocommerce_price'] = $orig_gf['disable_woocommerce_price'];
+            $trnsl_gf['disable_calculations'] = $orig_gf['disable_calculations'];
+            $trnsl_gf['disable_label_subtotal'] = $orig_gf['disable_label_subtotal'];
+            $trnsl_gf['disable_label_options'] = $orig_gf['disable_label_options'];
+            $trnsl_gf['disable_label_total'] = $orig_gf['disable_label_total'];
+            $trnsl_gf['disable_anchor'] = $orig_gf['disable_anchor'];
+
+            update_post_meta( $trnsl_product_id, '_gravity_form_data', $trnsl_gf );
+        }
     }
    
 }
