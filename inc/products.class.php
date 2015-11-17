2a84e6f6393aa37c73597f066087581c6306d057
sergey.r <sergey.r@icanlocalize.com>
2015-11-17 14:29:47 +0200

    small fix

--- inc/products.class.php
+++ inc/products.class.php
@@ -790,7 +790,7 @@ class WCML_Products{
         }
 
         foreach ($all_meta as $key => $meta) {
-            if( !isset($settings['translation-management']['custom_fields_translation'][$key]) || ( isset($settings['translation-management']['custom_fields_translation'][$key]) && $settings['translation-management']['custom_fields_translation'][$key] == 0 ) ){
+            if( !isset($settings['translation-management']['custom_fields_translation'][$key]) || $settings['translation-management']['custom_fields_translation'][$key] == 0 ){
                 continue;
             }
             foreach ($meta as $meta_value) {
