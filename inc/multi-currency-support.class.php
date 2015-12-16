ae4dea325ae3e808baa7e8346ef7b57a3a9c202c
Mihai G <mihaigrigori@gmail.com>
2015-12-16 12:24:52 +0200

    Added a filter to allow loading the multi currency logic arbitrarily

--- inc/multi-currency-support.class.php
+++ inc/multi-currency-support.class.php
@@ -66,7 +66,7 @@ class WCML_Multi_Currency_Support{
             }
         }
         
-        return $load;
+        return apply_filters('wcml_load_multi_currency', $load);
     }
     
     function init(){
