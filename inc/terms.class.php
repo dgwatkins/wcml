235f50329d80670ed6a7d556acd7afa35c775a13
sergey.r <sergey.r@icanlocalize.com>
2015-12-21 18:32:23 +0200

    wcml-810 Attribute lowercase in attribute tab, but uppercase in variations

--- inc/terms.class.php
+++ inc/terms.class.php
@@ -880,7 +880,15 @@ class WCML_Terms{
 
             $trnsl_term_id = apply_filters( 'translate_object_id', $term_obj->term_id, $taxonomy, true, $language );
 
-            $filtered_terms[] = !$is_objects_array ? ( isset( $is_slug ) ? get_term( $trnsl_term_id, $taxonomy )->slug : strtolower( get_term( $trnsl_term_id, $taxonomy )->name ) ) : get_term( $trnsl_term_id, $taxonomy );
+            if( $is_objects_array ){
+                $filtered_terms[] = get_term( $trnsl_term_id, $taxonomy );
+            }else{
+                if( isset( $is_slug ) ){
+                    $filtered_terms[] = get_term( $trnsl_term_id, $taxonomy )->slug;
+                }else{
+                    $filtered_terms[] = ( is_ajax() && isset( $_POST['action'] ) && in_array( $_POST['action'], array( 'woocommerce_add_variation', 'woocommerce_link_all_variations') ) ) ? strtolower ( get_term( $trnsl_term_id, $taxonomy )->name ) : get_term( $trnsl_term_id, $taxonomy )->name;
+                }
+            }
         }
 
         return $filtered_terms;
