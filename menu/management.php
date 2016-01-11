826b27cd67ba48b6b0971fea3e670ff914af3931
Kasia Janoska <kasia.j@icanlocalize.com>
2016-01-11 14:10:25 +0100

    wcml-847 - Styling for wpml-dismissible messages

--- menu/management.php
+++ menu/management.php
@@ -74,14 +74,11 @@ if(isset($_GET['tab'])){
             <?php include WCML_PLUGIN_PATH . '/menu/sub/settings.php'; ?>
         <?php endif; ?>
     </div>
-
-</div>
-
-<?php if( !isset( $woocommerce_wpml->settings['rate-block'] ) ): ?>
-    <div class="wrap wcml-wrap wpml-is-dismissible">
-
-            <?php echo sprintf(__('Thank you for using %s! You can express your love and support by %s rating our plugin and saying that %s for you.', 'woocommerce-multilingual'),'<strong>WooCommerce Multilingual</strong>', '<a href="https://wordpress.org/support/view/plugin-reviews/woocommerce-multilingual?filter=5#postform" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733;</a>', '<a href="https://wordpress.org/plugins/woocommerce-multilingual/?compatibility[version]='.$woocommerce_wpml->get_supported_wp_version().'&compatibility[topic_version]='.WCML_VERSION.'&compatibility[compatible]=1#compatibility" target="_blank">'.__('it works','woocommerce-multilingual').'</a>')?>
-
+    <?php if( !isset( $woocommerce_wpml->settings['rate-block'] ) ): ?>
+        <div class="notice wpml-is-dismissible">
+            <p>
+                <?php echo sprintf(__('Thank you for using %s! You can express your love and support by %s rating our plugin and saying that %s for you.', 'woocommerce-multilingual'),'<strong>WooCommerce Multilingual</strong>', '<a href="https://wordpress.org/support/view/plugin-reviews/woocommerce-multilingual?filter=5#postform" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733;</a>', '<a href="https://wordpress.org/plugins/woocommerce-multilingual/?compatibility[version]='.$woocommerce_wpml->get_supported_wp_version().'&compatibility[topic_version]='.WCML_VERSION.'&compatibility[compatible]=1#compatibility" target="_blank">'.__('it works','woocommerce-multilingual').'</a>')?>
+            </p>
             <button class="notice-dismiss hide-rate-block" data-setting="rate-block">
                 <span class="screen-reader-text">
                     <?php _e('Hide','woocommerce-multilingual') ?>
@@ -89,5 +86,8 @@ if(isset($_GET['tab'])){
             </button>
             <?php wp_nonce_field('wcml_settings', 'wcml_settings_nonce'); ?>
 
-    </div>
-<?php endif; ?>
+        </div>
+    <?php endif; ?>
+</div>
+
+
