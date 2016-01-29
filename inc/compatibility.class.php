4334efd2f09f961c813406a0efa563ba3c479709
Mihai G <mihaigrigori@gmail.com>
2016-01-29 10:31:04 +0200

    Fixed an issue prevent the correct plugin activation in some cases + version number + changelog update

--- inc/compatibility.class.php
+++ inc/compatibility.class.php
@@ -110,7 +110,7 @@ class WCML_Compatibility {
             $this->flatsome = new WCML_Flatsome();
         }
         
-        // Plugin Name: WooCommerce - Gravity Forms Product Add-Ons
+        // WooCommerce - Gravity Forms Product Add-Ons
         if (class_exists('woocommerce_gravityforms')) {
             $this->gravityforms_product_addons = new WCML_Gravityforms_Product_Addons();
         }
