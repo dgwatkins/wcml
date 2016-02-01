f7ed3d8b3085dd6e475087990b7e72c5f4ca2af9
Mihai G <mihaigrigori@gmail.com>
2016-02-01 14:38:27 +0200

    Added needed perameter for Installer to show the 'commercial' tab

--- inc/installer-loader.php
+++ inc/installer-loader.php
@@ -3,7 +3,9 @@
 if( file_exists( WCML_PLUGIN_PATH . '/inc/installer/loader.php' ) ){
 
     include WCML_PLUGIN_PATH . '/inc/installer/loader.php' ;
-    $args = array();
+    $args = array(
+        'plugins_install_tab' => 1
+    );
     WP_Installer_Setup( $wp_installer_instance, $args );
 
 }
