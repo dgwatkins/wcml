b5e74e4882e57d3cfb2f6ccdd8153ef700bb377a
sergey.r <sergey.r@icanlocalize.com>
2016-03-16 15:16:53 +0200

    wcml-1004 custom prices not saved for new product ( added product type checking )

--- inc/currencies/class-wcml-custom-prices.php
+++ inc/currencies/class-wcml-custom-prices.php
@@ -352,7 +352,7 @@ class WCML_Custom_Prices{
     public function save_custom_prices( $post_id ){
         $nonce = filter_input( INPUT_POST, '_wcml_custom_prices_nonce', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
 
-        if( isset( $_POST[ '_wcml_custom_prices' ] ) && isset( $nonce ) && wp_verify_nonce( $nonce, 'wcml_save_custom_prices' ) ){
+        if( isset( $_POST[ '_wcml_custom_prices' ] ) && isset( $nonce ) && wp_verify_nonce( $nonce, 'wcml_save_custom_prices' ) && !$this->woocommerce_wpml->products->is_variable_product( $post_id ) ){
             if( isset( $_POST[ '_wcml_custom_prices' ][ $post_id ] ) || isset( $_POST[ '_wcml_custom_prices' ][ 'new' ] ) ) {
                 $wcml_custom_prices_option = isset( $_POST[ '_wcml_custom_prices' ][ $post_id ] ) ? $_POST[ '_wcml_custom_prices' ][ $post_id ] : isset( $_POST[ '_wcml_custom_prices' ][ 'new' ] );
             }else{
