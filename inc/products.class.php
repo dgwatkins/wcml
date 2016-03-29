5bbef633352fc528f02e6f94dd955fa7ebad0024
Mihai G <mihaigrigori@gmail.com>
2016-03-29 17:17:16 +0300

    The translation of a WooCommerce duplicate product was marked as published wcml-1024

--- inc/products.class.php
+++ inc/products.class.php
@@ -2981,7 +2981,14 @@ class WCML_Products{
 
                         $new_id_obj = get_post( $new_id );
                         $new_slug = wp_unique_post_slug( sanitize_title( $new_id_obj->post_title ), $new_id, $post_to_duplicate->post_status, $post_to_duplicate->post_type, $new_id_obj->post_parent );
-                        $wpdb->update( $wpdb->posts, array( 'post_name' => $new_slug, 'post_status' => $post_to_duplicate->post_status ), array( 'ID' => $new_id ) );
+                        $wpdb->update(
+                            $wpdb->posts,
+                            array(
+                                'post_name' => $new_slug,
+                                'post_status' => 'draft'
+                            ),
+                            array( 'ID' => $new_id )
+                        );
 
                         do_action( 'wcml_after_duplicate_product' , $new_id, $post_to_duplicate );
 
