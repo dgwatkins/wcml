bb3482d901927a6b5cb44f4676753707c002852a
sergey.r <sergey.r@icanlocalize.com>
2016-05-24 22:23:05 +0300

    fixed attr test

--- tests/tests/inc/test-attributes.php
+++ tests/tests/inc/test-attributes.php
@@ -14,18 +14,18 @@ class Test_WCML_Attributes extends WCML_UnitTestCase {
 		$es_term = $this->wcml_helper->add_attribute_term( 'blanco', $attr, 'es', $term['trid'] );
 
 		$_POST[ 'wcml-is-translatable-attr' ] = 1;
-		$attr = wc_attribute_taxonomy_name( $attr );
+		$attr_formated = wc_attribute_taxonomy_name( $attr );
 		$this->woocommerce_wpml->attributes->set_attribute_readonly_config( 1, array( 'attribute_name' => $attr ) );
-		$this->assertEquals( 1, $this->woocommerce_wpml->attributes->is_translatable_attribute( $attr ) );
-		$attr_terms = $this->woocommerce_wpml->attributes->get_attribute_terms($attr );
+		$this->assertEquals( 1, $this->woocommerce_wpml->attributes->is_translatable_attribute( $attr_formated ) );
+		$attr_terms = $this->woocommerce_wpml->attributes->get_attribute_terms($attr_formated );
 		$this->assertEquals( 2, count( $attr_terms ) );
 
 
 		unset( $_POST[ 'wcml-is-translatable-attr' ] );
 		$this->woocommerce_wpml->attributes->set_attribute_readonly_config( 1, array( 'attribute_name' => $attr ) );
-		$this->assertEquals( 0, $this->woocommerce_wpml->attributes->is_translatable_attribute( $attr ) );
+		$this->assertEquals( 0, $this->woocommerce_wpml->attributes->is_translatable_attribute( $attr_formated ) );
 
-		$attr_terms = $this->woocommerce_wpml->attributes->get_attribute_terms( $attr );
+		$attr_terms = $this->woocommerce_wpml->attributes->get_attribute_terms( $attr_formated );
 		$this->assertEquals( 1, count( $attr_terms ) );
 	}
 
