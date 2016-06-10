67c160f661901352f27de319bd2c182849d8b1a6
Eduard Maghakyan <eduard.m@icanlocalize.com>
2016-06-10 16:10:41 +0400

    Use filter in sitepress to hide column

--- inc/class-wcml-products-screen-options.php
+++ inc/class-wcml-products-screen-options.php
@@ -25,8 +25,25 @@ class WCML_Products_Screen_Options extends WPML_Templates_Factory {
 	 */
 	public function init() {
 		add_filter( 'default_hidden_columns', array( $this, 'filter_screen_options' ), 10, 2 );
+		add_filter( 'wpml_hide_management_column', array( $this, 'sitepress_screen_option_filter' ), 10, 2 );
 		add_action( 'admin_init', array( $this, 'save_translation_controls' ), 10, 1 );
 		add_action( 'admin_notices', array( $this, 'product_page_admin_notices' ), 10 );
+	}
+
+	/**
+	 * Hide management column by defaul for products.
+	 *
+	 * @param $is_visible
+	 * @param $post_type
+	 *
+	 * @return bool
+	 */
+	public function sitepress_screen_option_filter( $is_visible, $post_type ) {
+		if ( 'product' === $post_type ) {
+			$is_visible = false;
+		}
+
+		return $is_visible;
 	}
 
 	/**
