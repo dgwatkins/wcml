f5d4d1dd76243bcd867c057819d05c9ca1d485af
Eduard Maghakyan <eduard.m@icanlocalize.com>
2016-07-06 17:29:24 +0400

    Code cleanup

---
+++ tests/tests/compatibility/test-wcml-tab-manager.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Class Test_WCML_Tab_Manager
+ */
+class Test_WCML_Tab_Manager extends WCML_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+	}
+
+	/**
+	 * @test
+	 */
+	public function make__product_tabs_not_translatable_by_default() {
+
+	}
+}
