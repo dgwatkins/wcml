9759bb46d6cd4be1304c1a466b8a487603461259
Mihai Grigori <mihai@wpml.org>
2016-08-01 17:41:25 +0300

    wcml-1381 Free shipping minimum order amount level not converted to another currencies
    Unit tests
    
    cleanup
    
    Fix

--- inc/currencies/class-wcml-multi-currency-shipping.php
+++ inc/currencies/class-wcml-multi-currency-shipping.php
@@ -9,16 +9,17 @@ class WCML_Multi_Currency_Shipping{
 
         $this->multi_currency =& $multi_currency;
 
-        $rates = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}woocommerce_shipping_zone_methods WHERE method_id IN('flat_rate', 'local_pickup')" );
+        $rates = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}woocommerce_shipping_zone_methods WHERE method_id IN('flat_rate', 'local_pickup', 'free_shipping')" );
         foreach( $rates as $method ){
             $option_name = sprintf('woocommerce_%s_%d_settings', $method->method_id, $method->instance_id );
             add_filter('option_' . $option_name, array($this, 'convert_shipping_cost'));
         }
 
-        add_filter( 'wcml_shipping_price_amount', array($this, 'shipping_price_filter') ); // WCML filters
-        add_filter( 'wcml_shipping_free_min_amount', array($this, 'shipping_free_min_amount') ); // WCML filters
+        add_filter( 'wcml_shipping_price_amount', array( $this, 'shipping_price_filter' ) ); // WCML filters
+        add_filter( 'wcml_shipping_free_min_amount', array( $this, 'shipping_free_min_amount') ); // WCML filters
 
-        add_filter('option_woocommerce_free_shipping_settings', array($this, 'adjust_min_amount_required'));
+        // Before WooCommerce 2.6
+        add_filter('option_woocommerce_free_shipping_settings', array( $this, 'adjust_min_amount_required' ) );
 
     }
 
@@ -26,6 +27,10 @@ class WCML_Multi_Currency_Shipping{
 
         if( isset($settings['cost']) ){
             $settings['cost'] = $this->multi_currency->prices->raw_price_filter($settings['cost'], $this->multi_currency->get_client_currency());
+        }
+
+        if( !empty( $settings['requires'] ) && $settings['requires'] == 'min_amount' ){
+            $settings['min_amount'] = apply_filters( 'wcml_shipping_free_min_amount', $settings['min_amount'] );
         }
 
         return $settings;
@@ -47,10 +52,11 @@ class WCML_Multi_Currency_Shipping{
 
     }
 
+    // Before WooCommerce 2.6
     public function adjust_min_amount_required($options){
 
-        if(!empty($options['min_amount'])){
-            $options['min_amount'] = apply_filters('wcml_shipping_free_min_amount', $options['min_amount']);
+        if( !empty( $options['min_amount'] ) ){
+            $options['min_amount'] = apply_filters( 'wcml_shipping_free_min_amount', $options['min_amount'] );
         }
 
         return $options;
