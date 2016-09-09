0a2eb8a3c588089f92d248a0dbf85fc6b15f335b
Mihai Grigori <mihai@wpml.org>
2016-09-09 12:39:15 +0300

    wcml-1458 List what we are going to do in the wizard, in the announcement message.
    Styling for UL in wizard notice

--- inc/admin-menus/class-wcml-setup.php
+++ inc/admin-menus/class-wcml-setup.php
@@ -83,10 +83,18 @@ class WCML_Setup {
         wp_enqueue_style( 'wcml-setup-wizard-notice', WCML_PLUGIN_URL . '/res/css/wcml-setup-wizard-notice.css' );
         ?>
         <div id="wcml-setup-wizard" class="updated message wpml-message">
-            <p><?php printf( __('%sWelcome to WooCommerce Multilingual!%s Please take a moment to configure the main settings and then you are ready to start translating your products.', 'woocommerce-multilingual'), '<strong>', '</strong><br>') ?></p>
+            <p>
+                <strong><?php _e('Prepare your WooCommerce store to run multilingual!', 'woocommerce-multilingual'); ?></strong><br />
+                <?php _e('We need to help you with a few steps to turn your WooCommerce store multilingual. These steps include:', 'woocommerce-multilingual'); ?>
+                <ul class="wcml-notice-list">
+                    <li><?php _e("Translating the 'store' pages", 'woocommerce-multilingual'); ?></li>
+                    <li><?php _e("Choosing which attributes to make translatable", 'woocommerce-multilingual'); ?></li>
+                    <li><?php _e("Choosing if you need multiple currencies", 'woocommerce-multilingual'); ?></li>
+                </ul>
+            </p>
             <p class="submit">
-                <a href="<?php echo esc_url( admin_url('admin.php?page=wcml-setup') ); ?>" class="button-primary"><?php _e('Run the Setup Wizard', 'woocommerce-multilingual') ?></a>
-                <a href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'wcml-setup-skip', 1 ), 'wcml_setup_skip_nonce', '_wcml_setup_nonce' ) ); ?>" class="button-secondary skip"><?php _e('Skip Setup', 'woocommerce-multilingual') ?></a>
+                <a href="<?php echo esc_url( admin_url('admin.php?page=wcml-setup') ); ?>" class="button-primary"><?php _e('Start the Setup Wizard', 'woocommerce-multilingual') ?></a>
+                <a href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'wcml-setup-skip', 1 ), 'wcml_setup_skip_nonce', '_wcml_setup_nonce' ) ); ?>" class="button-secondary skip"><?php _e('Skip', 'woocommerce-multilingual') ?></a>
             </p>
         </div>
         <?php
