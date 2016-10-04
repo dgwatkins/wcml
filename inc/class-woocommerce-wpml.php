0435825c120d3cb0246719aed47ec186148cd73c
Mihai Grigori <mihai@wpml.org>
2016-10-04 11:17:05 +0300

    wcml-1533 If you have one Currency in the preview section you are getting "0" (zero)

--- inc/class-woocommerce-wpml.php
+++ inc/class-woocommerce-wpml.php
@@ -124,7 +124,8 @@ class woocommerce_wpml {
                 'wcml_delete_currency',
                 'wcml_update_currency_lang',
                 'wcml_update_default_currency',
-                'wcml_price_preview'
+                'wcml_price_preview',
+	            'wcml_currencies_switcher_preview'
         );
         if($this->settings['enable_multi_currency'] == WCML_MULTI_CURRENCIES_INDEPENDENT
             || ( isset($_GET['page']) && $_GET['page'] == 'wpml-wcml' && isset($_GET['tab']) && $_GET['tab'] == 'multi-currency' )
