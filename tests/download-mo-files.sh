d2051c280c92c8160e2f113564d62e5e1b4b24d5
Mihai G <mihaigrigori@gmail.com>
2016-10-14 12:53:50 +0300

    Use correct language folder (a custom one is used for tests)

--- tests/download-mo-files.sh
+++ tests/download-mo-files.sh
@@ -7,23 +7,19 @@ WC_VERSION_FOR_MO_FILES='2.6.4'
 WCML_PLUGIN_PATH=${OTGS_CI_DEPENDENCY_PATH}
 
 WP_TESTS_DIR=${WCML_PLUGIN_PATH}/tests/wordpress-tests-lib/
-WP_CORE_DIR=${WCML_PLUGIN_PATH}/tests/wordpress/
-WP_CORE_LANG_DIR=${WCML_PLUGIN_PATH}/tests/wordpress/wp-content/languages/
-WP_CORE_LANG_PLUGINS_DIR=${WCML_PLUGIN_PATH}/tests/wordpress/wp-content/languages/plugins/
+WP_TESTS_LANG_PLUGINS_DIR=${WP_TESTS_DIR}/data/languages/plugins
+
 
 # Create languages directory
-
-mkdir -p ${WP_CORE_LANG_DIR}
-mkdir -p ${WP_CORE_LANG_PLUGINS_DIR}
-ls ${WP_TESTS_DIR}
+mkdir -p ${WP_TESTS_LANG_PLUGINS_DIR}
 
 #WC lang packs
-wget -P ${WP_CORE_LANG_PLUGINS_DIR} https://downloads.wordpress.org/translation/plugin/woocommerce/$WC_VERSION_FOR_MO_FILES/fr_FR.zip
-wget -P ${WP_CORE_LANG_PLUGINS_DIR} https://downloads.wordpress.org/translation/plugin/woocommerce/$WC_VERSION_FOR_MO_FILES/de_DE.zip
-wget -P ${WP_CORE_LANG_PLUGINS_DIR} https://downloads.wordpress.org/translation/plugin/woocommerce/$WC_VERSION_FOR_MO_FILES/ru_RU.zip
-wget -P ${WP_CORE_LANG_PLUGINS_DIR} https://downloads.wordpress.org/translation/plugin/woocommerce/$WC_VERSION_FOR_MO_FILES/es_ES.zip
+wget -P ${WP_TESTS_LANG_PLUGINS_DIR} https://downloads.wordpress.org/translation/plugin/woocommerce/$WC_VERSION_FOR_MO_FILES/fr_FR.zip
+wget -P ${WP_TESTS_LANG_PLUGINS_DIR} https://downloads.wordpress.org/translation/plugin/woocommerce/$WC_VERSION_FOR_MO_FILES/de_DE.zip
+wget -P ${WP_TESTS_LANG_PLUGINS_DIR} https://downloads.wordpress.org/translation/plugin/woocommerce/$WC_VERSION_FOR_MO_FILES/ru_RU.zip
+wget -P ${WP_TESTS_LANG_PLUGINS_DIR} https://downloads.wordpress.org/translation/plugin/woocommerce/$WC_VERSION_FOR_MO_FILES/es_ES.zip
 
-cd ${WP_CORE_LANG_PLUGINS_DIR}
+cd ${WP_TESTS_LANG_PLUGINS_DIR}
 unzip fr_FR.zip
 unzip de_DE.zip
 unzip ru_RU.zip
