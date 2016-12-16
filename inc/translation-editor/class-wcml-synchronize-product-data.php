2e6e1bfeb2da3d92ecd55d3e82b783f2fe40e970
sergey.r <sergey.r@icanlocalize.com>
2016-12-16 11:35:09 +0200

    Variation "Use custom settings for translations download files" and "Add separate download files for translations" does not work #wcml-1699
    fixed not displayed saved info in editor #wcml-1699

--- inc/translation-editor/class-wcml-synchronize-product-data.php
+++ inc/translation-editor/class-wcml-synchronize-product-data.php
@@ -96,8 +96,6 @@ class WCML_Synchronize_Product_Data{
             return;
         }
 
-        //save files option
-        $this->woocommerce_wpml->downloadable->save_files_option( $original_product_id );
         //update products order
         $this->woocommerce_wpml->products->update_order_for_product_translations( $original_product_id );
 
@@ -111,10 +109,12 @@ class WCML_Synchronize_Product_Data{
             }
         }
 
+        //save custom options for variations
+        $this->woocommerce_wpml->sync_variations_data->sync_product_variations_custom_data( $original_product_id );
+
         if( $this->woocommerce_wpml->settings[ 'enable_multi_currency' ] == WCML_MULTI_CURRENCIES_INDEPENDENT ) {
             //save custom prices
             $this->woocommerce_wpml->multi_currency->custom_prices->save_custom_prices( $original_product_id );
-            $this->woocommerce_wpml->multi_currency->custom_prices->sync_product_variations_custom_prices( $original_product_id );
         }
 
     }
@@ -142,7 +142,6 @@ class WCML_Synchronize_Product_Data{
         $this->sync_product_taxonomies( $original_product_id, $tr_product_id, $lang );
 
         //duplicate variations
-
         $this->woocommerce_wpml->sync_variations_data->sync_product_variations( $original_product_id, $tr_product_id, $lang );
 
         $this->sync_linked_products( $original_product_id, $tr_product_id, $lang );
