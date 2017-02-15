8b133d68f059e35bd74f290e0b7cf6caf6e6b73b
sergey.r <sergey.r@icanlocalize.com>
2017-02-15 09:58:51 +0200

    reverted code #wcml-1766

--- inc/currencies/currency-switcher/class-wcml-currency-switcher-widget.php
+++ inc/currencies/currency-switcher/class-wcml-currency-switcher-widget.php
@@ -7,11 +7,14 @@ class WCML_Currency_Switcher_Widget extends WP_Widget {
     }
 
     function widget($args, $instance) {
+        global $woocommerce_wpml;
+
+        $switcher_settings = $woocommerce_wpml->settings['currency_switchers'][ $args[ 'id' ] ];
 
         echo $args['before_widget'];
 
-        if( !empty( $instance['settings']['widget_title'] ) ){
-            $widget_title = apply_filters( 'widget_title', $instance['settings']['widget_title'] );
+        if( !empty( $switcher_settings['widget_title'] ) ){
+            $widget_title = apply_filters( 'widget_title', $switcher_settings['widget_title'] );
             echo $args['before_title']. $widget_title . $args['after_title'];
         }
 
@@ -25,7 +28,7 @@ class WCML_Currency_Switcher_Widget extends WP_Widget {
         if ( ! $new_instance && ! $old_instance ) {
             $new_instance = array(
                 'id' => $_POST['sidebar'],
-                'seetings' => WCML_Currency_Switcher::get_settings( $_POST['sidebar'] )
+                'settings' => WCML_Currency_Switcher::get_settings( $_POST['sidebar'] )
             );
         }
 
