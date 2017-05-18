4a3e49c2819459384db85a4e8f4eeeb21a6f7870
Mihai Grigori <mihai@wpml.org>
2017-05-18 17:26:27 +0300

    Dev version number

--- wpml-woocommerce.php
+++ wpml-woocommerce.php
@@ -6,14 +6,14 @@
   Author: OnTheGoSystems
   Author URI: http://www.onthegosystems.com/
   Text Domain: woocommerce-multilingual
-  Version: 4.1.3
+  Version: 4.2-dev
 */
 
 if ( defined( 'WCML_VERSION' ) ) {
 	return;
 }
 
-define( 'WCML_VERSION', '4.1.3' );
+define( 'WCML_VERSION', '4.2' );
 define( 'WCML_PLUGIN_PATH', dirname( __FILE__ ) );
 define( 'WCML_PLUGIN_FOLDER', basename( WCML_PLUGIN_PATH ) );
 define( 'WCML_LOCALE_PATH', WCML_PLUGIN_PATH . '/locale' );
