bc15f590b2ca6f15f8fd3fa8c1f97340a0f1e3e0
sergey.r <sergey.r@icanlocalize.com>
2017-06-15 11:51:25 +0300

    wcml-2006 WC Product Bundle wrong currency price if we use custom prices settings
    Small code tweaks  #wcml-2006
    
    small fix #wcml-2006
    
    small test fix #wcml-2006

--- tests/phpunit/tests/classes/compatibility/test-wcml-product-bundles.php
+++ tests/phpunit/tests/classes/compatibility/test-wcml-product-bundles.php
@@ -61,4 +61,63 @@ class Test_WCML_Product_Bundles extends OTGS_TestCase {
 
 	}
 
+	/**
+	* @test
+	*/
+	public function update_bundles_custom_prices_values() {
+
+		$prices = array( );
+		$code = rand_str();
+		$base_regular_price = rand( 1, 100 );
+		$base_sale_price = rand( 1, 100 );
+
+		$_POST[ '_custom_regular_price' ][ $code ] = $base_regular_price;
+		$_POST[ '_custom_sale_price' ][ $code ] = $base_sale_price;
+
+		$expected_prices_codes = array(
+			'_wc_pb_base_regular_price' => $base_regular_price,
+			'_wc_pb_base_sale_price' => $base_sale_price
+		);
+
+		\WP_Mock::wpFunction( 'wc_format_decimal', array(
+				'args' => array( $base_regular_price ),
+				'return' => $base_regular_price
+			)
+		);
+
+		\WP_Mock::wpFunction( 'wc_format_decimal', array(
+				'args' => array( $base_sale_price ),
+				'return' => $base_sale_price
+			)
+		);
+
+		$subject = $this->get_subject();
+		$filtered_prices = $subject->update_bundles_custom_prices_values( $prices, $code );
+
+		$this->assertEquals( $expected_prices_codes, $filtered_prices );
+
+	}
+
+	/**
+	* @test
+	*/
+	public function update_bundles_base_price() {
+
+
+		$post_id = rand( 1, 100 );
+		$product_price = rand( 1, 100 );
+		$custom_prices = array( '_wc_pb_base_regular_price' => rand( 1, 100 ) );
+		$code = rand_str();
+
+		\WP_Mock::wpFunction( 'update_post_meta', array(
+				'args' => array( $post_id, '_wc_pb_base_price_' . $code, $product_price ),
+				'times' => 1
+			)
+		);
+
+		$subject = $this->get_subject();
+		$filtered_prices = $subject->update_bundles_base_price( $post_id, $product_price, $custom_prices, $code );
+
+	}
+
 }
