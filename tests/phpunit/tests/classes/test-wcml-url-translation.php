f05af7db76f1cc4b62000f88d6a33a3132514d61
sergey.r <sergey.r@icanlocalize.com>
2017-08-03 11:16:34 +0300

    wcml-2090 Attribute slug not translated if the attribute base is not translated

--- tests/phpunit/tests/classes/test-wcml-url-translation.php
+++ tests/phpunit/tests/classes/test-wcml-url-translation.php
@@ -8,6 +8,21 @@ class Test_WCML_url_translation extends OTGS_TestCase {
 		parent::setUp();
 
 		\WP_Mock::wpPassthruFunction('_x');
+	}
+
+	public function get_woocommerce_multilingual(){
+
+		return $this->getMockBuilder('woocommerce_wpml')
+		     ->disableOriginalConstructor()
+		     ->getMock();
+
+	}
+
+	public function get_sitepress() {
+
+		return $this->getMockBuilder( 'SitePress' )
+		            ->disableOriginalConstructor()
+		            ->getMock();
 
 	}
 
@@ -16,11 +31,16 @@ class Test_WCML_url_translation extends OTGS_TestCase {
 	 */
 	public function use_untranslated_default_url_bases(){
 
-		$url_translation = new WCML_Url_Translation();
+		$url_translation = new WCML_Url_Translation( $this->get_woocommerce_multilingual(), $this->get_sitepress(), $this->stubs->wpdb() );
 
 		$url_translation->default_product_base = rand_str();
 		$url_translation->default_product_category_base = rand_str();
 		$url_translation->default_product_tag_base = rand_str();
+
+		\WP_Mock::wpFunction( 'get_option', array(
+			'values' => 'permalink_structure',
+			'return' => false
+		) );
 
 		// set all
 		$permalinks = [
@@ -41,6 +61,106 @@ class Test_WCML_url_translation extends OTGS_TestCase {
 		$filtered = $url_translation->use_untranslated_default_url_bases( $permalinks );
 		$this->assertNotEquals( $filtered['product_base'], $url_translation->default_product_base );
 		$this->assertEquals( $filtered['product_base'], $permalinks['product_base'] );
+
+	}
+
+	/**
+	 * @test
+	 */
+	public function translate_attributes_bases_in_rewrite_rules() {
+
+		$woocommerce_wpml = $this->get_woocommerce_multilingual();
+		$woocommerce_wpml->strings = $this->getMockBuilder( 'WCML_WC_Strings' )
+		                                  ->disableOriginalConstructor()
+		                                  ->setMethods( array( 'get_string_language' ) )
+		                                  ->getMock();
+
+		$woocommerce_wpml->strings->method( 'get_string_language' )->willReturn( rand_str() );
+
+		$sitepress = $this->getMockBuilder( 'SitePress' )
+		                  ->disableOriginalConstructor()
+		                  ->setMethods( array( 'get_current_language' ) )
+		                  ->getMock();
+
+		$sitepress->method( 'get_current_language' )->willReturn( rand_str() );
+
+		$subject = new WCML_Url_Translation( $woocommerce_wpml, $sitepress , $this->stubs->wpdb() );
+
+		$attribute_base_slug = rand_str();
+		$attribute_slug = rand_str();
+
+		$attribute_taxonomies = array();
+		$attr_object = new stdClass();
+		$attr_object->attribute_name = $attribute_slug;
+		$attribute_taxonomies[] = $attr_object;
+
+		\WP_Mock::wpFunction( 'wc_get_attribute_taxonomies', array(
+			'return' => $attribute_taxonomies
+		) );
+
+		$taxonomy_object = new stdClass();
+		$taxonomy_object->rewrite[ 'slug' ] = $attribute_base_slug.'/'.$attribute_slug;
+
+		\WP_Mock::wpFunction( 'get_taxonomy', array(
+			'values' => 'pa_'.$attribute_slug,
+			'return' => $taxonomy_object
+		) );
+
+		$this->rewrite_rules_attribute_base_is_translated( $subject, $attribute_base_slug, $attribute_slug );
+		$this->rewrite_rules_attribute_base_not_translated( $subject, $attribute_base_slug, $attribute_slug );
+
+	}
+
+	public function rewrite_rules_attribute_base_is_translated( $subject, $attribute_base_slug, $attribute_slug ){
+
+		$translated_attribute_base_slug = rand_str();
+		$translated_attribute_slug = rand_str();
+
+		\WP_Mock::onFilter( 'wpml_translate_single_string' )
+		        ->with( $attribute_base_slug, $subject->url_strings_context(), $subject->url_string_name( 'attribute' ) )
+		        ->reply( $translated_attribute_base_slug );
+
+		\WP_Mock::onFilter( 'wpml_translate_single_string' )
+		        ->with( $attribute_slug, $subject->url_strings_context(), $subject->url_string_name( 'attribute_slug', $attribute_slug ) )
+		        ->reply( $translated_attribute_slug );
+
+		$rule_string = rand_str();
+
+		$values = array();
+		$values[ $attribute_base_slug.'/'.$attribute_slug.'/([^/]+)/?$' ] = $rule_string;
+
+		$expected_values = array();
+		$expected_values[ $translated_attribute_base_slug.'/'.$translated_attribute_slug.'/([^/]+)/?$' ] = $rule_string;
+
+		$filtered_rules = $subject->translate_attributes_bases_in_rewrite_rules( $values );
+
+		$this->assertEquals( $expected_values, $filtered_rules );
+
+	}
+
+	public function rewrite_rules_attribute_base_not_translated( $subject, $attribute_base_slug, $attribute_slug ){
+
+		$translated_attribute_slug = rand_str();
+
+		\WP_Mock::onFilter( 'wpml_translate_single_string' )
+		        ->with( $attribute_base_slug, $subject->url_strings_context(), $subject->url_string_name( 'attribute' ) )
+		        ->reply( $attribute_base_slug );
+
+		\WP_Mock::onFilter( 'wpml_translate_single_string' )
+		        ->with( $attribute_slug, $subject->url_strings_context(), $subject->url_string_name( 'attribute_slug', $attribute_slug ) )
+		        ->reply( $translated_attribute_slug );
+
+		$rule_string = rand_str();
+
+		$values = array();
+		$values[ $attribute_base_slug.'/'.$attribute_slug.'/([^/]+)/?$' ] = $rule_string;
+
+		$expected_values = array();
+		$expected_values[ $attribute_base_slug.'/'.$translated_attribute_slug.'/([^/]+)/?$' ] = $rule_string;
+
+		$filtered_rules = $subject->translate_attributes_bases_in_rewrite_rules( $values );
+
+		$this->assertEquals( $expected_values, $filtered_rules );
 
 	}
 
