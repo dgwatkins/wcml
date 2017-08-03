e969b9d6ef805e4fe5d43be147438ac9307c22fb
sergey.r <sergey.r@icanlocalize.com>
2017-08-03 13:12:27 +0300

    wcml-2101 WC Bookings + WC Product Add-ons - When Max bookings per block is larger than 1, the cart total won't include add-ons price

--- tests/phpunit/tests/classes/compatibility/test-wcml-product-addons.php
+++ tests/phpunit/tests/classes/compatibility/test-wcml-product-addons.php
@@ -27,8 +27,9 @@ class Test_WCML_Product_Addons extends OTGS_TestCase {
 
 		$subject = $this->get_subject();
 		\WP_Mock::expectFilterAdded( 'wcml_do_not_display_custom_fields_for_product', array( $subject, 'replace_tm_editor_custom_fields_with_own_sections' ) );
-		$subject->add_hooks();
+		\WP_Mock::expectFilterAdded( 'wcml_cart_contents_not_changed', array( $subject, 'filter_booking_addon_product_in_cart_contents'	), 20 );
 
+		$subject->add_hooks();
 	}
 	/**
 	 * @test
