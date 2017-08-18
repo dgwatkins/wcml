333418d41e2b5eeb86b57857f539a2e8c4129db3
sergey.r <sergey.r@icanlocalize.com>
2017-08-18 13:43:54 +0300

    wcml-2121 WooCommerce Klarna -> Author changing ajax actions -> we need to merge the updates to his compatibility class https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-2121

--- tests/phpunit/tests/classes/compatibility/test-wcml-klarna-gateways.php
+++ tests/phpunit/tests/classes/compatibility/test-wcml-klarna-gateways.php
@@ -5,13 +5,37 @@ class Test_WCML_Klarna_Gateways extends OTGS_TestCase {
 	/**
 	 * @test
 	 */
-	public function ajax_action_needs_multi_currency(){
+	public function add_hooks(){
 
 		$subject = new WCML_Klarna_Gateway();
 
 		\WP_Mock::expectFilterAdded( 'wcml_multi_currency_ajax_actions', array( $subject, 'ajax_action_needs_multi_currency' ) );
 
 		$subject->add_hooks();
+
+	}
+	
+	/**
+	 * @test
+	 */
+	public function ajax_action_needs_multi_currency() {
+
+		$subject = new WCML_Klarna_Gateway();
+
+		$expected_actions_list = array(
+			'klarna_checkout_cart_callback_update',
+			'klarna_checkout_coupons_callback',
+			'klarna_checkout_remove_coupon_callback',
+			'klarna_checkout_cart_callback_remove',
+			'klarna_checkout_shipping_callback',
+			'kco_iframe_shipping_option_change_cb',
+			'klarna_checkout_order_note_callback',
+			'kco_iframe_change_cb',
+			'kco_iframe_shipping_address_change_v2_cb',
+			'kco_iframe_shipping_address_change_cb'
+		);
+
+		$this->assertEquals( $expected_actions_list, $subject->ajax_action_needs_multi_currency( array() ) );
 
 	}
 }
