76b7f3a36f94eba939c08300ecc23997f10176ca
sergey.r <sergey.r@icanlocalize.com>
2017-08-29 23:49:20 +0300

    wcml-2134 In certain cases, updating variable product does not refresh the translation displayed price because it is heavy transient cached https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-2134

---
+++ tests/phpunit/tests/classes/inc/test-wcml-synchronize-variations-data.php
@@ -0,0 +1,194 @@
+<?php
+
+class Test_WCML_Synchronize_Variations_Data extends OTGS_TestCase {
+
+	/**
+	 * @return woocommerce_wpml
+	 */
+	public function get_woocommerce_wpml() {
+
+		return $this->getMockBuilder( 'woocommerce_wpml' )
+		            ->disableOriginalConstructor()
+		            ->getMock();
+
+	}
+
+	/**
+	 * @return SitePress
+	 */
+	public function get_sitepress() {
+
+		return $this->getMockBuilder( 'SitePress' )
+		            ->disableOriginalConstructor()
+		            ->getMock();
+
+	}
+
+	/**
+	 * @return WCML_Synchronize_Variations_Data
+	 */
+	private function get_subject( $woocommerce_wpml = null, $sitepress = null ) {
+
+		if ( null === $woocommerce_wpml ) {
+			$woocommerce_wpml = $this->get_woocommerce_wpml();
+		}
+
+		if ( null === $sitepress ) {
+			$sitepress = $this->get_sitepress();
+		}
+
+		return new WCML_Synchronize_Variations_Data( $woocommerce_wpml, $sitepress, $this->stubs->wpdb() );
+	}
+
+	/**
+	 * @test
+	 */
+	function sync_variations_taxonomies_no_terms_assigned() {
+
+		$original_variation_id   = mt_rand( 1, 100 );
+		$translated_variation_id = mt_rand( 101, 200 );
+		$taxonomy                = new stdClass();;
+		$taxonomy->name = rand_str();
+
+		$woocommerce_wpml = $this->get_woocommerce_wpml_mock_sync_variations_taxonomies();
+
+		$woocommerce_wpml->terms->expects( $this->once() )->method( 'is_translatable_wc_taxonomy' )->with( $taxonomy->name )->willReturn( false );
+
+		$subject = $this->get_subject( $woocommerce_wpml );
+
+		\WP_Mock::wpFunction( 'remove_filter', array(
+			'times'  => 1,
+			'return' => true
+		) );
+
+		\WP_Mock::wpFunction( 'get_object_taxonomies', array(
+			'args'   => array( 'product_variation' ),
+			'times'  => 1,
+			'return' => array( $taxonomy )
+		) );
+
+		$terms_for_original = array();
+		\WP_Mock::wpFunction( 'get_the_terms', array(
+			'args'   => array( $original_variation_id, $taxonomy->name ),
+			'times'  => 1,
+			'return' => $terms_for_original
+		) );
+
+		$expected_terms_for_translation = array();
+		\WP_Mock::wpFunction( 'wp_set_object_terms', array(
+			'args'   => array( $translated_variation_id, $expected_terms_for_translation, $taxonomy->name ),
+			'times'  => 1,
+			'return' => true
+		) );
+
+		$subject->sync_variations_taxonomies( $original_variation_id, $translated_variation_id, rand_str() );
+
+	}
+
+	/**
+	 * @test
+	 */
+	function sync_variations_taxonomies() {
+
+		$original_variation_id     = mt_rand( 1, 100 );
+		$translated_variation_id   = mt_rand( 101, 200 );
+		$not_translatable_taxonomy = rand_str();
+		$translatable_taxonomy     = rand_str();
+
+		$woocommerce_wpml = $this->get_woocommerce_wpml_mock_sync_variations_taxonomies();
+
+		$sitepress = $this->getMockBuilder( 'SitePress' )
+		                  ->disableOriginalConstructor()
+		                  ->setMethods( array( 'is_translated_taxonomy' ) )
+		                  ->getMock();
+
+		$this->taxonomies[ $not_translatable_taxonomy ] = false;
+		$this->taxonomies[ $translatable_taxonomy ]     = true;
+
+		$sitepress->method( 'is_translated_taxonomy' )->willReturnCallback( function ( $tax ) {
+			return $this->taxonomies[ $tax ];
+		} );
+
+		$subject = $this->get_subject( $woocommerce_wpml, $sitepress );
+
+		\WP_Mock::wpFunction( 'remove_filter', array(
+			'times'  => 1,
+			'return' => true
+		) );
+
+		\WP_Mock::wpFunction( 'get_object_taxonomies', array(
+			'args'   => array( 'product_variation' ),
+			'times'  => 1,
+			'return' => array( $not_translatable_taxonomy, $translatable_taxonomy )
+		) );
+
+		$terms         = array();
+		$term          = new stdClass();
+		$term->term_id = mt_rand( 201, 300 );
+		$terms[]       = $term;
+
+		\WP_Mock::wpFunction( 'get_the_terms', array(
+			'args'   => array( $original_variation_id, $not_translatable_taxonomy ),
+			'times'  => 1,
+			'return' => $terms
+		) );
+
+		$expected_terms = array( $term->term_id );
+
+		\WP_Mock::wpFunction( 'wp_set_object_terms', array(
+			'args'   => array( $translated_variation_id, $expected_terms, $not_translatable_taxonomy, true ),
+			'times'  => 1,
+			'return' => true
+		) );
+
+		$language           = rand_str();
+		$terms              = array();
+		$term               = new stdClass();
+		$term->term_id      = mt_rand( 301, 400 );
+		$terms[]            = $term;
+		$translated_term_id = mt_rand( 401, 500 );
+
+		\WP_Mock::wpFunction( 'get_the_terms', array(
+			'args'   => array( $original_variation_id, $translatable_taxonomy ),
+			'times'  => 1,
+			'return' => $terms
+		) );
+
+		\WP_Mock::onFilter( 'translate_object_id' )->with( $term->term_id, $translatable_taxonomy, false, $language )->reply( $translated_term_id );
+
+		$expected_terms = array( $translated_term_id );
+
+		\WP_Mock::wpFunction( 'wp_set_object_terms', array(
+			'args'   => array( $translated_variation_id, $expected_terms, $translatable_taxonomy, true ),
+			'times'  => 1,
+			'return' => true
+		) );
+
+		$subject->sync_variations_taxonomies( $original_variation_id, $translated_variation_id, $language );
+
+	}
+
+	function get_woocommerce_wpml_mock_sync_variations_taxonomies() {
+
+		$woocommerce_wpml = $this->getMockBuilder( 'woocommerce_wpml' )
+		                         ->disableOriginalConstructor()
+		                         ->getMock();
+
+		$woocommerce_wpml->sync_product_data = $this->getMockBuilder( 'WCML_Synchronize_Product_Data' )
+		                                            ->disableOriginalConstructor()
+		                                            ->setMethods( array( 'check_if_product_fields_sync_needed' ) )
+		                                            ->getMock();
+
+		$woocommerce_wpml->sync_product_data->method( 'check_if_product_fields_sync_needed' )->willReturn( true );
+
+
+		$woocommerce_wpml->terms = $this->getMockBuilder( 'WCML_Terms' )
+		                                ->disableOriginalConstructor()
+		                                ->setMethods( array( 'is_translatable_wc_taxonomy' ) )
+		                                ->getMock();
+
+		return $woocommerce_wpml;
+
+	}
+
+}
