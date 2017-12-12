2d1f325de9393d150bce90f5e82777044ee12d85
sergey.r <sergey.r@icanlocalize.com>
2017-12-12 16:59:15 +0200

    Manually set price (2nd currency) -> adding product to manually created order results in price of the first currency being used
    - convert item totals while adding new order item on edit order page
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-2261

--- tests/phpunit/tests/classes/currencies/test-wcml-multi-currency-orders.php
+++ tests/phpunit/tests/classes/currencies/test-wcml-multi-currency-orders.php
@@ -48,7 +48,24 @@ class Test_WCML_Multi_Currency_Orders extends OTGS_TestCase {
 			}
 		) );
 
+	}
 
+	public function tearDown() {
+		unset( $this->sitepress, $this->woocommerce, $this->order );
+		parent::tearDown();
+	}
+
+	/**
+	 * @return WCML_REST_API_Support
+	 */
+	private function get_subject(){
+		return new WCML_Multi_Currency_Orders( $this->wcml_multi_currency, $this->woocommerce_wpml );
+	}
+
+	/**
+	 * @test
+	 */
+	public function get_currency_for_new_order(){
 
 		$that = $this;
 		\WP_Mock::wpFunction( 'is_admin', array(
@@ -86,26 +103,6 @@ class Test_WCML_Multi_Currency_Orders extends OTGS_TestCase {
 				$that->options[$option_name] = $option_value;
 			},
 		) );
-
-
-	}
-
-	public function tearDown() {
-		unset( $this->sitepress, $this->woocommerce, $this->order );
-		parent::tearDown();
-	}
-
-	/**
-	 * @return WCML_REST_API_Support
-	 */
-	private function get_subject(){
-		return new WCML_Multi_Currency_Orders( $this->wcml_multi_currency, $this->woocommerce_wpml );
-	}
-
-	/**
-	 * @test
-	 */
-	public function get_currency_for_new_order(){
 
 		$subject = $this->get_subject();
 
@@ -155,5 +152,72 @@ class Test_WCML_Multi_Currency_Orders extends OTGS_TestCase {
 		$filtered_currency = $subject->get_currency_for_new_order( $original_currency, $this->order );
 		$this->assertEquals( $_COOKIE['_wcml_order_currency'], $filtered_currency );
 
+	}
+
+
+	/**
+	 * @test
+	 */
+	public function action_different_than_woocommerce_add_order_item(){
+		$_POST['action'] = rand_str();
+
+		$items = array();
+
+		$subject = $this->get_subject();
+		$filtered_items = $subject->set_totals_for_order_items( $items );
+
+		$this->assertSame( $items, $filtered_items );
+	}
+
+	/**
+	 * @test
+	 */
+	public function it_sets_totals_for_order_items_for_ajax_add_new_order_item_call(){
+		$_POST['action'] = 'woocommerce_add_order_item';
+		$_POST['order_id'] = 1;
+		$_POST['item_to_add'][0] = 10;
+		$original_product_id = 20;
+		$converted_price = 100;
+
+		$order_currency = 'EUR';
+
+		$item =  $this->getMockBuilder( 'WC_Order_Item_Product' )
+		              ->disableOriginalConstructor()
+		              ->setMethods( array( 'get_type', 'set_subtotal', 'set_total', 'save' ) )
+		              ->getMock();
+
+		$items = array( $item );
+
+		\WP_Mock::wpFunction( 'get_post_meta', array(
+			'args' => array( $_POST['order_id'], '_order_currency', true ),
+			'return' => $order_currency
+		) );
+
+		$this->woocommerce_wpml->products = $this->getMockBuilder( 'WCML_Products' )
+		                                         ->disableOriginalConstructor()
+		                                         ->setMethods( array( 'get_original_product_id' ) )
+		                                         ->getMock();
+
+		$this->woocommerce_wpml->products->method( 'get_original_product_id' )->willReturn( $original_product_id );
+
+
+		\WP_Mock::wpFunction( 'sanitize_text_field', array(
+			'args' => array( $_POST['item_to_add'][0] ),
+			'return' => $_POST['item_to_add'][0]
+		) );
+
+		\WP_Mock::wpFunction( 'get_post_meta', array(
+			'args' => array( $original_product_id, '_price_' . $order_currency, true ),
+			'return' => $converted_price
+		) );
+
+		$item->method( 'get_type' )->willReturn( 'line_item' );
+		$item->expects( $this->once() )->method( 'set_subtotal' )->with( $converted_price )->willReturn( true );
+		$item->expects( $this->once() )->method( 'set_total' )->with( $converted_price )->willReturn( true );
+		$item->expects( $this->once() )->method( 'save' )->willReturn( true );
+
+
+		$subject = $this->get_subject();
+		$subject->set_totals_for_order_items( $items );
 	}
 }
