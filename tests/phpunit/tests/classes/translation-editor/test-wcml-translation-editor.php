cb8d246610b5458e817b96c954bc35ccdb9f08e8
sergey.r <sergey.r@icanlocalize.com>
2017-12-25 11:55:38 +0200

    Variation downloadable files not locked in the native editor
    - use Variable ID to get original one and check files sync setting
    - tests coverage
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-2270

--- tests/phpunit/tests/classes/translation-editor/test-wcml-translation-editor.php
+++ tests/phpunit/tests/classes/translation-editor/test-wcml-translation-editor.php
@@ -61,5 +61,61 @@ class Test_WCML_Translation_Editor extends OTGS_TestCase {
 		$this->assertFalse( $subject->show_page_builders_translation_editor_warning( true, $post_id) );
 	}
 
+	/**
+	 * @test
+	 */
+	public function it_locks_variable_fields_for_variable_with_auto_sync_download_files(){
+
+		$post_id = mt_rand( 1, 10 );
+		$_GET['post'] = $post_id;
+
+		\WP_Mock::wpFunction( 'get_post_type', array(
+			'args'   => array( $post_id ),
+			'return' => 'product'
+		));
+
+		\WP_Mock::wpFunction( 'get_post_status', array(
+			'args'   => array( $post_id ),
+			'return' => 'publish'
+		));
+
+		$this->woocommerce_wpml->products = $this->getMockBuilder( 'wcml_products' )
+		                                         ->disableOriginalConstructor()
+												->setMethods( array( 'is_original_product', 'get_original_product_id'))
+		                                         ->getMock();
+
+		$this->woocommerce_wpml->products->method( 'is_original_product' )->with( $post_id )->willReturn( false );
+
+		$variation = new stdClass();
+		$variation->ID = mt_rand( 11, 20 );
+		$original_id = mt_rand( 21, 30 );
+
+		$variations = array( $variation );
+
+		\WP_Mock::wpFunction( 'get_posts', array(
+			'return' => $variations
+		));
+
+		\WP_Mock::wpFunction( 'absint', array(
+			'args'   => array( $variation->ID ),
+			'return' => $variation->ID
+		));
+
+		$this->woocommerce_wpml->products->method( 'get_original_product_id' )->with( $variation->ID )->willReturn( $original_id );
+
+		\WP_Mock::wpFunction( 'get_post_meta', array(
+			'args'   => array( $original_id, 'wcml_sync_files', true ),
+			'return' => false
+		));
+
+		$subject = $this->get_subject();
+
+		ob_start();
+		$subject->lock_variable_fields( true );
+		$output = ob_get_clean();
+
+		$this->assertContains( ' wcml_lock_variation_fields( {"'.$variation->ID.'":true} );', $output );
+	}
+
 
 }
