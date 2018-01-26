64aeafaec141905a79aa52e7e10da8cee1e99814
sergey.r <sergey.r@icanlocalize.com>
2018-01-26 12:30:14 +0200

    Bad Request when finishing checkout 2nd language and WooCommerce 3.3
    - filter 'ajax_url' only for WC ajax params
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-2293

--- tests/phpunit/tests/classes/inc/test-wcml-ajax-setup.php
+++ tests/phpunit/tests/classes/inc/test-wcml-ajax-setup.php
@@ -12,7 +12,7 @@ class Test_WCML_Ajax_Setup extends OTGS_TestCase {
 
 		$this->sitepress = $this->getMockBuilder( 'Sitepress' )
 			->disableOriginalConstructor()
-			->setMethods( array( 'get_wp_api', 'get_current_language' ) )
+			->setMethods( array( 'get_wp_api', 'get_current_language', 'get_default_language' ) )
 			->getMock();
 
 		$this->wp_api = $this->getMockBuilder( 'WPML_WP_API' )
@@ -80,8 +80,67 @@ class Test_WCML_Ajax_Setup extends OTGS_TestCase {
 			->willReturn( false );
 
 		$subject = $this->get_subject();
-		\WP_Mock::expectFilterAdded( 'woocommerce_get_script_data', array( $subject, 'filter_woocommerce_ajax_params' ) );
+		\WP_Mock::expectFilterAdded( 'woocommerce_get_script_data', array( $subject, 'add_language_parameter_to_ajax_url' ) );
 		$subject->init();
 	}
+
+	/**
+	 * @test
+	 */
+	public function it_adds_language_parameter_to_ajax_url()
+	{
+		$woocommerce_params['ajax_url'] = rand_str();
+		$expected_ajax_url = rand_str();
+
+		$this->wp_api->expects( $this->once() )
+		             ->method('constant')
+		             ->with('ICL_LANGUAGE_CODE')
+		             ->willReturn( rand_str( 2 ) );
+
+		$this->sitepress->method( 'get_current_language' )->willReturn( rand_str( 2 ) );
+		$this->sitepress->method( 'get_default_language' )->willReturn( rand_str( 2 ) );
+
+		\WP_Mock::wpFunction( 'add_query_arg', array(
+			'times' => 1,
+			'return' => $expected_ajax_url
+		) );
+
+		$subject = $this->get_subject();
+		$filtered_params = $subject->add_language_parameter_to_ajax_url( $woocommerce_params );
+
+		$this->assertEquals( array( 'ajax_url' => $expected_ajax_url ), $filtered_params );
+	}
+
+	/**
+	 * @test
+	 */
+	public function it_does_not_add_language_parameter_to_ajax_url_for_default_language()
+	{
+		$woocommerce_params['ajax_url'] = rand_str();
+		$lang_code = rand_str( 2 );
+
+		$this->sitepress->method( 'get_current_language' )->willReturn( $lang_code );
+		$this->sitepress->method( 'get_default_language' )->willReturn( $lang_code );
+
+		$subject = $this->get_subject();
+		$filtered_params = $subject->add_language_parameter_to_ajax_url( $woocommerce_params );
+
+		$this->assertEquals( $woocommerce_params, $filtered_params );
+	}
+
+	/**
+	 * @test
+	 */
+	public function it_does_not_add_language_parameter_to_ajax_url()
+	{
+		$woocommerce_params = array();
+
+		$subject = $this->get_subject();
+		$filtered_params = $subject->add_language_parameter_to_ajax_url( $woocommerce_params );
+
+		$this->assertEquals( $woocommerce_params, $filtered_params );
+	}
+
+
 
 }
