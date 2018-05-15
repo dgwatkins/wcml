9a09893d72935dc57e104ce64b50c522cdbc4cbf
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2018-05-15 11:52:55 -0300

    Update a test due to changes in the translator API
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmltm-2540

--- tests/tests/admin-menus/test-wcml-admin-menus.php
+++ tests/tests/admin-menus/test-wcml-admin-menus.php
@@ -12,6 +12,7 @@ class Test_WCML_Admin_Menus extends WCML_UnitTestCase {
 	}
 
 	public function test_check_user_admin_access(){
+		global $wpdb;
 
 		$current_user_id = get_current_user_id();
 
@@ -21,10 +22,9 @@ class Test_WCML_Admin_Menus extends WCML_UnitTestCase {
 		$prevent_access = WCML_Admin_Menus::check_user_admin_access( true );
 		$this->assertTrue( $prevent_access );
 
-		$tm = new TranslationManagement();
-
-		$language_pairs = array( 'en' => array( 'es' => 1 ) );
-		$tm->add_translator( $user->ID, $language_pairs );
+		$user->add_cap( WPML_Translator_Role::CAPABILITY );
+		$language_pair_records = new WPML_Language_Pair_Records( $wpdb, new WPML_Language_Records( $wpdb ) );
+		$language_pair_records->store( $user->ID, array( 'en' => array( 'es' ) ) );
 		wp_set_current_user( $user->ID );
 
 		$prevent_access = WCML_Admin_Menus::check_user_admin_access( true );
