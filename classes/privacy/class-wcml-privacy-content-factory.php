758634043022249246e31f944ec1c90a2b9a061c
Andrea Sciamanna <andrea.s@onthegosystems.com>
2018-06-06 15:09:42 +0200

    Fatal error: Class 'WPML_Privacy_Content' not found
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlmedia-437

--- classes/privacy/class-wcml-privacy-content-factory.php
+++ classes/privacy/class-wcml-privacy-content-factory.php
@@ -9,6 +9,10 @@ class WCML_Privacy_Content_Factory implements IWPML_Backend_Action_Loader {
 	 * @return IWPML_Action
 	 */
 	public function create() {
-		return new WCML_Privacy_Content();
+		if ( class_exists( 'WPML_Privacy_Content' ) ) {
+			return new WCML_Privacy_Content();
+		}
+
+		return null;
 	}
 }
