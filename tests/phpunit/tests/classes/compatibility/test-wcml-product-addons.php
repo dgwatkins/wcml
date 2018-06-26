db000c34942e5b7cba9de5b28e916c81700cf53b
sergey.r <sergey.r@icanlocalize.com>
2018-06-26 16:03:11 +0300

    WooCommerce Product Add-ons is not added to cart in the translated language
    - do not run filtering query args on archive pages only
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-2456

--- tests/phpunit/tests/classes/compatibility/test-wcml-product-addons.php
+++ tests/phpunit/tests/classes/compatibility/test-wcml-product-addons.php
@@ -69,8 +69,8 @@ class Test_WCML_Product_Addons extends OTGS_TestCase {
 		$global_addon->ID = mt_rand( 1, 10 );
 		$global_addons[] = $global_addon;
 
-		\WP_Mock::wpFunction( 'is_product', array(
-			'return' => true
+		\WP_Mock::wpFunction( 'is_archive', array(
+			'return' => false
 		) );
 
 		\WP_Mock::wpFunction( 'get_posts', array(
