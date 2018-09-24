9abfed9fab4e750f6bd15d77060502c4463bb37a
git <david.g@onthegosystems.com>
2018-09-24 16:49:03 +0000

    Code contributed by litespeed cache author.

--- compatibility/class-wcml-litespeed-cache.php
+++ compatibility/class-wcml-litespeed-cache.php
@@ -3,7 +3,23 @@
 class WCML_LiteSpeed_Cache {
 
 	function add_hooks() {
-		add_filter( 'wcml_is_cache_enabled_for_switching_currency', '__return_true' );
+		// LiteSpeed_Cache_API::vary is available since 2.6.
+		if ( method_exists( 'LiteSpeed_Cache_API', 'v' ) && LiteSpeed_Cache_API::v( '2.6' ) ) {
+			add_filter( 'wcml_client_currency', array( $this, 'apply_client_currency' ) );
+			add_action( 'wcml_set_client_currency', array( $this, 'set_client_currency' ) );
+		}
+	}
+
+	function set_client_currency( $currency ) {
+		$this->apply_client_currency( $currency );
+
+		LiteSpeed_Cache_API::force_vary();
+	}
+
+	function apply_client_currency( $currency ) {
+		LiteSpeed_Cache_API::vary( 'wcml_currency', $currency, get_option( 'woocommerce_currency' ) );
+
+		return $currency;
 	}
 
 }
