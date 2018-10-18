9dfa564183957e5e1c0c9d80da3dbdab963f0c02
Andrea Sciamanna <andrea.s@onthegosystems.com>
2018-10-18 17:52:34 +0200

    WIP

--- classes/multi-currency/payment-gateways/class-wcml-currencies-payment-gateways.php
+++ classes/multi-currency/payment-gateways/class-wcml-currencies-payment-gateways.php
@@ -8,6 +8,8 @@ class WCML_Currencies_Payment_Gateways {
 	const OPTION_KEY = 'wcml_custom_payment_gateways_for_currencies';
 
 	private $available_gateways = array();
+	private $supported_gateways = array();
+	private $payment_gateways  = array();
 
 	/**
 	 * @param string $currency
@@ -50,30 +52,16 @@ class WCML_Currencies_Payment_Gateways {
 	public function get_gateways() {
 		$this->available_gateways = $this->get_available_payment_gateways();
 
-		$supported_gateways = array(
+		$this->supported_gateways = array(
 			'bacs' => 'WCML_Payment_Gateway_Bacs',
 		);
+		$this->supported_gateways = apply_filters( 'wcml_supported_currency_payment_gateways', $this->supported_gateways );
 
-		$supported_gateways = apply_filters( 'wcml_supported_currency_payment_gateways', $supported_gateways );
-
-		if ( is_array( $supported_gateways ) ) {
-			/** @var \WCML_Payment_Gateway $supported_gateway */
-			foreach ( $supported_gateways as $id => $supported_gateway ) {
-				if ( is_subclass_of( $supported_gateways, 'WCML_Payment_Gateway' ) && array_key_exists( $id, $this->available_gateways ) ) {
-					$payment_gateways[ $id ] = new $supported_gateway( $this->available_gateways[ $id ] );
-				}
-			}
-		}
-
-		$non_supported_gateways = array_diff( array_keys( $this->available_gateways ), array_keys( $payment_gateways ) );
-
-		/** @var int $non_supported_gateway */
-		foreach ( $non_supported_gateways as $non_supported_gateway ) {
-			$payment_gateways[ $non_supported_gateway ] = new WCML_Not_Supported_Payment_Gateway( $this->available_gateways[ $non_supported_gateway ] );
-		}
+		$this->store_supported_gateways();
+		$this->store_non_supported_gateways();
 
 
-		return $payment_gateways;
+		return $this->payment_gateways;
 	}
 
 	/**
@@ -81,6 +69,35 @@ class WCML_Currencies_Payment_Gateways {
 	 */
 	private function get_available_payment_gateways() {
 		return WC()->payment_gateways()->get_available_payment_gateways();
+	}
+
+	/**
+	 * @param $id
+	 *
+	 * @return bool
+	 */
+	private function is_a_valid_gateway( $id ) {
+		return is_subclass_of( $this->supported_gateways, 'WCML_Payment_Gateway' ) && array_key_exists( $id, $this->available_gateways );
+	}
+
+	private function store_supported_gateways() {
+		if ( is_array( $this->supported_gateways ) ) {
+			/** @var \WCML_Payment_Gateway $supported_gateway */
+			foreach ( $this->supported_gateways as $id => $supported_gateway ) {
+				if ( $this->is_a_valid_gateway( $id ) ) {
+					$this->payment_gateways[ $id ] = new $supported_gateway( $this->available_gateways[ $id ] );
+				}
+			}
+		}
+	}
+
+	private function store_non_supported_gateways() {
+		$non_supported_gateways = array_diff( array_keys( $this->available_gateways ), array_keys( $this->payment_gateways ) );
+
+		/** @var int $non_supported_gateway */
+		foreach ( $non_supported_gateways as $non_supported_gateway ) {
+			$this->payment_gateways[ $non_supported_gateway ] = new WCML_Not_Supported_Payment_Gateway( $this->available_gateways[ $non_supported_gateway ] );
+		}
 	}
 
 }
