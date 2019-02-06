08c076bdf8a6d9eeca88f8da963b3c7ce050d731
Greg Wierzba <grzegorz.w@onthegosystems.com>
2019-02-06 16:04:25 +0100

    Trailing comma added to product gallery handling

--- tests/phpunit/tests/classes/media/test-wcml-append-gallery-to-post-media-ids.php
+++ tests/phpunit/tests/classes/media/test-wcml-append-gallery-to-post-media-ids.php
@@ -16,7 +16,12 @@ class Test_WCML_Append_Gallery_To_Post_Media_Ids extends OTGS_TestCase {
 	public function it_should_add_hooks() {
 		$subject = $this->get_subject();
 
-		$this->expectFilterAdded( 'wpml_ids_of_media_used_in_post', [ $subject, 'add_product_gallery_images' ], 10, 2 );
+		$this->expectFilterAdded(
+			'wpml_ids_of_media_used_in_post',
+			[ $subject, 'add_product_gallery_images' ],
+			10,
+			2
+		);
 
 		$subject->add_hooks();
 
@@ -35,22 +40,24 @@ class Test_WCML_Append_Gallery_To_Post_Media_Ids extends OTGS_TestCase {
 
 		$post_id = mt_rand( 1, 199 );
 
-		\WP_Mock::userFunction( 'get_post_meta', [
-			'args'   => [ $post_id, '_product_image_gallery', true ],
-			'return' => $gallery_string
-		] );
+		\WP_Mock::userFunction(
+			'get_post_meta',
+			[
+				'args'   => [ $post_id, '_product_image_gallery', true ],
+				'return' => $gallery_string,
+			]
+		);
 
 		$this->assertSame( $expected, $subject->add_product_gallery_images( $existing_ids, $post_id ) );
 	}
 
 	public function gallery_strings_provider() {
 		return [
-			'nothing happens'    => [ '', [], [] ],
-			'nothing added'      => [ '', [ 3, 4 ], [ 3, 4 ] ],
-			'add ids'            => [ '1,2, 3 ,4 , 5', [], [ 1, 2, 3, 4, 5 ] ],
-			'doesnt add doubles' => [ '1,2,3', [ 1, 2 ], [ 1, 2, 3 ] ],
+			'nothing happens'       => [ '', [], [] ],
+			'nothing added'         => [ '', [ 3, 4 ], [ 3, 4 ] ],
+			'add ids'               => [ '1,2, 3 ,4 , 5', [], [ 1, 2, 3, 4, 5 ] ],
+			'doesnt add doubles'    => [ '1,2,3', [ 1, 2 ], [ 1, 2, 3 ] ],
+			'ignore trailing_comma' => [ '1,2,3,', [ 1, 2 ], [ 1, 2, 3 ] ],
 		];
 	}
-
-
 }
