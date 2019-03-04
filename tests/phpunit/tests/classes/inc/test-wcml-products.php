b30a19c8492e7129119f08a4248e2f92a85405e2
Andrea Sciamanna <andrea.s@onthegosystems.com>
2019-03-04 16:14:07 +0100

    Add $object_id to `wcml_price_custom_fields_filtered` filter
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-2693
    
    - Adjusted some code style and PHPDoc
    - Extracted calls to `apply_filters( 'wcml_price_custom_fields_filtered' ...` to a global method to remove duplication and risk of inconsistency between calls (they were all identical)
    - Deprecated `wcml_price_custom_fields_filtered` and replaced with `wcml_price_custom_fields` ("filtered" in the name of a **filter** is redundant and does not give any additional value
    - Added failsafe logic in `wcml_price_custom_fields` to prevent a filter to not return an array
    - Adjusted existing tests
    - Added coverage for `\wcml_price_custom_fields`

--- tests/phpunit/tests/classes/inc/test-wcml-products.php
+++ tests/phpunit/tests/classes/inc/test-wcml-products.php
@@ -320,6 +320,7 @@ class Test_WCML_Products extends OTGS_TestCase {
 	/**
 	 * @test
 	 * @runInSeparateProcess
+	 * @group wcml_price_custom_fields
 	 */
 	public function it_filter_product_data(){
 
@@ -348,6 +349,24 @@ class Test_WCML_Products extends OTGS_TestCase {
 			'args'  => array( $product_id ),
 			'return' => 'product'
 		));
+
+		WP_Mock::userFunction( 'wcml_price_custom_fields', array(
+			'return' => function ( $object_id ) {
+				$default_keys = array(
+					'_max_variation_price',
+					'_max_variation_regular_price',
+					'_max_variation_sale_price',
+					'_min_variation_price',
+					'_min_variation_regular_price',
+					'_min_variation_sale_price',
+					'_price',
+					'_regular_price',
+					'_sale_price',
+				);
+
+				return apply_filters( 'wcml_price_custom_fields', $default_keys, $object_id );
+			}
+		) );
 
 		$enable_multi_currency = 1;
 		$this->wp_api->method( 'constant' )->with( 'WCML_MULTI_CURRENCIES_INDEPENDENT' )->willReturn( $enable_multi_currency );
