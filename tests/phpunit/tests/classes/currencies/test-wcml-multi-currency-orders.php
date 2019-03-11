33f0c4e12830b469ba8d1576f3ca2ca30abd2697
sergey.r <sergey.r@icanlocalize.com>
2019-03-11 15:08:26 +0200

    When multicurrency is enabled, adding manually a product to an order has some weird issues
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-2703

--- tests/phpunit/tests/classes/currencies/test-wcml-multi-currency-orders.php
+++ tests/phpunit/tests/classes/currencies/test-wcml-multi-currency-orders.php
@@ -304,7 +304,7 @@ class Test_WCML_Multi_Currency_Orders extends OTGS_TestCase {
 
 		$item =  $this->getMockBuilder( 'WC_Order_Item_Product' )
 		              ->disableOriginalConstructor()
-		              ->setMethods( array( 'get_type', 'set_subtotal', 'set_total', 'save', 'meta_exists', 'add_meta_data', 'get_subtotal', 'get_total', 'get_quantity', 'get_product_id', 'get_variation_id' ) )
+		              ->setMethods( array( 'get_type', 'set_subtotal', 'set_total', 'save', 'meta_exists', 'add_meta_data', 'update_meta_data', 'get_subtotal', 'get_total', 'get_quantity', 'get_product_id', 'get_variation_id' ) )
 		              ->getMock();
 
 		$items = array( $item );
@@ -335,6 +335,7 @@ class Test_WCML_Multi_Currency_Orders extends OTGS_TestCase {
 		$item->method( 'get_type' )->willReturn( 'line_item' );
 		$item->method( 'meta_exists' )->willReturn( false );
 		$item->method( 'add_meta_data' )->willReturn( true );
+		$item->method( 'update_meta_data' )->willReturn( true );
 		$item->method( 'get_quantity' )->willReturn( 1 );
 		$item->method( 'get_subtotal' )->willReturn( $subtotal );
 		$item->method( 'get_total' )->willReturn( $total );
@@ -352,6 +353,100 @@ class Test_WCML_Multi_Currency_Orders extends OTGS_TestCase {
 	/**
 	 * @test
 	 */
+	public function it_updates_order_currency_and_converted_totals_for_order_items_for_ajax_save_order_items_call(){
+		$_POST['action']     = 'woocommerce_save_order_items';
+		$_POST['order_id']   = 3;
+
+		$product_id          = 10;
+		$original_product_id = 9;
+
+		$this->subtotal      = 100;
+		$this->total         = 100;
+		$item_price          = 10;
+
+		$this->old_quantity  = 10;
+		$new_quantity        = 15;
+
+		$expected_total      = 150;
+		$expected_subtotal   = 150;
+
+		$_COOKIE['_wcml_order_currency'] = 'USD';
+		$order_currency = '';
+
+		$item =  $this->getMockBuilder( 'WC_Order_Item_Product' )
+		              ->disableOriginalConstructor()
+		              ->setMethods( array( 'get_type', 'get_meta', 'set_subtotal', 'set_total', 'save', 'meta_exists', 'update_meta_data', 'get_subtotal', 'get_total', 'get_quantity', 'get_product_id', 'get_variation_id', 'get_product' ) )
+		              ->getMock();
+
+		$items = array( $item );
+
+		\WP_Mock::wpFunction( 'get_post_meta', array(
+			'args' => array( $_POST['order_id'], '_order_currency', true ),
+			'return' => $order_currency
+		) );
+
+		\WP_Mock::wpFunction( 'update_post_meta', array(
+			'args' => array( $_POST['order_id'], '_order_currency', $_COOKIE['_wcml_order_currency'] ),
+			'times' => 1,
+			'return' => true
+		) );
+
+		$this->woocommerce_wpml->products = $this->getMockBuilder( 'WCML_Products' )
+		                                         ->disableOriginalConstructor()
+		                                         ->setMethods( array( 'get_original_product_id' ) )
+		                                         ->getMock();
+
+		$this->woocommerce_wpml->products->method( 'get_original_product_id' )->willReturn( $original_product_id );
+
+
+		$this->wcml_multi_currency->prices = $this->getMockBuilder( 'WCML_Prices' )
+		                             ->disableOriginalConstructor()
+		                             ->setMethods( array( 'raw_price_filter' ) )
+		                             ->getMock();
+
+		$product = $this->getMockBuilder( 'WC_Product' )
+		                ->disableOriginalConstructor()
+		                ->setMethods( array( 'get_price' ) )
+		                ->getMock();
+		$product->method( 'get_price' )->willReturn( $item_price );
+
+		\WP_Mock::wpFunction( 'get_post_meta', array(
+			'args' => array( $original_product_id, '_price_' . $order_currency, true ),
+			'return' => false
+		) );
+
+		$that = $this;
+		$item->method( 'get_meta' )->willReturnCallback( function ( $const ) use ( $that ) {
+			if ( '_wcml_converted_total' === $const ) {
+				return $that->total;
+			} else if ( '_wcml_converted_subtotal' === $const ) {
+				return $that->subtotal;
+			} else if ( '_wcml_total_qty' === $const ) {
+				return $that->old_quantity;
+			}
+		} );
+
+		$item->method( 'get_type' )->willReturn( 'line_item' );
+		$item->method( 'meta_exists' )->willReturn( true );
+		$item->method( 'update_meta_data' )->willReturn( true );
+		$item->method( 'get_quantity' )->willReturn( $new_quantity );
+		$item->method( 'get_subtotal' )->willReturn( $this->subtotal );
+		$item->method( 'get_total' )->willReturn( $this->total );
+		$item->method( 'get_product_id' )->willReturn( $product_id );
+		$item->method( 'get_product' )->willReturn( $product );
+		$item->method( 'get_variation_id' )->willReturn( 0 );
+		$this->wcml_multi_currency->prices->method( 'raw_price_filter' )->willReturn( $item_price );
+		$item->expects( $this->once() )->method( 'set_subtotal' )->with( $expected_total )->willReturn( true );
+		$item->expects( $this->once() )->method( 'set_total' )->with( $expected_subtotal )->willReturn( true );
+		$item->expects( $this->once() )->method( 'save' )->willReturn( true );
+
+		$subject = $this->get_subject();
+		$subject->set_totals_for_order_items( $items );
+	}
+
+	/**
+	 * @test
+	 */
 	public function it_adds_woocommerce_hidden_order_itemmeta(){
 
 		$itemmeta = array();
@@ -359,6 +454,6 @@ class Test_WCML_Multi_Currency_Orders extends OTGS_TestCase {
 		$subject = $this->get_subject();
 		$filtered_itemmeta = $subject->add_woocommerce_hidden_order_itemmeta( $itemmeta );
 
-		$this->assertSame( array( '_wcml_converted_subtotal', '_wcml_converted_total'), $filtered_itemmeta );
+		$this->assertSame( array( '_wcml_converted_subtotal', '_wcml_converted_total', '_wcml_total_qty' ), $filtered_itemmeta );
 	}
 }
