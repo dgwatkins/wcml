7b49fa6713a5bc6dad6312ff51980b832e81d4e4
Sergey.r <sergey.r@onthegosystems.com>
2019-03-26 11:50:45 +0200

    [FIXUP] small fix

--- classes/class-woocommerce-wpml.php
+++ classes/class-woocommerce-wpml.php
@@ -180,13 +180,13 @@ class woocommerce_wpml {
 		    $this->translation_editor->add_hooks();
 		    $tp_support = new WCML_TP_Support( $this, $wpdb , new WPML_Element_Translation_Package );
 		    $tp_support->add_hooks();
+		    $this->sync_variations_data = new WCML_Synchronize_Variations_Data($this, $sitepress, $wpdb);
 	    }
 
 	    if( is_admin() ) {
+		    $this->sync_variations_data->add_hooks();
             $this->troubleshooting = new WCML_Troubleshooting( $this, $sitepress, $wpdb );
             $this->languages_upgrader = new WCML_Languages_Upgrader;
-            $this->sync_variations_data = new WCML_Synchronize_Variations_Data($this, $sitepress, $wpdb);
-            $this->sync_variations_data->add_hooks();
 			$this->wcml_products_screen = new WCML_Products_Screen_Options($sitepress);
             $this->wcml_products_screen->init();
 	        $wcml_pointers = new WCML_Pointers();
