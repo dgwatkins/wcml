3eba090e31f76526ed577cef7a2d4aec3d9ca386
andreasp <andreas.p@onthegosystems.com>
2019-04-24 17:02:20 +0300

    Version 4.6.2-beta1

--- wpml-woocommerce.php
+++ wpml-woocommerce.php
@@ -8,7 +8,7 @@
   Text Domain: woocommerce-multilingual
   Requires at least: 4.7
   Tested up to: 5.1.1
-  Version: 4.6.1
+  Version: 4.6.2-beta1
   WC requires at least: 3.3.0
   WC tested up to: 3.6.1
 */
@@ -17,7 +17,7 @@ if ( defined( 'WCML_VERSION' ) ) {
 	return;
 }
 
-define( 'WCML_VERSION', '4.6.1' );
+define( 'WCML_VERSION', '4.6.2-beta1' );
 define( 'WCML_PLUGIN_PATH', dirname( __FILE__ ) );
 define( 'WCML_PLUGIN_FOLDER', basename( WCML_PLUGIN_PATH ) );
 define( 'WCML_LOCALE_PATH', WCML_PLUGIN_PATH . '/locale' );
@@ -98,4 +98,3 @@ function load_wcml_without_wpml() {
 		$woocommerce_wpml = new woocommerce_wpml();
 	}
 }
-
