8968f13b7e7686c2756511425335b5753655b049
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2019-05-17 11:24:16 +0200

    Rewrite WCML_Checkout_Addons to work with plugin version 2.0
    - removed hook wc_checkout_add_ons_options no longer used in checkout addons plugin
    - created a new hook for option_wc_checkout_add_ons, filters labels to translate them and price to update
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-2792

--- inc/class-wcml-compatibility.php
+++ inc/class-wcml-compatibility.php
@@ -20,9 +20,9 @@ class WCML_Compatibility {
 	/**
 	 * WCML_Compatibility constructor.
 	 *
-	 * @param SitePress $sitepress
-	 * @param woocommerce_wpml $woocommerce_wpml
-	 * @param wpdb $wpdb
+	 * @param SitePress                        $sitepress
+	 * @param woocommerce_wpml                 $woocommerce_wpml
+	 * @param wpdb                             $wpdb
 	 * @param WPML_Element_Translation_Package $tp
 	 */
 	function __construct( SitePress $sitepress, woocommerce_wpml $woocommerce_wpml, wpdb $wpdb, WPML_Element_Translation_Package $tp ) {
@@ -35,33 +35,33 @@ class WCML_Compatibility {
 	}
 
 	function init() {
-		//hardcoded list of extensions and check which ones the user has and then include the corresponding file from the ‘compatibility’ folder
+		// hardcoded list of extensions and check which ones the user has and then include the corresponding file from the ‘compatibility’ folder
 		global $woocommerce;
 
-		//WooCommerce Tab Manager plugin
+		// WooCommerce Tab Manager plugin
 		if ( class_exists( 'WC_Tab_Manager' ) ) {
 			$this->tab_manager = new WCML_Tab_Manager( $this->sitepress, $woocommerce, $this->woocommerce_wpml, $this->wpdb, $this->tp );
 			$this->tab_manager->add_hooks();
 		}
 
-		//WooCommerce Table Rate Shipping plugin
+		// WooCommerce Table Rate Shipping plugin
 		if ( defined( 'TABLE_RATE_SHIPPING_VERSION' ) ) {
 			$this->table_rate_shipping = new WCML_Table_Rate_Shipping( $this->sitepress, $this->woocommerce_wpml );
 			$this->table_rate_shipping->add_hooks();
 		}
 
-		//WooCommerce Subscriptions
+		// WooCommerce Subscriptions
 		if ( class_exists( 'WC_Subscriptions' ) ) {
 			$this->wp_subscriptions = new WCML_WC_Subscriptions( $this->woocommerce_wpml, $this->wpdb );
 			$this->wp_subscriptions->add_hooks();
 		}
 
-		//WooCommerce Name Your Price
+		// WooCommerce Name Your Price
 		if ( class_exists( 'WC_Name_Your_Price' ) ) {
 			$this->name_your_price = new WCML_WC_Name_Your_Price();
 		}
 
-		//Product Bundle
+		// Product Bundle
 		if ( class_exists( 'WC_Product_Bundle' ) && function_exists( 'WC_PB' ) ) {
 			$product_bundle_items  = new WCML_WC_Product_Bundles_Items();
 			$this->product_bundles = new WCML_Product_Bundles( $this->sitepress, $this->woocommerce_wpml, $product_bundle_items, $this->wpdb );
@@ -83,19 +83,19 @@ class WCML_Compatibility {
 		if ( defined( 'PER_PRODUCT_SHIPPING_VERSION' ) ) {
 			new WCML_Per_Product_Shipping();
 		}
-		//Store Exporter plugin
+		// Store Exporter plugin
 		if ( defined( 'WOO_CE_PATH' ) ) {
 			$this->wc_exporter = new WCML_wcExporter( $this->sitepress, $this->woocommerce_wpml );
 			$this->wc_exporter->add_hooks();
 		}
 
-		//Gravity Forms
+		// Gravity Forms
 		if ( class_exists( 'GFForms' ) ) {
 			$this->gravityforms = new WCML_gravityforms( $this->sitepress, $this->woocommerce_wpml );
 			$this->gravityforms->add_hooks();
 		}
 
-		//Sensei WooThemes
+		// Sensei WooThemes
 		if ( class_exists( 'WooThemes_Sensei' ) ) {
 			$this->sensei = new WCML_Sensei(
 				$this->sitepress,
@@ -105,7 +105,7 @@ class WCML_Compatibility {
 			$this->sensei->add_hooks();
 		}
 
-		//Extra Product Options
+		// Extra Product Options
 		if ( class_exists( 'TM_Extra_Product_Options' ) ) {
 			$this->extra_product_options = new WCML_Extra_Product_Options();
 		}
@@ -138,7 +138,7 @@ class WCML_Compatibility {
 		}
 
 		// WooCommerce Advanced Ajax Layered Navigation
-		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+		include_once ABSPATH . 'wp-admin/includes/plugin.php';
 		if ( is_plugin_active( 'woocommerce-ajax-layered-nav/ajax_layered_nav-widget.php' ) ) {
 			$this->wc_ajax_layered_nav_widget = new WCML_Ajax_Layered_Nav_Widget();
 		}
@@ -154,7 +154,7 @@ class WCML_Compatibility {
 		}
 
 		// woocommerce checkout addons
-		if ( function_exists( 'init_woocommerce_checkout_add_ons' ) ) {
+		if ( class_exists( 'WC_Checkout_Add_Ons_Loader' ) ) {
 			$this->wc_checkout_addons = new WCML_Checkout_Addons();
 		}
 
@@ -166,7 +166,7 @@ class WCML_Compatibility {
 			$this->wpseo = new WCML_WPSEO();
 		}
 
-		//Adventure Tours theme
+		// Adventure Tours theme
 		if ( function_exists( 'adventure_tours_check' ) ) {
 			$this->adventure_tours = new WCML_Adventure_tours( $this->woocommerce_wpml, $this->sitepress, $this->tp );
 			$this->adventure_tours->add_hooks();
@@ -177,7 +177,7 @@ class WCML_Compatibility {
 			$this->flatsome = new WCML_Flatsome();
 		}
 
-		//Aurum Theme
+		// Aurum Theme
 		if ( wp_get_theme()->get( 'Name' ) === 'Aurum' ) {
 			new WCML_Aurum();
 		}
@@ -227,14 +227,14 @@ class WCML_Compatibility {
 		}
 
 		// MaxStore-Pro Theme
-		if ( defined( 'ETHEME_THEME_NAME') && 'Blanco' === ETHEME_THEME_NAME ) {
+		if ( defined( 'ETHEME_THEME_NAME' ) && 'Blanco' === ETHEME_THEME_NAME ) {
 			$this->etheme_blanco = new WCML_Etheme_Blanco();
 			$this->etheme_blanco->add_hooks();
 		}
 
 		// WPBakery Page Builder
-		if ( defined( 'WPB_VC_VERSION') ) {
-			$this->wpb_vc= new WCML_Wpb_Vc();
+		if ( defined( 'WPB_VC_VERSION' ) ) {
+			$this->wpb_vc = new WCML_Wpb_Vc();
 			$this->wpb_vc->add_hooks();
 		}
 
@@ -245,7 +245,7 @@ class WCML_Compatibility {
 		}
 
 		// Woo Variations Table
-		if ( defined( 'WOO_VARIATIONS_TABLE_VERSION') ) {
+		if ( defined( 'WOO_VARIATIONS_TABLE_VERSION' ) ) {
 			$this->wpb_woo_var_table = new WCML_Woo_Var_Table( $this->sitepress->get_current_language() );
 			$this->wpb_woo_var_table->add_hooks();
 		}
