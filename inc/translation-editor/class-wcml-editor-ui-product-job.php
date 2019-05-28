c6b01042b6f6385726e83e0052bba518aa8e2475
sergey.r <sergey.r@icanlocalize.com>
2019-05-28 14:56:45 +0300

    revert c6d95476629940bef584502700aaf3340fb53970

--- inc/translation-editor/class-wcml-editor-ui-product-job.php
+++ inc/translation-editor/class-wcml-editor-ui-product-job.php
@@ -918,10 +918,6 @@ class WCML_Editor_UI_Product_Job extends WPML_Editor_UI_Job {
 	public function requires_translation_complete_for_each_field() {
 		return false;
 	}
-
-	public function is_hide_completed_switcher() {
-		return true;
-	}
 	
 	public function is_hide_empty_fields() {
 		return false;
