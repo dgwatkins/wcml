43de73ed7761fd78196916d9665f905f67a86ae6
sergey.r <sergey.r@icanlocalize.com>
2019-06-14 13:02:24 +0300

    Scheduled prices bug in the logic -> always the sale price is being used
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-2617

--- tests/phpunit/tests/classes/currencies/test-wcml-custom-prices.php
+++ tests/phpunit/tests/classes/currencies/test-wcml-custom-prices.php
@@ -8,13 +8,15 @@ class Test_WCML_Custom_Prices extends OTGS_TestCase {
 		            ->getMock();
 	}
 
-	private function get_subject( $woocommerce_wpml = false ) {
+	private function get_subject( $woocommerce_wpml = false, $wpdb = false ) {
 
 		if( !$woocommerce_wpml ){
 			$woocommerce_wpml = $this->get_woocommerce_wpml();
 		}
-
-		return new WCML_Custom_Prices( $woocommerce_wpml );
+		if ( ! $wpdb ) {
+			$wpdb = $this->stubs->wpdb();
+		}
+		return new WCML_Custom_Prices( $woocommerce_wpml, $wpdb );
 	}
 
 	/**
@@ -135,6 +137,169 @@ class Test_WCML_Custom_Prices extends OTGS_TestCase {
 		$subject = $this->get_subject();
 
 		$this->assertEquals( $sale_price, $subject->update_custom_prices( $post_id, $custom_prices, $code ) );
+	}
+
+	/**
+	 * @test
+	 * @dataProvider product_custom_prices
+	 */
+	public function it_should_get_product_custom_prices( $price, $regular_price, $sale_price, $schedule, $date_from, $date_to, $origin_date_from, $origin_date_to, $expected_custom_prices ) {
+
+		\WP_Mock::passthruFunction( 'wp_cache_set' );
+		\WP_Mock::passthruFunction( 'wp_cache_get' );
+
+		$product_id = 101;
+		$currency = 'USD';
+
+		$wcml_price_custom_fields = array(
+			'_price',
+			'_regular_price',
+			'_sale_price',
+		);
+
+		$product_meta = array(
+			'_price_' . $currency                 => array( $price ),
+			'_regular_price_' . $currency         => array( $regular_price ),
+			'_sale_price_' . $currency            => array( $sale_price ),
+			'_wcml_schedule'                      => array( $schedule ),
+			'_sale_price_dates_from_' . $currency => array( $date_from ),
+			'_sale_price_dates_to_' . $currency   => array( $date_to ),
+			'_sale_price_dates_from'              => array( $origin_date_from ),
+			'_sale_price_dates_to'                => array( $origin_date_to ),
+			'_wcml_custom_prices_status'          => array( '1' )
+		);
+
+		\WP_Mock::userFunction( 'current_time', [ 'return' => time() ] );
+
+		\WP_Mock::userFunction( 'get_option', array(
+			'args' => array( 'woocommerce_currency' ),
+			'times' => 1,
+			'return' => 'EUR'
+		) );
+
+		\WP_Mock::userFunction( 'wcml_price_custom_fields', array(
+			'times' => 1,
+			'return' => $wcml_price_custom_fields
+		) );
+
+		\WP_Mock::userFunction( 'get_post_custom', array(
+			'args' => array( $product_id ),
+			'times' => 1,
+			'return' => $product_meta
+		) );
+
+		$woocommerce_wpml = $this->get_woocommerce_wpml();
+
+		$woocommerce_wpml->products = $this->getMockBuilder( 'wcml_products' )
+		                                         ->disableOriginalConstructor()
+		                                         ->setMethods( array( 'get_original_product_id'))
+		                                         ->getMock();
+
+		$woocommerce_wpml->products->method( 'get_original_product_id' )->with( $product_id )->willReturn( $product_id );
+
+
+		$subject = $this->get_subject( $woocommerce_wpml );
+
+		$this->assertEquals( $expected_custom_prices, $subject->get_product_custom_prices( $product_id, $currency ) );
+	}
+
+	/**
+	 * @return array
+	 */
+	public function product_custom_prices(){
+
+		return array(
+			array(
+				8,
+				10,
+				8,
+				'',
+				'',
+				'',
+				'',
+				'',
+				array(
+					'_price'         => 8,
+					'_regular_price' => 10,
+					'_sale_price'    => 8,
+				)
+			),
+			array(
+				10,
+				10,
+				'',
+				'',
+				'',
+				'',
+				'',
+				'',
+				array(
+					'_price'         => 10,
+					'_regular_price' => 10,
+					'_sale_price'    => '',
+				)
+			),
+			array(
+				8,
+				10,
+				8,
+				1,
+				strtotime( date( 'Y-m-d H:i:s', strtotime("-1 day") ) ),
+				strtotime( date( 'Y-m-d H:i:s', strtotime("+1 day") ) ),
+				'',
+				'',
+				array(
+					'_price'         => 8,
+					'_regular_price' => 10,
+					'_sale_price'    => 8,
+				)
+			),
+			array(
+				8,
+				10,
+				8,
+				1,
+				strtotime( date( 'Y-m-d H:i:s', strtotime("-5 day") ) ),
+				strtotime( date( 'Y-m-d H:i:s', strtotime("-1 day") ) ),
+				'',
+				'',
+				array(
+					'_price'         => 10,
+					'_regular_price' => 10,
+					'_sale_price'    => '',
+				)
+			),
+			array(
+				8,
+				10,
+				8,
+				'',
+				'',
+				'',
+				strtotime( date( 'Y-m-d H:i:s', strtotime("-1 day") ) ),
+				strtotime( date( 'Y-m-d H:i:s', strtotime("+1 day") ) ),
+				array(
+					'_price'         => 8,
+					'_regular_price' => 10,
+					'_sale_price'    => 8,
+				)
+			),
+			array(
+				10,
+				10,
+				8,
+				'',
+				'',
+				'',
+				strtotime( date( 'Y-m-d H:i:s', strtotime("-5 day") ) ),
+				strtotime( date( 'Y-m-d H:i:s', strtotime("-1 day") ) ),
+				array(
+					'_price'         => 10,
+					'_regular_price' => 10,
+					'_sale_price'    => '',
+				)
+			),
+		);
 	}
 
 }
