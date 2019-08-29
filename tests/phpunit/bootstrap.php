11099cf594cebd57ddc4d26bbe5040e64fee6a3e
Igor Gergel <igor.g@onthegosystems.com>
2019-08-29 19:50:00 +0300

    Fix initialization of FunctionMocker:: in phpunit tests
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlga-524

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * PHP Unit tests bootstrap file.
+ *
+ * @package WCML
+ */
+
+use tad\FunctionMocker\FunctionMocker;
+
 define( 'WPML_TESTS_SITE_DIR', __DIR__ . '/site' );
 define( 'WPML_TESTS_SITE_URL', 'http://domain.tld' );
 define( 'WCML_TESTS_MAIN_FILE', __DIR__ . '/../../wpml-woocommerce.php' );
@@ -58,5 +66,21 @@ if ( version_compare( PHP_VERSION, '5.3.0' ) >= 0 ) {
 	$autoloader = $autoloader_dir . '/autoload_52.php';
 }
 require_once $autoloader;
+
+FunctionMocker::init(
+	[
+		'blacklist' => [
+			realpath( WCML_PATH ),
+		],
+		'whitelist' => [
+			realpath( WCML_PATH . '/classes' ),
+			realpath( WCML_PATH . '/compatibility' ),
+			realpath( WCML_PATH . '/inc' ),
+			realpath( WCML_PATH . '/tests/phpunit/includes' ),
+			realpath( WCML_PATH . '/tests/phpunit/stubs' ),
+		],
+		'redefinable-internals' => [],
+	]
+);
 
 require_once WCML_PATH . '/vendor/otgs/unit-tests-framework/phpunit/bootstrap.php';
