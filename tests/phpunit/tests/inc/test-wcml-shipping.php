594e3d2b4dd50a06edb320ee91395082b9bf6235
Sergey Ratushnuy <sergey.r@icanlocalize.com>
2019-09-18 15:35:25 +0300

    Shipping not calculated in secondary language
    - if WC shipping flat rates saved before you translate shipping class we need filter instance option to get proper cost value
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-2838

--- tests/phpunit/tests/inc/test-wcml-shipping.php
+++ tests/phpunit/tests/inc/test-wcml-shipping.php
@@ -36,6 +36,42 @@ class Test_WCML_Shipping extends OTGS_TestCase {
 	/**
 	 * @test
 	 */
+	public function add_hooks(){
+
+		$subject = $this->get_subject();
+
+		WP_Mock::expectActionAdded( 'woocommerce_tax_rate_added', array( $subject, 'register_tax_rate_label_string'), 10, 2 );
+		WP_Mock::expectActionAdded( 'wp_ajax_woocommerce_shipping_zone_methods_save_settings', array( $subject, 'save_shipping_zone_method_from_ajax'), 9 );
+		WP_Mock::expectActionAdded( 'icl_save_term_translation', array( $subject, 'sync_class_costs_for_new_shipping_classes'), 100, 2 );
+		WP_Mock::expectActionAdded( 'wp_ajax_woocommerce_shipping_zone_methods_save_settings', array( $subject, 'update_woocommerce_shipping_settings_for_class_costs_from_ajax'), 9 );
+
+		WP_Mock::expectFilterAdded( 'woocommerce_package_rates', array( $subject, 'translate_shipping_methods_in_package' ) );
+		WP_Mock::expectFilterAdded( 'woocommerce_rate_label', array( $subject, 'translate_woocommerce_rate_label' ) );
+		WP_Mock::expectFilterAdded( 'pre_update_option_woocommerce_flat_rate_settings', array( $subject, 'update_woocommerce_shipping_settings_for_class_costs' ) );
+		WP_Mock::expectFilterAdded( 'pre_update_option_woocommerce_international_delivery_settings', array( $subject, 'update_woocommerce_shipping_settings_for_class_costs' ) );
+		WP_Mock::expectFilterAdded( 'woocommerce_shipping_flat_rate_instance_option', array( $subject, 'get_original_shipping_class_rate' ), 10, 3 );
+
+		$wc = $this->getMockBuilder( 'WooCommerce' )->disableOriginalConstructor()->getMock();
+
+		$wc->shipping = $this->getMockBuilder( 'WC_Shipping' )->setMethods( array( 'get_shipping_methods' ) )->disableOriginalConstructor()->getMock();
+
+		$shipping_method = new stdClass();
+		$shipping_method->id = 1;
+		$wc->shipping->expects( $this->once() )
+		                     ->method( 'get_shipping_methods' )
+		                     ->willReturn( array( $shipping_method ) );
+
+		WP_Mock::userFunction( 'WC', array( 'return' => $wc ) );
+
+		WP_Mock::expectFilterAdded( 'woocommerce_shipping_' . $shipping_method->id . '_instance_settings_values', array( $subject, 'register_zone_shipping_strings' ), 9, 2 );
+		WP_Mock::expectFilterAdded( 'option_woocommerce_' . $shipping_method->id . '_settings', array( $subject, 'translate_shipping_strings' ), 9, 2 );
+
+		$subject->add_hooks();
+	}
+
+	/**
+	 * @test
+	 */
 	public function translate_shipping_method_title(){
 
 		$title = rand_str();
@@ -166,6 +202,75 @@ class Test_WCML_Shipping extends OTGS_TestCase {
 
 		$filtered_title = $subject->translate_shipping_method_title( $title, $shipping_id );
 		$this->assertEquals( $title, $filtered_title );
+	}
 
+	/**
+	 * @test
+	 */
+	public function it_should_get_original_shipping_class_rate_if_current_not_set(){
+
+		$rate = '';
+		$language = 'es';
+		$current_class_id = 22;
+		$original_class_id = 21;
+		$class_name = 'class_cost_'.$current_class_id;
+		$shipping_method = $this->getMockBuilder( 'WC_Shipping_Method' )->disableOriginalConstructor()->getMock();
+		$shipping_method->instance_settings = array();
+		$shipping_method->instance_settings['class_cost_'.$original_class_id] = 100;
+
+		$sitepress = $this->getMockBuilder( 'SitePress' )
+		                  ->disableOriginalConstructor()
+		                  ->setMethods( array(
+			                  'term_translations',
+			                  'get_current_language'
+		                  ) )
+		                  ->getMock();
+		$term_translations = $this->getMockBuilder( 'WPML_Term_Translations' )
+		                  ->disableOriginalConstructor()
+		                  ->setMethods( array(
+			                  'get_original_element'
+		                  ) )
+		                  ->getMock();
+
+		$sitepress->method( 'term_translations' )->willReturn( $term_translations );
+		$sitepress->method( 'get_current_language' )->willReturn( $language );
+		$term_translations->method( 'get_original_element' )->with( $current_class_id )->willReturn( $original_class_id );
+
+		$subject = $this->get_subject( $sitepress );
+
+		$filtered_rate = $subject->get_original_shipping_class_rate( $rate, $class_name, $shipping_method );
+
+		$this->assertEquals( $shipping_method->instance_settings['class_cost_'.$original_class_id], $filtered_rate );
+
+	}
+
+	/**
+	 * @test
+	 */
+	public function it_should_get_current_shipping_class_rate_if_set(){
+
+		$rate = '110';
+		$shipping_method = $this->getMockBuilder( 'WC_Shipping_Method' )->disableOriginalConstructor()->getMock();
+
+		$subject = $this->get_subject();
+
+		$filtered_rate = $subject->get_original_shipping_class_rate( $rate, 'class_cost_23', $shipping_method );
+
+		$this->assertEquals( $rate, $filtered_rate );
+	}
+
+	/**
+	 * @test
+	 */
+	public function it_should_get_current_rate_if_not_class_name_passed(){
+
+		$rate = '150';
+		$shipping_method = $this->getMockBuilder( 'WC_Shipping_Method' )->disableOriginalConstructor()->getMock();
+
+		$subject = $this->get_subject();
+
+		$filtered_rate = $subject->get_original_shipping_class_rate( $rate, 'cost', $shipping_method );
+
+		$this->assertEquals( $rate, $filtered_rate );
 	}
 }
