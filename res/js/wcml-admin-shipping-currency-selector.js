c7f9b6b5f4770138cafb7e05367fd063219fb2f8
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2020-03-31 14:24:34 +0200

    [WIP] [GUI] Interface to provide and save shipping costs in different curencies - Flat rate case.
    - class WCML_Multi_Currency_Shipping_Admin to handle adding shipping costs for flat rate (will be extended by adding other shipping methods
    - javascript in wp-content/plugins/woocommerce-multilingual/res/js/wcml-admin-shipping-currency-selector.js to hiding / displaying field for costs
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3159

---
+++ res/js/wcml-admin-shipping-currency-selector.js
@@ -0,0 +1,28 @@
+var watch_wc_settings_screen = function () {
+    var settings_screen = document.querySelector('.woocommerce_page_wc-settings');
+
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if ( mutation.type === 'childList' ) {
+                var cost_fields = document.querySelectorAll('.wcml-shipping-cost-currency');
+                if ( cost_fields.length > 0 ) {
+                    var enable_manual_costs_selected = document.querySelector('.select.wcml-enable-shipping-custom-currency option[selected="selected"]');
+                    cost_fields.forEach(function (field) {
+                        field.closest('tr').hidden = enable_manual_costs_selected.value === 'auto';
+                    });
+
+                    var cost_selector = document.querySelector('.select.wcml-enable-shipping-custom-currency');
+                    cost_selector.addEventListener( 'change', function( event ) {
+                        cost_fields.forEach(function (field) {
+                            field.closest('tr').hidden = cost_selector.value !== 'manual';
+                        });
+                    } );
+                }
+            }
+        });
+    });
+    var config = { attributes: true, childList: true, characterData: true };
+    observer.observe(settings_screen, config);
+};
+
+document.addEventListener('DOMContentLoaded', watch_wc_settings_screen, false);
