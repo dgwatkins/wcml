64e61983c1c43cb0367728797129561d3e02a432
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2020-04-02 10:11:32 +0200

    working version, state before attempt to refactor JS script

--- inc/currencies/class-wcml-multicurrency-shipping-admin.php
+++ inc/currencies/class-wcml-multicurrency-shipping-admin.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Class WCML_Multi_Currency_Shipping_Admin
+ *
+ * @author Konrad
+ */
 class WCML_Multi_Currency_Shipping_Admin {
 	/** @var woocommerce_wpml */
 	private $woocommerce_wpml;
@@ -7,23 +12,52 @@ class WCML_Multi_Currency_Shipping_Admin {
 	/** @var WCML_Multi_Currency */
 	private $wcml_multi_currency;
 
+	/**
+	 * WCML_Multi_Currency_Shipping_Admin constructor.
+	 *
+	 * @param WCML_Multi_Currency $wcml_multi_currency
+	 * @param woocommerce_wpml $woocommerce_wpml
+	 */
 	public function __construct( WCML_Multi_Currency $wcml_multi_currency, woocommerce_wpml $woocommerce_wpml ) {
 		$this->woocommerce_wpml = $woocommerce_wpml;
 		$this->wcml_multi_currency = $wcml_multi_currency;
 	}
 
+	/**
+	 * Registers hooks.
+	 */
 	public function add_hooks() {
 		add_filter( 'woocommerce_shipping_instance_form_fields_flat_rate', [ $this, 'woocommerce_shipping_instance_form_fields_flat_rate' ], 10, 1 );
 		add_action( 'admin_enqueue_scripts', [ $this, 'load_js' ] );
 	}
 
-	public function woocommerce_shipping_instance_form_fields_flat_rate( $field ) {
+	/**
+	 * Adds fields to display screen for flat rate shipping method.
+	 *
+	 * Adds two kind of fields:
+	 * - The select field to enable/disable shipping costs in other currencies.
+	 * @see \WCML_Multi_Currency_Shipping_Admin::add_enable_field
+	 * - The input field for each registered currency to provide shipping costs.
+	 * @see \WCML_Multi_Currency_Shipping_Admin::add_currencies_fields
+	 *
+	 * @param array $field
+	 *
+	 * @return array
+	 */
+	public function woocommerce_shipping_instance_form_fields_flat_rate( array $field ) {
 		$field = $this->add_enable_field( $field );
 		$field = $this->add_currencies_fields( $field );
 		return $field;
 	}
 
-	private function add_enable_field( $field ) {
+	/**
+	 * Adds select field to enable/disable shipping costs in other currencies.
+	 *
+	 * @param array $field
+	 *
+	 * @return array
+	 */
+	private function add_enable_field( array $field ) {
 		$enable_field = [
 			'title' => esc_html__( 'Enable costs in custom currencies', 'woocommerce-multilingual' ),
 			'type' => 'select',
@@ -39,7 +73,14 @@ class WCML_Multi_Currency_Shipping_Admin {
 		return $field;
 	}
 
-	private function add_currencies_fields( $field ) {
+	/**
+	 * Adds input field for each registered currency to provide shipping costs.
+	 *
+	 * @param array $field
+	 *
+	 * @return array
+	 */
+	private function add_currencies_fields( array $field ) {
 		foreach ( $this->wcml_multi_currency->get_currency_codes() as $currency_code ) {
 			if ( $this->wcml_multi_currency->get_default_currency() === $currency_code ) {
 				continue;
@@ -64,6 +105,9 @@ class WCML_Multi_Currency_Shipping_Admin {
 		return $field;
 	}
 
+	/**
+	 * Enqueues script responsible for JS actions on shipping fields.
+	 */
 	public function load_js() {
 		wp_enqueue_script(
 			'wcml-admin-shipping-currency-selector',
--- src/js/multicurrencyShippingAdmin/watchWcSettingsScreen.js
+++ src/js/multicurrencyShippingAdmin/watchWcSettingsScreen.js
@@ -1,17 +1,17 @@
 export default function () {
-    var settings_screen = document.querySelector('.woocommerce_page_wc-settings');
+    const settings_screen = document.querySelector('.woocommerce_page_wc-settings');
 
-    var observer = new MutationObserver(function(mutations) {
+    const observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             if ( mutation.type === 'childList' ) {
-                var cost_fields = document.querySelectorAll('.wcml-shipping-cost-currency');
+                const cost_fields = document.querySelectorAll('.wcml-shipping-cost-currency');
                 if ( cost_fields.length > 0 ) {
-                    var enable_manual_costs_selected = document.querySelector('.select.wcml-enable-shipping-custom-currency option[selected="selected"]');
+                    const enable_manual_costs_selected = document.querySelector('.select.wcml-enable-shipping-custom-currency option[selected="selected"]');
                     cost_fields.forEach(function (field) {
                         field.closest('tr').hidden = enable_manual_costs_selected.value === 'auto';
                     });
 
-                    var cost_selector = document.querySelector('.select.wcml-enable-shipping-custom-currency');
+                    const cost_selector = document.querySelector('.select.wcml-enable-shipping-custom-currency');
                     cost_selector.addEventListener( 'change', function( event ) {
                         cost_fields.forEach(function (field) {
                             field.closest('tr').hidden = cost_selector.value !== 'manual';
@@ -21,6 +21,6 @@ export default function () {
             }
         });
     });
-    var config = { attributes: true, childList: true, characterData: true };
-    observer.observe(settings_screen, config);
+    const observer_config = { attributes: true, childList: true, characterData: true };
+    observer.observe( settings_screen, observer_config );
 };
