b5f61f497ac1f7ed58ced9768b57664540048913
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2020-04-03 09:37:31 -0300

    FIXUP Move bootstrap condition to the factory

--- classes/Multicurrency/Shipping/AdminHooks.php
+++ classes/Multicurrency/Shipping/AdminHooks.php
@@ -29,10 +29,8 @@ class AdminHooks implements IWPML_Action {
 	 * Registers hooks.
 	 */
 	public function add_hooks() {
-		if ( $this->is_multicurrency_enabled() && $this->has_additional_currency_defined() ) {
-			add_filter( 'woocommerce_shipping_instance_form_fields_flat_rate', [ $this, 'add_currency_shipping_fields_to_shipping_method_form' ], 10, 1 );
-			add_action( 'admin_enqueue_scripts', [ $this, 'load_js' ] );
-		}
+		add_filter( 'woocommerce_shipping_instance_form_fields_flat_rate', [ $this, 'add_currency_shipping_fields_to_shipping_method_form' ], 10, 1 );
+		add_action( 'admin_enqueue_scripts', [ $this, 'load_js' ] );
 	}
 
 	/**
@@ -49,10 +47,9 @@ class AdminHooks implements IWPML_Action {
 	 * @return array
 	 */
 	public function add_currency_shipping_fields_to_shipping_method_form( array $field ) {
-		if ( $this->is_multicurrency_enabled() && $this->has_additional_currency_defined() ) {
-			$field = $this->add_enable_field( $field );
-			$field = $this->add_currencies_fields( $field );
-		}
+		$field = $this->add_enable_field( $field );
+		$field = $this->add_currencies_fields( $field );
+
 		return $field;
 	}
 
@@ -109,35 +106,6 @@ class AdminHooks implements IWPML_Action {
 		}
 
 		return $field;
-	}
-
-	/**
-	 * Is multicurrency feature enabled in WCML.
-	 *
-	 * @return bool
-	 */
-	private function is_multicurrency_enabled() {
-		return isset( $this->woocommerce_wpml->settings['enable_multi_currency'] )
-			&& $this->woocommerce_wpml->settings['enable_multi_currency'] === WCML_MULTI_CURRENCIES_INDEPENDENT;
-	}
-
-	/**
-	 * Does user defined at least one additional currency in WCML.
-	 *
-	 * @return bool
-	 */
-	private function has_additional_currency_defined() {
-		$available_currencies = $this->wcml_multi_currency->get_currency_codes();
-		if ( is_array( $available_currencies ) ) {
-			$default_currency = $this->wcml_multi_currency->get_default_currency();
-			foreach ( $available_currencies as $key => $currency_code ) {
-				if ( $currency_code === $default_currency ) {
-					unset( $available_currencies[ $key ] );
-				}
-			}
-		}
-
-		return is_array( $available_currencies ) && count( $available_currencies ) > 0;
 	}
 
 	/**
--- classes/Multicurrency/Shipping/AdminHooksFactory.php
+++ classes/Multicurrency/Shipping/AdminHooksFactory.php
@@ -8,6 +8,48 @@ class AdminHooksFactory implements \IWPML_Backend_Action_Loader {
 		/** @var \woocommerce_wpml $woocommerce_wpml */
 		global $woocommerce_wpml;
 
-		return new AdminHooks( $woocommerce_wpml->get_multi_currency(), $woocommerce_wpml );
+		if ( $this->is_multicurrency_enabled() && $this->has_additional_currency_defined() ) {
+			return new AdminHooks( $woocommerce_wpml->get_multi_currency(), $woocommerce_wpml );
+		}
+
+		return null;
+	}
+
+
+
+	/**
+	 * Is multicurrency feature enabled in WCML.
+	 *
+	 * @return bool
+	 */
+	private function is_multicurrency_enabled() {
+		/** @var \woocommerce_wpml $woocommerce_wpml */
+		global $woocommerce_wpml;
+
+		return isset( $woocommerce_wpml->settings['enable_multi_currency'] )
+			&& $woocommerce_wpml->settings['enable_multi_currency'] === WCML_MULTI_CURRENCIES_INDEPENDENT;
+	}
+
+	/**
+	 * Does user defined at least one additional currency in WCML.
+	 *
+	 * @return bool
+	 */
+	private function has_additional_currency_defined() {
+		/** @var \woocommerce_wpml $woocommerce_wpml */
+		global $woocommerce_wpml;
+
+		$available_currencies = $woocommerce_wpml->get_multi_currency()->get_currency_codes();
+
+		if ( is_array( $available_currencies ) ) {
+			$default_currency = $woocommerce_wpml->get_multi_currency()->get_default_currency();
+			foreach ( $available_currencies as $key => $currency_code ) {
+				if ( $currency_code === $default_currency ) {
+					unset( $available_currencies[ $key ] );
+				}
+			}
+		}
+
+		return is_array( $available_currencies ) && count( $available_currencies ) > 0;
 	}
 }
