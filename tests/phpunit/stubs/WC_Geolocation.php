9b58d4148c9f965b40e4f27e319db68daec3a050
Sergey Ratushnuy <sergey.r@icanlocalize.com>
2020-04-07 11:24:00 +0300

    Create a class to get country and currency by Geolocation
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3156

---
+++ tests/phpunit/stubs/WC_Geolocation.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Class WC_Geolocation
+ *
+ * @author OnTheGo Systems
+ */
+class WC_Geolocation {
+
+	public static function get_ip_address() {
+	}
+
+	public static function geolocate_ip( $ip_address = '', $fallback = false, $api_fallback = true ) {
+	}
+}
