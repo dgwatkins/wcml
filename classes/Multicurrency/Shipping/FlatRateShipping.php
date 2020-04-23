0f280fb538b8f163b3711b078ff140d4a4f7bc0e
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2020-04-23 12:03:41 +0200

    [GUI] Interface to provide and save shipping costs in different curencies - Free shipping case.
    - AdminHooks split onto separate classes to handle logic for already existing flat rate and newly supported free shipping
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3162

--- classes/Multicurrency/Shipping/FlatRateShipping.php
+++ classes/Multicurrency/Shipping/FlatRateShipping.php
@@ -9,4 +9,44 @@ class FlatRateShipping implements ShippingMode {
 	public function getMethodId() {
 		return 'flat_rate';
 	}
+
+	public function supportsShippingClasses() {
+		return true;
+	}
+
+	public function getSettingsFormKey( $currencyCode ) {
+		return $this->getCostKey( $currencyCode );
+	}
+
+	public function getMinimalOrderAmountValue( $amount, $shipping, $currency ) {
+		return $amount;
+	}
+
+	public function getShippingCostValue( \WC_Shipping_Rate $rate, $currency ) {
+		if ( ! isset( $rate->cost ) ) {
+			$rate->cost = 0;
+		}
+		if ( isset( $rate->method_id, $rate->instance_id ) ) {
+			if ( $this->isManualPricingEnabled( $rate ) ) {
+				$rate_settings = $this->getWpOption( $rate->method_id, $rate->instance_id );
+				$cost_name = $this->getCostKey( $currency );
+				if ( isset( $rate_settings[ $cost_name ] ) ) {
+					$rate->cost = $rate_settings[ $cost_name ];
+				}
+			}
+		}
+		return $rate->cost;
+	}
+
+	public function isManualPricingEnabled( $instance ) {
+		return self::isEnabled( $this->getWpOption( $instance->method_id, $instance->instance_id ) );
+	}
+
+	private function getWpOption( $method_id, $instance_id ) {
+		if ( null === $this->wpOption ) {
+			$option_name = sprintf( 'woocommerce_%s_%d_settings', $method_id, $instance_id );
+			$this->wpOption = get_option( $option_name );
+		}
+		return $this->wpOption;
+	}
 }
