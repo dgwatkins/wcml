6e250e71783e73061a7c32e5cc18ffc2e0feec60
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2020-04-23 12:03:41 +0200

    [GUI] Interface to provide and save shipping costs in different curencies - Free shipping case.
    - AdminHooks split onto separate classes to handle logic for already existing flat rate and newly supported free shipping
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3162

---
+++ classes/Multicurrency/Shipping/IGeneralShipping.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace WCML\Multicurrency\Shipping;
+
+use WCML_Multi_Currency;
+
+abstract class IGeneralShipping {
+	/**
+	 * Returns shipping method id (shipping option key).
+	 *
+	 * @return string
+	 */
+	public function getMethodId() {
+		return $this->methodId;
+	}
+
+	/**
+	 * Returns field title.
+	 *
+	 * This value is visible on shipping method configuration screen, on the left.
+	 *
+	 * @param string $currencyCode
+	 *
+	 * @return string
+	 */
+	abstract public function getFieldTitle( $currencyCode );
+
+	/**
+	 * Returns field description.
+	 *
+	 * This value is visible on shipping method configuration screen, when mouse over
+	 * the question mark icon, next to field title.
+	 *
+	 * @param string $currencyCode
+	 *
+	 * @return string
+	 */
+	abstract public function getFieldDescription( $currencyCode );
+}
