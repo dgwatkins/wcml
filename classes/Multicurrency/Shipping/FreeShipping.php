cacdc263f302aa8f8edb9a0257c583f71f95379a
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2020-04-29 12:47:32 +0200

    [Calculations] Apply manual shipping rates in different currencies - Free Shipping case
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3165

--- classes/Multicurrency/Shipping/FreeShipping.php
+++ classes/Multicurrency/Shipping/FreeShipping.php
@@ -3,6 +3,8 @@
 namespace WCML\Multicurrency\Shipping;
 
 class FreeShipping implements ShippingMode {
+	use ShippingModeBase;
+
 	public function getFieldTitle( $currencyCode ) {
 		if ( ! is_string( $currencyCode ) ) {
 			$currencyCode = '';
@@ -25,7 +27,39 @@ class FreeShipping implements ShippingMode {
 		return 'free_shipping';
 	}
 
-	public function getCostKey( $currencyCode ) {
+	/**
+	 * Returns minimal amount key for given currency.
+	 *
+	 * @param string $currencyCode Currency code.
+	 *
+	 * @return string
+	 */
+	private function getMinimalOrderAmountKey( $currencyCode ) {
 		return sprintf( 'min_amount_%s', $currencyCode );
+	}
+
+	public function getSettingsFormKey( $currencyCode ) {
+		return $this->getMinimalOrderAmountKey( $currencyCode );
+	}
+
+	public function getMinimalOrderAmountValue( $amount, $shipping, $currency ) {
+		if ( $this->isManualPricingEnabled( $shipping ) ) {
+			$key = $this->getMinimalOrderAmountKey( $currency );
+			if ( isset( $shipping[ $key ] ) ) {
+				$amount = $shipping[ $key ];
+			}
+		}
+		return $amount;
+	}
+
+	public function getShippingCostValue( \WC_Shipping_Rate $rate, $currency ) {
+		if ( ! isset( $rate->cost ) ) {
+			$rate->cost = 0;
+		}
+		return $rate->cost;
+	}
+
+	public function isManualPricingEnabled( $instance ) {
+		return is_array( $instance ) && self::isEnabled( $instance );
 	}
 }
