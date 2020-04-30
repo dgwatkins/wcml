9395c158882ecfbdc6757dfc6762b133f4f88003
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2020-04-30 21:31:17 +0200

    renamed VariableCostModeBase to VariableCost

--- classes/Multicurrency/Shipping/VariableCost.php
+++ classes/Multicurrency/Shipping/VariableCost.php
@@ -3,28 +3,14 @@
 namespace WCML\Multicurrency\Shipping;
 
 trait VariableCost {
-	/**
-	 * @see \WCML\Multicurrency\Shipping\ShippingMode::getFieldTitle
-	 *
-	 * @param string $currencyCode
-	 *
-	 * return string
-	 */
+	private $wpOption = null;
+
 	public function getFieldTitle( $currencyCode ) {
 		return sprintf( esc_html_x( 'Cost in %s',
 			'The label for the field with shipping cost in additional currency. The currency symbol will be added in place of %s specifier.',
 			'woocommerce-multilingual' ), $currencyCode );
 	}
 
-	private $wpOption = null;
-
-	/**
-	 * @see \WCML\Multicurrency\Shipping\ShippingMode::getFieldDescription
-	 *
-	 * @param string $currencyCode
-	 *
-	 * @return string
-	 */
 	public function getFieldDescription( $currencyCode ) {
 		return sprintf( esc_html_x( 'The shipping cost if customer choose %s as a purchase currency.',
 			'The description for the field with shipping cost in additional currency. The currency symbol will be added in place of %s specifier.',
@@ -42,77 +28,38 @@ trait VariableCost {
 		return sprintf( 'cost_%s', $currencyCode );
 	}
 
-	/**
-	 * @see \WCML\Multicurrency\Shipping\ShippingMode::getSettingsFormKey
-	 *
-	 * @param string $currencyCode
-	 *
-	 * @return string
-	 */
 	public function getSettingsFormKey( $currencyCode ) {
 		return $this->getCostKey( $currencyCode );
 	}
 
-	/**
-	 * @see \WCML\Multicurrency\Shipping\ShippingMode::getMinimalOrderAmountValue
-	 *
-	 * @param integer|float|string $amount   The value as saved for original language.
-	 * @param array                $shipping The shipping metadata.
-	 * @param string               $currency Currency code.
-	 *
-	 * @return mixed
-	 */
 	public function getMinimalOrderAmountValue( $amount, $shipping, $currency ) {
 		return $amount;
 	}
 
-	/**
-	 * @see \WCML\Multicurrency\Shipping\ShippingMode::getShippingCostValue
-	 *
-	 * @param \WC_Shipping_Rate $rate
-	 * @param string            $currency
-	 *
-	 * @return int|mixed|string
-	 */
 	public function getShippingCostValue( \WC_Shipping_Rate $rate, $currency ) {
 		if ( ! isset( $rate->cost ) ) {
 			$rate->cost = 0;
 		}
 		if ( isset( $rate->method_id, $rate->instance_id ) ) {
 			if ( $this->isManualPricingEnabled( $rate ) ) {
-				$rateSettings = $this->getWpOption( $rate->method_id, $rate->instance_id );
-				$costName = $this->getCostKey( $currency );
-				if ( isset( $rateSettings[ $costName ] ) ) {
-					$rate->cost = $rateSettings[ $costName ];
+				$rate_settings = $this->getWpOption( $rate->method_id, $rate->instance_id );
+				$cost_name = $this->getCostKey( $currency );
+				if ( isset( $rate_settings[ $cost_name ] ) ) {
+					$rate->cost = $rate_settings[ $cost_name ];
 				}
 			}
 		}
 		return $rate->cost;
 	}
 
-	/**
-	 * @see \WCML\Multicurrency\Shipping\ShippingMode::isManualPricingEnabled
-	 *
-	 * @param \WC_Shipping_Rate $instance
-	 *
-	 * @return mixed
-	 */
 	public function isManualPricingEnabled( $instance ) {
 		return self::isEnabled( $this->getWpOption( $instance->method_id, $instance->instance_id ) );
 	}
 
-	/**
-	 * Returns shipping data from wp_options table.
-	 *
-	 * @param string $methodId
-	 * @param int    $instanceId
-	 *
-	 * @return bool|mixed|void|null
-	 */
-	private function getWpOption( $methodId, $instanceId ) {
+	private function getWpOption( $method_id, $instance_id ) {
 		if ( null === $this->wpOption ) {
-			$optionName = sprintf( 'woocommerce_%s_%d_settings', $methodId, $instanceId );
-			$this->wpOption = get_option( $optionName );
+			$option_name = sprintf( 'woocommerce_%s_%d_settings', $method_id, $instance_id );
+			$this->wpOption = get_option( $option_name );
 		}
 		return $this->wpOption;
 	}
