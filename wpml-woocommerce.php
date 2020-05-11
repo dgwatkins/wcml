5ef639281a64fb2e483b1bcf5b1abb8bcbf2409f
Dimitris Mitsis <dimitris.m@onthegosystems.com>
2020-05-11 17:45:18 +0300

    Updated WP tested up to 5.4.1

--- wpml-woocommerce.php
+++ wpml-woocommerce.php
@@ -7,8 +7,8 @@
  * Author URI: http://www.onthegosystems.com/
  * Text Domain: woocommerce-multilingual
  * Requires at least: 4.7
- * Tested up to: 5.3
- * Version: 4.7.9
+ * Tested up to: 5.4.1
+ * Version: 4.8.0
  * Plugin Slug: woocommerce-multilingual
  * WC requires at least: 3.3.0
  * WC tested up to: 3.8.0
