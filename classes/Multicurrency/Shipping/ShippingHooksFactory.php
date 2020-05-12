c21774b38879c1764bcf1d25034c86e9f12451d1
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2020-05-12 08:44:04 +0200

    [fixup] fixed issue with GUI fields always displaying costs equal to zero

--- classes/Multicurrency/Shipping/ShippingHooksFactory.php
+++ classes/Multicurrency/Shipping/ShippingHooksFactory.php
@@ -18,8 +18,9 @@ class ShippingHooksFactory implements \IWPML_Deferred_Action_Loader, \IWPML_Back
 		) {
 			if ( $this->isShippingPageRequest() || is_ajax() ) {
 				$hooks[] = new AdminHooks( $woocommerce_wpml->get_multi_currency() );
+			} else {
+				$hooks[] =  new FrontEndHooks( $woocommerce_wpml->get_multi_currency() );
 			}
-			$hooks[] =  new FrontEndHooks( $woocommerce_wpml->get_multi_currency() );
 		}
 
 		return $hooks;
