8d99fff311c99989e4784869e02e39709b4b58ef
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2020-05-19 10:46:49 -0300

    FIXUP Use Ramda in Utils.js

--- src/js/multicurrencyOptions/Utils.js
+++ src/js/multicurrencyOptions/Utils.js
@@ -1,4 +1,5 @@
 /* global wcmlMultiCurrency */
+import * as R from 'ramda';
 
 export const capitalize = str => str[0].toUpperCase() + str.slice(1);
 
@@ -10,43 +11,38 @@ export const getSmallFormattedPrice = (currency) => {
     return getFormattedPrice('', '99', '9')(currency);
 };
 
-const getFormattedPrice = (firstPart, secondPart, thirdPart) => (currency) => {
+const getFormattedPrice = (thousands, units, decimals) => (currency) => {
     const currencyData = getCurrencyData(currency.code);
 
-    return getFormatPlaceholder(firstPart, secondPart, currency.position)
+    return getFormatPlaceholder(thousands, units, currency.position)
         .replace(/__SYMBOL__/, currencyData.symbol)
-        .replace(/__THOUSAND_SEP__/, firstPart ? currency.thousand_sep : '')
+        .replace(/__THOUSAND_SEP__/, thousands ? currency.thousand_sep : '')
         .replace(/__DECIMAL_SEP__/, currency.num_decimals > 0 ? currency.decimal_sep : '')
-        .replace(/__DECIMALS_NUMBER__/, thirdPart.repeat(currency.num_decimals));
+        .replace(/__DECIMALS_NUMBER__/, decimals.repeat(currency.num_decimals));
 };
-
-export const getCurrencyLabel = code => getCurrencyData(code).label;
-
-export const getCurrencySymbol = code => getCurrencyData(code).symbol;
 
 const getCurrencyData = code => {
     return wcmlMultiCurrency.allCurrencies.filter(currencyData => currencyData.code === code )[0];
 };
 
-const getFormatPlaceholder = (firstPart, secondPart, position) => {
+export const getCurrencyLabel = R.pipe( getCurrencyData, R.prop('label') );
+
+export const getCurrencySymbol = R.pipe( getCurrencyData, R.prop('symbol') );
+
+const getFormatPlaceholder = (thousands, units, position) => {
     switch(position){
         case 'left':
-            return '__SYMBOL__' + firstPart + '__THOUSAND_SEP__' + secondPart + '__DECIMAL_SEP____DECIMALS_NUMBER__';
+            return '__SYMBOL__' + thousands + '__THOUSAND_SEP__' + units + '__DECIMAL_SEP____DECIMALS_NUMBER__';
         case 'right':
-            return firstPart + '__THOUSAND_SEP__' + secondPart + '__DECIMAL_SEP____DECIMALS_NUMBER____SYMBOL__';
+            return thousands + '__THOUSAND_SEP__' + units + '__DECIMAL_SEP____DECIMALS_NUMBER____SYMBOL__';
         case 'left_space':
-            return '__SYMBOL__ ' + firstPart + '__THOUSAND_SEP__' + secondPart + '__DECIMAL_SEP____DECIMALS_NUMBER__';
+            return '__SYMBOL__ ' + thousands + '__THOUSAND_SEP__' + units + '__DECIMAL_SEP____DECIMALS_NUMBER__';
         case 'right_space':
-            return firstPart + '__THOUSAND_SEP__' + secondPart + '__DECIMAL_SEP____DECIMALS_NUMBER__ __SYMBOL__';
+            return thousands + '__THOUSAND_SEP__' + units + '__DECIMAL_SEP____DECIMALS_NUMBER__ __SYMBOL__';
     }
 };
 
-export const validateRate = (value) => {
-    const isPositive = value => value > 0;
-    const isNumber = value => !isNaN(parseFloat(value)) && isFinite(value);
-
-    return isNumber(value) && isPositive(value);
-};
+export const validateRate = R.allPass( [R.pipe( parseFloat, isNaN, R.not ), isFinite, R.gt( R.__, 0 )] );
 
 export const triggerActiveCurrenciesChange = function(payload) {
     payload.currencyData = getCurrencyData(payload.currency.code);
