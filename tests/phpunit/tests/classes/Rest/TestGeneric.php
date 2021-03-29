5cb300547dee41ddc865e811740dadf428b78af2
pierre Sylvestre <pierre.s@onthegosystems.com>
2021-03-29 09:38:55 -0300

    Set the language globally at different stages of a REST request
    1. On init, we'll set the language if it's passed as a URL query var.
    2. Before running a REST callback, we'll use the `lang` REST parameter,
    3. If not provided, we'll try to use the language of the queried
    product.
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3591

--- tests/phpunit/tests/classes/Rest/TestGeneric.php
+++ tests/phpunit/tests/classes/Rest/TestGeneric.php
@@ -11,58 +11,6 @@ class TestGeneric extends \OTGS_TestCase {
 	/**
 	 * @test
 	 */
-	public function set_wrong_language_for_request() {
-
-		$wrong_language   = 'de';
-
-		$WP_REST_Server = $this->getMockBuilder( 'WP_REST_Server' )->disableOriginalConstructor()->getMock();
-
-		$_GET['lang'] = $wrong_language;
-
-		\WP_Mock::onFilter( 'wpml_language_is_active' )->with( false, $wrong_language )->reply( false );
-
-		Generic::setLanguageForRequest( $WP_REST_Server );
-	}
-
-
-	/**
-	 * @test
-	 */
-	public function set_secondary_language_for_request() {
-
-		$other_language   = 'ro';
-
-		$WP_REST_Server = $this->getMockBuilder( 'WP_REST_Server' )->disableOriginalConstructor()->getMock();
-
-		\WP_Mock::onFilter( 'wpml_language_is_active' )->with( false, $other_language )->reply( true );
-
-		$_GET['lang'] = $other_language;
-		// Make is_request_to_rest_api return true
-		\WP_Mock::userFunction( 'trailingslashit', [
-			'return' => function ( $url ) {
-				return rtrim( $url, '/' ) . '/';
-			},
-		] );
-		\WP_Mock::userFunction( 'rest_get_url_prefix', [
-			'return' => function () {
-				return 'wp-json';
-			},
-		] );
-
-		\WP_Mock::userFunction( 'wpml_switch_language_action', [
-			'args' => [ $other_language ],
-			'return' => true,
-		] );
-
-		$_SERVER['REQUEST_URI'] = 'wp-json/wc/';
-		Generic::setLanguageForRequest( $WP_REST_Server );
-
-		unset( $_SERVER['REQUEST_URI'] );
-	}
-
-	/**
-	 * @test
-	 */
 	function auto_adjust_included_ids() {
 
 		$wp_query = $this->getMockBuilder( 'WP_Query' )
