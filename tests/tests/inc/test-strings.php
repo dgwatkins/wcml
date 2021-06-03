5eb4755391e6f9b55f3ffa10d6270177bb9f7191
David Garcia Watkins <david.gw@onthegosystems.com>
2021-06-03 18:25:48 +0200

    Translate singular labels

--- tests/tests/inc/test-strings.php
+++ tests/tests/inc/test-strings.php
@@ -7,18 +7,34 @@ class Test_WCML_Strings extends WCML_UnitTestCase {
 		parent::setUp();
 	}
 
-	function test_translate_attributes_label_in_wp_taxonomies() {
-		global $wp_taxonomies, $WPML_String_Translation;
+	function test_translate_attribute_labels() {
+		global $wp_taxonomies, $WPML_String_Translation, $woocommerce_wpml;
+
 		$WPML_String_Translation->init_active_languages();
 
 		$label = 'Test attr';
 		$name = wc_attribute_taxonomy_name( $label );
 
-		$taxonomy_data                  = array(
+		add_filter( 'woocommerce_attribute_taxonomies', function() use ( $label ) {
+			$slug = wc_sanitize_taxonomy_name( $label );
+			return [
+				$slug => (object) [
+					'attribute_id'    => '123',
+					'attribute_label' => $label,
+					'attribute_name'  => $slug,
+				],
+			];
+		} );
+
+		add_filter( 'woocommerce_taxonomy_args_' . $name, function( $args ) use ( $woocommerce_wpml, $label ) {
+			return $woocommerce_wpml->strings->translate_attribute_labels( $args, $label );
+		} );
+
+		$taxonomy_data = array(
 			'hierarchical'          => true,
 			'update_count_callback' => '_update_post_term_count',
 			'labels'                => array(
-				'name'              => $label,
+				'name'              => sprintf( __( 'Product %s', 'woocommerce' ), $label ),
 				'singular_name'     => $label,
 				'search_items'      => sprintf( __( 'Search %s', 'woocommerce' ), $label ),
 				'all_items'         => sprintf( __( 'All %s', 'woocommerce' ), $label ),
@@ -46,18 +62,14 @@ class Test_WCML_Strings extends WCML_UnitTestCase {
 			)
 		);
 
-
-		$taxonomy_data['rewrite'] = array(
-			'slug'         => empty( $permalinks['attribute_base'] ) ? '' : trailingslashit( $permalinks['attribute_base'] ) . sanitize_title( $label ),
-			'with_front'   => false,
-			'hierarchical' => true
-		);
-
-		do_action( 'wpml_register_single_string',  'WordPress', 'taxonomy singular name: '.$label, $label );
+		do_action( 'wpml_register_single_string',  'WordPress', 'taxonomy singular name: ' . $label, $label );
+		do_action( 'wpml_register_single_string',  'WordPress', 'taxonomy general name: Product ' . $label, $label );
 
 		$string_id = icl_get_string_id( $label, 'WordPress', 'taxonomy singular name: '.$label );
-
 		icl_add_string_translation( $string_id, 'es', 'Test attr es', ICL_TM_COMPLETE );
+
+		$string_id = icl_get_string_id( $label, 'WordPress', 'taxonomy general name: Product '.$label );
+		icl_add_string_translation( $string_id, 'es', 'Product test attr es', ICL_TM_COMPLETE );
 
 		$WPML_String_Translation->clear_string_filter( 'es' );
 
@@ -65,7 +77,8 @@ class Test_WCML_Strings extends WCML_UnitTestCase {
 		register_taxonomy( $name, apply_filters( "woocommerce_taxonomy_objects_{$name}", array( 'product' ) ), apply_filters( "woocommerce_taxonomy_args_{$name}", $taxonomy_data ) );
 
 		$this->assertTrue( (bool) has_filter('wpml_translate_single_string') );
-		$this->assertEquals( 'Test attr es', $wp_taxonomies[$name]->labels->name );
+		$this->assertEquals( 'Test attr es', $wp_taxonomies[$name]->labels->singular_name );
+		$this->assertEquals( 'Product test attr es', $wp_taxonomies[$name]->labels->name );
 	}
 
 
