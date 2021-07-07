781afb5bcae0845c57c71b12cd8495be0e71360e
pierre Sylvestre <pierre.s@onthegosystems.com>
2021-07-07 16:37:08 -0300

    Prevent PHP notice when adding a currency switcher widget with WP 5.8 blocks or Elementor Page Builder
    First we'll try to get the sidebar `id` from the `args` (legacy), if
    missing we'll fallback to the `$instance`.
    
    Eventuallyt, if no sidebar `id` is provided, the display will fallback to the default currency widget.
    
    I also did some reformatting/improvement in the unit test and removed the
    `runInSeparateProcess` which was not needed.
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3510

--- tests/phpunit/tests/currency-switcher/test-wcml-currency-switcher-widget.php
+++ tests/phpunit/tests/currency-switcher/test-wcml-currency-switcher-widget.php
@@ -1,85 +1,122 @@
 <?php
 
+use tad\FunctionMocker\FunctionMocker;
+
 /**
  * Class Test_WCML_Currency_Switcher_Widget
  * @group currency-switcher
  */
 class Test_WCML_Currency_Switcher_Widget extends OTGS_TestCase {
 
-	function setUp() {
-		parent::setUp();
-	}
-
 	/**
 	 * @test
-	 *
-	 * @runInSeparateProcess
-	 * @preserveGlobalState disabled
 	 */
-	public function widget() {
-		\WP_Mock::wpPassthruFunction( '__' );
-		$widget_args = array(
+	public function widget_with_sidebar_id_from_args() {
+		$sidebar_id = 123;
+
+		\WP_Mock::passthruFunction( '__' );
+		$widget_args = [
 			'before_widget' => 'Text before widget',
 			'after_widget'  => 'Text after widget',
 			'before_title'  => 'Text before title',
 			'after_title'   => 'Text after title',
-			'id'            => mt_rand( 1, 10 ),
-		);
-		$widget_instance = array(
-			'settings' => array(
+			'id'            => $sidebar_id,
+		];
+		$widget_instance = [
+			'settings' => [
 				'widget_title' => 'Widget Title',
-			),
-		);
-		$this->stubs->WP_Widget();
+			],
+		];
+
+		$switcher = 'The currency switcher';
+
+		\WP_Mock::onAction( 'wcml_currency_switcher' )
+			->with( [ 'switcher_id' => $sidebar_id ] )
+			->perform( function() use ( $switcher ) {
+				echo $switcher;
+			} );
+
 		$subject = new WCML_Currency_Switcher_Widget();
 		ob_start();
 		$subject->widget( $widget_args, $widget_instance );
 		$output = ob_get_clean();
 		$expected = $widget_args['before_widget'] . $widget_args['before_title'] . $widget_instance['settings']['widget_title'];
-		$expected .= $widget_args['after_title'] . $widget_args['after_widget'];
+		$expected .= $widget_args['after_title'] . $switcher . $widget_args['after_widget'];
 		$this->assertEquals( $expected, $output );
 	}
 
 	/**
 	 * @test
-	 *
-	 * @runInSeparateProcess
-	 * @preserveGlobalState disabled
 	 */
-	public function update() {
-		$this->stubs->WP_Widget();
-		$sidebar_settings = array( 'settings' => 'sidebar_settings' );
-		$mock             = \Mockery::mock( 'alias:WCML_Currency_Switcher' );
-		$mock->shouldReceive( 'get_settings' )->with( 'sidebar1' )->andReturn( $sidebar_settings );
-		$_POST['sidebar'] = 'sidebar1';
-		$expected_instance = array(
-			'id'       => 'sidebar1',
-			'settings' => $sidebar_settings,
-		);
-		\WP_Mock::wpPassthruFunction( '__' );
+	public function widget_with_sidebar_id_from_instance() {
+		$sidebar_id = 123;
+
+		\WP_Mock::passthruFunction( '__' );
+		$widget_args = [
+			'before_widget' => 'Text before widget',
+			'after_widget'  => 'Text after widget',
+			'before_title'  => 'Text before title',
+			'after_title'   => 'Text after title',
+		];
+		$widget_instance = [
+			'id'       => $sidebar_id,
+			'settings' => [
+				'widget_title' => 'Widget Title',
+			],
+		];
+
+		$switcher = 'The currency switcher';
+
+		\WP_Mock::onAction( 'wcml_currency_switcher' )
+		        ->with( [ 'switcher_id' => $sidebar_id ] )
+		        ->perform( function() use ( $switcher ) {
+			        echo $switcher;
+		        } );
+
 		$subject = new WCML_Currency_Switcher_Widget();
-		$this->assertEquals( $expected_instance, $subject->update( false, false ) );
+		ob_start();
+		$subject->widget( $widget_args, $widget_instance );
+		$output = ob_get_clean();
+		$expected = $widget_args['before_widget'] . $widget_args['before_title'] . $widget_instance['settings']['widget_title'];
+		$expected .= $widget_args['after_title'] . $switcher . $widget_args['after_widget'];
+		$this->assertEquals( $expected, $output );
 	}
 
 	/**
 	 * @test
-	 *
-	 * @runInSeparateProcess
-	 * @preserveGlobalState disabled
+	 */
+	public function update() {
+		$sidebar_settings = [ 'settings' => 'sidebar_settings' ];
+
+		$csGetSettings = FunctionMocker::replace( WCML_Currency_Switcher::class . '::get_settings', $sidebar_settings );
+
+		$_POST['sidebar'] = 'sidebar1';
+		$expected_instance = [
+			'id'       => 'sidebar1',
+			'settings' => $sidebar_settings,
+		];
+		\WP_Mock::passthruFunction( '__' );
+		$subject = new WCML_Currency_Switcher_Widget();
+		$this->assertEquals( $expected_instance, $subject->update( false, false ) );
+
+		$csGetSettings->wasCalledWithOnce( [ 'sidebar1' ] );
+	}
+
+	/**
+	 * @test
 	 */
 	public function form() {
-		$instance = array( 'id' => mt_rand( 1, 20 ) );
+		$instance = [ 'id' => 123 ];
 		$admin_url = 'http://testsite.dev/wp-adminadmin.php?page=wpml-wcml&tab=multi-currency#currency-switcher/' . $instance['id'];
 		$text = 'Customize the currency switcher';
 		$expected = sprintf( '<p><a class="button button-secondary wcml-cs-widgets-edit-link" href="%s"><span class="otgs-ico-edit"></span> %s</a></p>', $admin_url, $text );
-		\WP_Mock::wpFunction( 'admin_url', array(
+		\WP_Mock::userFunction( 'admin_url', [
 			'args'   => 'admin.php?page=wpml-wcml&tab=multi-currency#currency-switcher/' . $instance['id'],
 			'return' => 'http://testsite.dev/wp-adminadmin.php?page=wpml-wcml&tab=multi-currency#currency-switcher/' . $instance['id'],
-		));
-		$this->stubs->WP_Widget();
-		\WP_Mock::wpPassthruFunction( 'esc_html__' );
-		\WP_Mock::wpPassthruFunction( 'esc_url' );
-		\WP_Mock::wpPassthruFunction( '__' );
+		] );
+		\WP_Mock::passthruFunction( 'esc_html__' );
+		\WP_Mock::passthruFunction( 'esc_url' );
+		\WP_Mock::passthruFunction( '__' );
 		$subject = new WCML_Currency_Switcher_Widget();
 		ob_start();
 		$subject->form( $instance );
