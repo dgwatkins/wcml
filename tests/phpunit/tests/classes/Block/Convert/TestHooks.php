e55d62ec3d51120fa2458543d69fce073ae012b9
pierre Sylvestre <pierre.s@onthegosystems.com>
2021-08-23 16:21:45 -0300

    Fix product search form block  widget not working in non-default language
    This block form is stored in the DB with the action URL in the
    default language (differently from the general search block which is
    rendered dynamically).
    
    We also need to add the language hidden field when the the site is using
    "language as a parameter". But this is handled by
    `SitePress::get_search_form_filter`.
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3720

--- tests/phpunit/tests/classes/Block/Convert/TestHooks.php
+++ tests/phpunit/tests/classes/Block/Convert/TestHooks.php
@@ -29,6 +29,7 @@ class TestHooks extends \OTGS_TestCase {
 		FunctionMocker::replace( 'WPML_URL_HTTP_Referer::is_post_edit_page', false );
 
 		\WP_Mock::expectFilterAdded( 'render_block_data', [ $subject, 'filterIdsInBlock' ] );
+		\WP_Mock::expectFilterAdded( 'render_block_woocommerce/product-search', [ $subject, 'filterProductSearchForm' ] );
 		\WP_Mock::expectActionAdded( 'parse_query', [ $subject, 'addCurrentLangToQueryVars' ] );
 		\WP_Mock::expectFilterNotAdded( 'rest_request_before_callbacks', [ $subject, 'useLanguageFrontendRestLang' ], 10, 3 );
 
@@ -45,6 +46,7 @@ class TestHooks extends \OTGS_TestCase {
 		FunctionMocker::replace( '\WPML_URL_HTTP_Referer::is_post_edit_page', true );
 
 		\WP_Mock::expectFilterAdded( 'render_block_data', [ $subject, 'filterIdsInBlock' ] );
+		\WP_Mock::expectFilterAdded( 'render_block_woocommerce/product-search', [ $subject, 'filterProductSearchForm' ] );
 		\WP_Mock::expectActionAdded( 'parse_query', [ $subject, 'addCurrentLangToQueryVars' ] );
 		\WP_Mock::expectFilterNotAdded( 'rest_request_before_callbacks', [ $subject, 'useLanguageFrontendRestLang' ], 10, 3 );
 
@@ -61,6 +63,7 @@ class TestHooks extends \OTGS_TestCase {
 		FunctionMocker::replace( '\WPML_URL_HTTP_Referer::is_post_edit_page', false );
 
 		\WP_Mock::expectFilterAdded( 'render_block_data', [ $subject, 'filterIdsInBlock' ] );
+		\WP_Mock::expectFilterAdded( 'render_block_woocommerce/product-search', [ $subject, 'filterProductSearchForm' ] );
 		\WP_Mock::expectActionAdded( 'parse_query', [ $subject, 'addCurrentLangToQueryVars' ] );
 		\WP_Mock::expectFilterAdded( 'rest_request_before_callbacks', [ $subject, 'useLanguageFrontendRestLang' ], 10, 3 );
 
@@ -207,6 +210,53 @@ class TestHooks extends \OTGS_TestCase {
 		];
 	}
 
+	/**
+	 * @test
+	 * @group wcml-3720
+	 */
+	public function itShouldReplaceSearchFormActionUrl() {
+		$homeUrlInCurrentLanguage = 'https://mysite.com/fr/';
+
+		$getForm = function( $homeUrl ) {
+			return '
+				<div class="wp-block-woocommerce-product-search">
+					<div class="wc-block-product-search">
+						<form role="search" method="get" action="' . $homeUrl . '">
+							<label for="wc-block-product-search-0" class="wc-block-product-search__label">Search (product)</label>
+							<div class="wc-block-product-search__fields">
+								<input type="search" id="wc-block-product-search-0" class="wc-block-product-search__field" placeholder="Search products&#x2026;" name="s"/>
+								<input type="hidden" name="post_type" value="product"/>
+								<button type="submit" class="wc-block-product-search__button" label="Search">
+								<svg aria-hidden="true" role="img" focusable="false" class="dashicon dashicons-arrow-right-alt2" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20">
+									<path d="M6 15l5-5-5-5 1-2 7 7-7 7z"></path>
+								</svg>
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>';
+		};
+
+		$formWithReplacedActionUrl = $getForm( $homeUrlInCurrentLanguage );
+		$expectedForm              = $formWithReplacedActionUrl . '... with language hidden field.';
+
+		\WP_Mock::userFunction( 'home_url' )
+		        ->with( '/' )
+		        ->andReturn( $homeUrlInCurrentLanguage );
+
+		$sitepress = $this->getSitepress();
+		$sitepress->method( 'get_search_form_filter' )
+			->with( $formWithReplacedActionUrl )
+			->willReturn( $expectedForm );
+
+		$subject = $this->getSubject( $sitepress );
+
+		$this->assertSame(
+			$expectedForm,
+			$subject->filterProductSearchForm( $getForm( 'https://any-url.com/' ) )
+		);
+	}
+
 	private function getSubject( $sitepress = null, $frontendRestLang = null ) {
 		$sitepress        = $sitepress ?: $this->getSitepress();
 		$frontendRestLang = $frontendRestLang ?: $this->getFrontendRestLanguage();
@@ -216,7 +266,7 @@ class TestHooks extends \OTGS_TestCase {
 
 	private function getSitepress() {
 		return $this->getMockBuilder( '\SitePress' )
-			->setMethods( [ 'get_object_id', 'get_current_language', 'switch_lang' ] )
+			->setMethods( [ 'get_object_id', 'get_current_language', 'switch_lang', 'get_search_form_filter' ] )
 			->disableOriginalConstructor()->getMock();
 	}
 
