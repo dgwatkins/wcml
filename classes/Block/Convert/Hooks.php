c6732608fa649d19a5342eae658ec5294c7681a4
pierre Sylvestre <pierre.s@onthegosystems.com>
2021-08-23 16:21:45 -0300

    Fix product search form block  widget not working in non-default language
    This block form is stored in the DB with the action URL in the
    default language (differently from the general search block which is
    rendered dynamically).
    
    We also need to add the language hidden field when the the site is using
    "language as a parameter". But this is handled by
    `SitePress::get_search_form_filter`.
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3720

--- classes/Block/Convert/Hooks.php
+++ classes/Block/Convert/Hooks.php
@@ -6,7 +6,8 @@ use IWPML_DIC_Action;
 use IWPML_Frontend_Action;
 use SitePress;
 use WCML\Rest\Frontend\Language;
-use WPML_Cookie;
+use WPML\FP\Just;
+use WPML\FP\Str;
 
 class Hooks implements IWPML_Frontend_Action, IWPML_DIC_Action {
 
@@ -23,6 +24,7 @@ class Hooks implements IWPML_Frontend_Action, IWPML_DIC_Action {
 
 	public function add_hooks() {
 		add_filter( 'render_block_data', [ $this, 'filterIdsInBlock' ] );
+		add_filter( 'render_block_woocommerce/product-search', [ $this, 'filterProductSearchForm' ] );
 		add_action( 'parse_query', [ $this, 'addCurrentLangToQueryVars' ] );
 
 		if ( ! ( is_admin() || \WPML_URL_HTTP_Referer::is_post_edit_page() ) ) {
@@ -73,5 +75,23 @@ class Hooks implements IWPML_Frontend_Action, IWPML_DIC_Action {
 	private function isWcRestRequest( \WP_REST_Request $request ) {
 		return strpos( $request->get_route(), '/wc/blocks/' ) === 0
 			|| strpos( $request->get_route(), '/wc/store/' ) === 0;
+	}
+
+	/**
+	 * @param string $blockContent
+	 *
+	 * @return string
+	 */
+	public function filterProductSearchForm( $blockContent ) {
+		// $replaceActionUrl :: string -> string
+		$replaceActionUrl = Str::pregReplace( '/(<form[^>]*action=")([^"]*)"/', "$1" . home_url( '/' ) . '"' );
+
+		// $addLanguageHiddenField :: string -> string
+		$addLanguageHiddenField = [ $this->sitepress, 'get_search_form_filter' ];
+
+		return Just::of( $blockContent )
+			->map( $replaceActionUrl )
+			->map( $addLanguageHiddenField )
+			->get();
 	}
 }
