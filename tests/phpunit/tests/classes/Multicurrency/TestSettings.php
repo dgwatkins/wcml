9b9e2f5bb65d2e465a2a340ff8d1ebd262435ac8
pierre Sylvestre <pierre.s@onthegosystems.com>
2022-02-07 15:51:08 -0300

    Force currency mode at runtime for standalone mode (rather than persist it)
    This might be safer than persisting the change in case WPML is
    temporarily disabled on the site (we do not loose the setting).
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3858

--- tests/phpunit/tests/classes/Multicurrency/TestSettings.php
+++ tests/phpunit/tests/classes/Multicurrency/TestSettings.php
@@ -19,12 +19,32 @@ class TestSettings extends \OTGS_TestCase {
 	/**
 	 * @test
 	 */
-	public function testMode() {
+	public function testModeInFullMode() {
+		\WP_Mock::userFunction( 'WCML\functions\isStandAlone' )->andReturn( false );
+
 		Settings::setMode( null );
 		$this->assertNull( Settings::getMode() );
 
 		Settings::setMode( Settings::MODE_BY_LANGUAGE );
 		$this->assertTrue( Settings::isModeByLanguage() );
+
+		Settings::setMode( Settings::MODE_BY_LOCATION );
+		$this->assertTrue( Settings::isModeByLocation() );
+	}
+
+	/**
+	 * @test
+	 */
+	public function testModeInStandaloneMode() {
+		\WP_Mock::userFunction( 'WCML\functions\isStandAlone' )->andReturn( true );
+
+		Settings::setMode( null );
+		$this->assertNull( Settings::getMode() );
+
+		// Force mode to location at runtime.
+		Settings::setMode( Settings::MODE_BY_LANGUAGE );
+		$this->assertFalse( Settings::isModeByLanguage() );
+		$this->assertTrue( Settings::isModeByLocation() );
 
 		Settings::setMode( Settings::MODE_BY_LOCATION );
 		$this->assertTrue( Settings::isModeByLocation() );
