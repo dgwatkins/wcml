dc7ac5c7cff303f0979413f75a39aece134a0833
pierre Sylvestre <pierre.s@onthegosystems.com>
2022-02-10 16:12:54 -0300

    Remove `WCML_Cart_Sync_Warnings` in the Standalone mode
    We display this warning only when one of the compatible addons is
    active. And we agreed the Standalone mode will not offer full
    compatibility with addons.
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3890

--- classes/class-woocommerce-wpml.php
+++ classes/class-woocommerce-wpml.php
@@ -305,8 +305,7 @@ class woocommerce_wpml {
 		$this->gateways = new WCML_WC_Gateways( $this, $sitepress );
 		$this->gateways->add_hooks();
 		$this->cart->add_hooks(); // object is instantiated before.
-		$this->cart_sync_warnings = new WCML_Cart_Sync_Warnings( $this, $sitepress );
-		$payment_method_filter    = new WCML_Payment_Method_Filter();
+		$payment_method_filter = new WCML_Payment_Method_Filter();
 		$payment_method_filter->add_hooks();
 
 		WCML_Install::initialize( $this, $sitepress );
