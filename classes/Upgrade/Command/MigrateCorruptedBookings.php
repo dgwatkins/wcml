2852f03693e9d3cc2edbe8055cbe6f1efddce79d
Andreas Panagiotopoulos <andreas.p@onthegosystems.com>
2022-03-01 19:07:19 +0200

    fixup

--- classes/Upgrade/Command/MigrateCorruptedBookings.php
+++ classes/Upgrade/Command/MigrateCorruptedBookings.php
@@ -17,38 +17,15 @@ class MigrateCorruptedBookings implements Command {
 	 * @return bool
 	 */
 	public function run() {
-		$offset = 0;
 		
-		if ( ! get_option( '_wcml_5_0_0_migrate_corrupted_booking_required' ) ) {
-			add_option( '_wcml_5_0_0_migrate_corrupted_booking_required' );
-		} else {
-			$offset = get_option( '_wcml_5_0_0_migrate_corrupted_booking_required' );
-		}
+		$this->wpdb->query(
+			"UPDATE {$this->wpdb->postmeta} p
+ INNER JOIN {$this->wpdb->postmeta} b
+ ON p.post_id = b.post_id AND p.meta_key = '_booking_product_id'
+   SET p.meta_value = ''
+   WHERE b.meta_key = '_booking_duplicate_of' AND b.meta_value IS NOT NULL"
+		);
 		
-		$returnBookingDuplicates = $this->wpdb->get_results(
-			$this->wpdb->prepare(
-				"SELECT post_id FROM {$this->wpdb->postmeta}
-				WHERE meta_key = '_booking_duplicate_of'
-				  AND meta_value IS NOT NULL LIMIT %d,20", $offset ) );
-		
-		if ( empty( $returnBookingDuplicates ) ) {
-			delete_option( '_wcml_5_0_0_migrate_corrupted_booking_required' );
-			
-			return true;
-		}
-		
-		$updateCorrectData = function ( $bookingDuplicate ) {
-			$this->wpdb->query( $this->wpdb->prepare(
-				"UPDATE {$this->wpdb->postmeta} SET meta_value = ''
-				WHERE meta_key = '_booking_product_id'
-				  AND post_id = %d", $bookingDuplicate->post_id ) );
-		};
-		
-		wpml_collect( $returnBookingDuplicates )
-			->each( $updateCorrectData );
-		
-		update_option( '_wcml_5_0_0_migrate_corrupted_booking_required', $offset += 20 );
-		
-		return false;
+		return true;
 	}
 }
