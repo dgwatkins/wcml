7d91c7fb3d0b62ec6df308ddebafdaa0ec9f19b9
pierre Sylvestre <pierre.s@onthegosystems.com>
2022-03-14 10:16:25 -0300

    Add notice to call for review in standalone mode
    Changes:
    - We had the notice classes from Core, but we missed to init the hooks.
    - We were missing to add the icons styles.
    - Updated the wpml/wpml-dependencies in addons to include some missing
    code from Core about wpml admin notices.
    - Add the extra DIC config from Core (which is missing in the
    standalone mode).
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3898

--- addons/load-standalone-dependencies.php
+++ addons/load-standalone-dependencies.php
@@ -13,12 +13,36 @@
 define( 'WCML_WPML_DEPENDENCY_URL', WCML_PLUGIN_URL . '/addons/vendor/wpml/wpml-dependencies/lib' );
 
 require_once WCML_PLUGIN_PATH . '/addons/vendor/autoload.php';
+require_once WCML_PLUGIN_PATH . '/addons/vendor/wpml/wpml-dependencies/lib/missing-functions.php';
+
+$setPartialDicConfigFromCore = function() {
+	\WPML\Container\share( \WCML\StandAlone\Container\Config::getSharedInstances() );
+	\WPML\Container\share( \WCML\StandAlone\Container\Config::getSharedClasses() );
+	\WPML\Container\alias( \WCML\StandAlone\Container\Config::getAliases() );
+	\WPML\Container\delegate( \WCML\StandAlone\Container\Config::getDelegated() );
+};
+
+$setPartialDicConfigFromCore();
 
 if ( is_admin() ) {
 	require_once WCML_PLUGIN_PATH . '/addons/vendor/wpml/wpml-dependencies/lib/inc/icl-admin-notifier.php';
 
-	$vendor_root_url = WCML_PLUGIN_URL . '/addons/vendor';
-	require_once WCML_PLUGIN_PATH . '/addons/vendor/otgs/icons/loader.php';
+	$loadOtgsIconsStyles = function() {
+		$vendor_root_url = WCML_PLUGIN_URL . '/addons/vendor';
+		require_once WCML_PLUGIN_PATH . '/addons/vendor/otgs/icons/loader.php';
+
+		add_action( 'admin_init', function() {
+			wp_enqueue_style( OTGS_ASSETS_ICONS_STYLES );
+		}, PHP_INT_MAX );
+	};
+
+	$loadOtgsIconsStyles();
 
 	( new \WCML\StandAlone\DependencyAssets( WCML_WPML_DEPENDENCY_URL ) )->add_hooks();
+
+	wpml_get_admin_notices(); // Initialize the notices hooks
+
+	( new WPML_Action_Filter_Loader() )->load( [
+		\WPML\Notices\DismissNotices::class,
+	] );
 }
