b2e508064ab00e8b8c8e6c06aaefe55d46f3b174
pierre Sylvestre <pierre.s@onthegosystems.com>
2022-03-18 13:48:49 -0300

    Set version to 5.0.0-beta1
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3335

--- wpml-woocommerce.php
+++ wpml-woocommerce.php
@@ -8,7 +8,7 @@
  * Text Domain: woocommerce-multilingual
  * Requires at least: 4.7
  * Tested up to: 5.9
- * Version: 5.0.0
+ * Version: 5.0.0-beta1
  * Plugin Slug: woocommerce-multilingual
  * WC requires at least: 3.9
  * WC tested up to: 6.3
@@ -36,7 +36,7 @@ if ( ! $wpml_php_version_check->is_ok() ) {
 	return;
 }
 
-define( 'WCML_VERSION', '5.0.0' );
+define( 'WCML_VERSION', '5.0.0-beta1' );
 define( 'WCML_PLUGIN_PATH', dirname( __FILE__ ) );
 define( 'WCML_PLUGIN_FOLDER', basename( WCML_PLUGIN_PATH ) );
 define( 'WCML_LOCALE_PATH', WCML_PLUGIN_PATH . '/locale' );
