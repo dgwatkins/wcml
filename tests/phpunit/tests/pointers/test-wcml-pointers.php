e1b2fb4d0c303730a2c32f9bb1b28834c317a0e3
pierre Sylvestre <pierre.s@onthegosystems.com>
2022-04-08 16:38:09 -0300

    Some code modernization in `Test_WCML_Pointers`
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3998

--- tests/phpunit/tests/pointers/test-wcml-pointers.php
+++ tests/phpunit/tests/pointers/test-wcml-pointers.php
@@ -8,6 +8,7 @@ use tad\FunctionMocker\FunctionMocker;
  * @group  wcml-1986
  */
 class Test_WCML_Pointers extends OTGS_TestCase {
+
 	public function setUp() {
 		parent::setUp();
 		WP_Mock::userFunction( 'WCML\functions\isStandAlone' )->andReturn( false );
@@ -23,7 +24,7 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 	 */
 	function it_add_hooks() {
 		$subject = new WCML_Pointers();
-		WP_Mock::expectActionAdded( 'admin_head', array( $subject, 'setup' ) );
+		WP_Mock::expectActionAdded( 'admin_head', [ $subject, 'setup' ] );
 		$subject->add_hooks();
 	}
 
@@ -31,15 +32,15 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 	 * @test
 	 */
 	function it_does_nothing_on_setup() {
-		WP_Mock::wpFunction( 'get_current_screen', array( 'return' => null ) );
-		WP_Mock::wpFunction( 'wp_register_style', array( 'times' => 0 ) );
+		WP_Mock::userFunction( 'get_current_screen', [ 'return' => null ] );
+		WP_Mock::userFunction( 'wp_register_style', [ 'times' => 0 ] );
 
 		$subject = new WCML_Pointers();
 
-		$this->expectActionAdded( 'admin_footer', array( $subject, 'add_products_translation_link' ), 100, 1, 0 );
-		$this->expectActionAdded( 'admin_footer', array( $subject, 'add_shipping_classes_translation_link' ), 10, 1, 0 );
-		$this->expectActionAdded( 'woocommerce_general_settings', array( $subject, 'add_multi_currency_link' ), 10, 1, 0 );
-		$this->expectActionAdded( 'woocommerce_account_settings', array( $subject, 'add_endpoints_translation_link' ), 10, 1, 0 );
+		$this->expectActionAdded( 'admin_footer', [ $subject, 'add_products_translation_link' ], 100, 1, 0 );
+		$this->expectActionAdded( 'admin_footer', [ $subject, 'add_shipping_classes_translation_link' ], 10, 1, 0 );
+		$this->expectActionAdded( 'woocommerce_general_settings', [ $subject, 'add_multi_currency_link' ], 10, 1, 0 );
+		$this->expectActionAdded( 'woocommerce_account_settings', [ $subject, 'add_endpoints_translation_link' ], 10, 1, 0 );
 
 		$subject->setup();
 	}
@@ -48,20 +49,19 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 	 * @test
 	 */
 	function it_does_add_products_translation_link_action() {
-		$current_screen     = new stdClass();
-		$current_screen->id = 'edit-product';
-		WP_Mock::wpFunction( 'get_current_screen', array( 'return' => $current_screen ) );
-		WP_Mock::wpFunction( 'wp_register_style', array( 'times' => 1, 'args' => array( 'wcml-pointers', WCML_PLUGIN_URL . '/res/css/wcml-pointers.css' ) ) );
+		$current_screen = (object) [ 'id' => 'edit-product' ];
+		WP_Mock::userFunction( 'get_current_screen', [ 'return' => $current_screen ] );
+		WP_Mock::userFunction( 'wp_register_style', [ 'times' => 1, 'args' => [ 'wcml-pointers', WCML_PLUGIN_URL . '/res/css/wcml-pointers.css' ] ] );
 
 		$subject = new WCML_Pointers();
 		
 		FunctionMocker::replace( 'WCML_Capabilities::canManageWcml', true );
 
-		WP_Mock::expectActionAdded( 'admin_footer', array( $subject, 'add_products_translation_link' ), 100 );
+		WP_Mock::expectActionAdded( 'admin_footer', [ $subject, 'add_products_translation_link' ], 100 );
 
-		$this->expectActionAdded( 'admin_footer', array( $subject, 'add_shipping_classes_translation_link' ), 10, 1, 0 );
-		$this->expectActionAdded( 'woocommerce_general_settings', array( $subject, 'add_multi_currency_link' ), 10, 1, 0 );
-		$this->expectActionAdded( 'woocommerce_account_settings', array( $subject, 'add_endpoints_translation_link' ), 10, 1, 0 );
+		$this->expectActionAdded( 'admin_footer', [ $subject, 'add_shipping_classes_translation_link' ], 10, 1, 0 );
+		$this->expectActionAdded( 'woocommerce_general_settings', [ $subject, 'add_multi_currency_link' ], 10, 1, 0 );
+		$this->expectActionAdded( 'woocommerce_account_settings', [ $subject, 'add_endpoints_translation_link' ], 10, 1, 0 );
 
 		$subject->setup();
 	}
@@ -70,10 +70,9 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 	 * @test
 	 */
 	function it_does_add_shipping_classes_translation_link_action() {
-		$current_screen     = new stdClass();
-		$current_screen->id = 'woocommerce_page_wc-settings';
-		WP_Mock::wpFunction( 'get_current_screen', array( 'return' => $current_screen ) );
-		WP_Mock::wpFunction( 'wp_register_style', array( 'times' => 1, 'args' => array( 'wcml-pointers', WCML_PLUGIN_URL . '/res/css/wcml-pointers.css' ) ) );
+		$current_screen = (object) [ 'id' => 'woocommerce_page_wc-settings' ];
+		WP_Mock::userFunction( 'get_current_screen', [ 'return' => $current_screen ] );
+		WP_Mock::userFunction( 'wp_register_style', [ 'times' => 1, 'args' => [ 'wcml-pointers', WCML_PLUGIN_URL . '/res/css/wcml-pointers.css' ] ] );
 
 		$_GET['tab']     = 'shipping';
 		$_GET['section'] = 'classes';
@@ -82,12 +81,12 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 		
 		FunctionMocker::replace( 'WCML_Capabilities::canManageWcml', true );
 
-		$this->expectActionAdded( 'admin_footer', array( $subject, 'add_products_translation_link' ), 100, 1, 0 );
+		$this->expectActionAdded( 'admin_footer', [ $subject, 'add_products_translation_link' ], 100, 1, 0 );
 
-		WP_Mock::expectActionAdded( 'admin_footer', array( $subject, 'add_shipping_classes_translation_link' ) );
+		WP_Mock::expectActionAdded( 'admin_footer', [ $subject, 'add_shipping_classes_translation_link' ] );
 
-		$this->expectActionAdded( 'woocommerce_general_settings', array( $subject, 'add_multi_currency_link' ), 10, 1, 0 );
-		$this->expectActionAdded( 'woocommerce_account_settings', array( $subject, 'add_endpoints_translation_link' ), 10, 1, 0 );
+		$this->expectActionAdded( 'woocommerce_general_settings', [ $subject, 'add_multi_currency_link' ], 10, 1, 0 );
+		$this->expectActionAdded( 'woocommerce_account_settings', [ $subject, 'add_endpoints_translation_link' ], 10, 1, 0 );
 
 		$subject->setup();
 	}
@@ -96,10 +95,9 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 	 * @test
 	 */
 	function it_does_add_multi_currency_link_in_general_tab() {
-		$current_screen     = new stdClass();
-		$current_screen->id = 'woocommerce_page_wc-settings';
-		WP_Mock::wpFunction( 'get_current_screen', array( 'return' => $current_screen ) );
-		WP_Mock::wpFunction( 'wp_register_style', array( 'times' => 1, 'args' => array( 'wcml-pointers', WCML_PLUGIN_URL . '/res/css/wcml-pointers.css' ) ) );
+		$current_screen = (object) [ 'id' => 'woocommerce_page_wc-settings' ];
+		WP_Mock::userFunction( 'get_current_screen', [ 'return' => $current_screen ] );
+		WP_Mock::userFunction( 'wp_register_style', [ 'times' => 1, 'args' => [ 'wcml-pointers', WCML_PLUGIN_URL . '/res/css/wcml-pointers.css' ] ] );
 
 		$_GET['tab'] = 'general';
 
@@ -107,12 +105,12 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 		
 		FunctionMocker::replace( 'WCML_Capabilities::canManageWcml', true );
 
-		$this->expectActionAdded( 'admin_footer', array( $subject, 'add_products_translation_link' ), 100, 1, 0 );
-		$this->expectActionAdded( 'admin_footer', array( $subject, 'add_shipping_classes_translation_link' ), 10, 1, 0 );
+		$this->expectActionAdded( 'admin_footer', [ $subject, 'add_products_translation_link' ], 100, 1, 0 );
+		$this->expectActionAdded( 'admin_footer', [ $subject, 'add_shipping_classes_translation_link' ], 10, 1, 0 );
 
-		WP_Mock::expectFilterAdded( 'woocommerce_general_settings', array( $subject, 'add_multi_currency_link' ) );
+		WP_Mock::expectFilterAdded( 'woocommerce_general_settings', [ $subject, 'add_multi_currency_link' ] );
 
-		$this->expectActionAdded( 'woocommerce_account_settings', array( $subject, 'add_endpoints_translation_link' ), 10, 1, 0 );
+		$this->expectActionAdded( 'woocommerce_account_settings', [ $subject, 'add_endpoints_translation_link' ], 10, 1, 0 );
 
 		$subject->setup();
 	}
@@ -121,21 +119,20 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 	 * @test
 	 */
 	function it_does_add_multi_currency_link_in_undefined_tab() {
-		$current_screen     = new stdClass();
-		$current_screen->id = 'woocommerce_page_wc-settings';
-		WP_Mock::wpFunction( 'get_current_screen', array( 'return' => $current_screen ) );
-		WP_Mock::wpFunction( 'wp_register_style', array( 'times' => 1, 'args' => array( 'wcml-pointers', WCML_PLUGIN_URL . '/res/css/wcml-pointers.css' ) ) );
+		$current_screen = (object) [ 'id' => 'woocommerce_page_wc-settings' ];
+		WP_Mock::userFunction( 'get_current_screen', [ 'return' => $current_screen ] );
+		WP_Mock::userFunction( 'wp_register_style', [ 'times' => 1, 'args' => [ 'wcml-pointers', WCML_PLUGIN_URL . '/res/css/wcml-pointers.css' ] ] );
 
 		$subject = new WCML_Pointers();
 		
 		FunctionMocker::replace( 'WCML_Capabilities::canManageWcml', true );
 
-		$this->expectActionAdded( 'admin_footer', array( $subject, 'add_products_translation_link' ), 100, 1, 0 );
-		$this->expectActionAdded( 'admin_footer', array( $subject, 'add_shipping_classes_translation_link' ), 10, 1, 0 );
+		$this->expectActionAdded( 'admin_footer', [ $subject, 'add_products_translation_link' ], 100, 1, 0 );
+		$this->expectActionAdded( 'admin_footer', [ $subject, 'add_shipping_classes_translation_link' ], 10, 1, 0 );
 
-		WP_Mock::expectFilterAdded( 'woocommerce_general_settings', array( $subject, 'add_multi_currency_link' ) );
+		WP_Mock::expectFilterAdded( 'woocommerce_general_settings', [ $subject, 'add_multi_currency_link' ] );
 
-		$this->expectActionAdded( 'woocommerce_account_settings', array( $subject, 'add_endpoints_translation_link' ), 10, 1, 0 );
+		$this->expectActionAdded( 'woocommerce_account_settings', [ $subject, 'add_endpoints_translation_link' ], 10, 1, 0 );
 
 		$subject->setup();
 	}
@@ -144,10 +141,9 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 	 * @test
 	 */
 	function it_does_add_endpoints_translation_link_in_account_tab() {
-		$current_screen     = new stdClass();
-		$current_screen->id = 'woocommerce_page_wc-settings';
-		WP_Mock::wpFunction( 'get_current_screen', array( 'return' => $current_screen ) );
-		WP_Mock::wpFunction( 'wp_register_style', array( 'times' => 1, 'args' => array( 'wcml-pointers', WCML_PLUGIN_URL . '/res/css/wcml-pointers.css' ) ) );
+		$current_screen = (object) [ 'id' => 'woocommerce_page_wc-settings' ];
+		WP_Mock::userFunction( 'get_current_screen', [ 'return' => $current_screen ] );
+		WP_Mock::userFunction( 'wp_register_style', [ 'times' => 1, 'args' => [ 'wcml-pointers', WCML_PLUGIN_URL . '/res/css/wcml-pointers.css' ] ] );
 
 		$_GET['tab'] = 'account';
 
@@ -155,9 +151,9 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 		
 		FunctionMocker::replace( 'WCML_Capabilities::canManageWcml', true );
 
-		$this->expectActionAdded( 'admin_footer', array( $subject, 'add_products_translation_link' ), 100, 1, 0 );
-		$this->expectActionAdded( 'admin_footer', array( $subject, 'add_shipping_classes_translation_link' ), 10, 1, 0 );
-		$this->expectActionAdded( 'woocommerce_general_settings', array( $subject, 'add_multi_currency_link' ), 10, 1, 0 );
+		$this->expectActionAdded( 'admin_footer', [ $subject, 'add_products_translation_link' ], 100, 1, 0 );
+		$this->expectActionAdded( 'admin_footer', [ $subject, 'add_shipping_classes_translation_link' ], 10, 1, 0 );
+		$this->expectActionAdded( 'woocommerce_general_settings', [ $subject, 'add_multi_currency_link' ], 10, 1, 0 );
 
 		$subject->setup();
 	}
@@ -166,8 +162,7 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 	 * @test
 	 */
 	function it_does_add_endpoints_translation_link_in_advanced_tab() {
-		$current_screen     = new stdClass();
-		$current_screen->id = 'woocommerce_page_wc-settings';
+		$current_screen = (object) [ 'id' => 'woocommerce_page_wc-settings' ];
 		WP_Mock::userFunction( 'get_current_screen', [ 'return' => $current_screen ] );
 		WP_Mock::userFunction( 'wp_register_style', [ 'times' => 1, 'args' => [ 'wcml-pointers', WCML_PLUGIN_URL . '/res/css/wcml-pointers.css' ] ] );
 
@@ -191,11 +186,11 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 	 * @param string $callback
 	 */
 	function it_adds_link_with_jQuery( $name, $admin_url, $callback ) {
-		$link = rand_str( 10 );
+		$link = 'some_link';
 
-		WP_Mock::wpFunction( 'admin_url', array( 'times' => 1, 'return' => $link, 'args' => array( $admin_url ) ) );
-		WP_Mock::wpFunction( '__', array( 'times' => 1, 'return' => $name, 'args' => array( $name, 'woocommerce-multilingual' ) ) );
-		WP_Mock::wpFunction( 'wp_enqueue_style', array( 'times' => 1, 'args' => array( 'wcml-pointers' ) ) );
+		WP_Mock::userFunction( 'admin_url', [ 'times' => 1, 'return' => $link, 'args' => [ $admin_url ] ] );
+		WP_Mock::userFunction( '__', [ 'times' => 1, 'return' => $name, 'args' => [ $name, 'woocommerce-multilingual' ] ] );
+		WP_Mock::userFunction( 'wp_enqueue_style', [ 'times' => 1, 'args' => [ 'wcml-pointers' ] ] );
 
 		$expected = $this->get_expected_script( $link, $name, $callback );
 
@@ -211,10 +206,10 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 	}
 
 	function dp_links() {
-		return array(
-			array( 'Translate WooCommerce products', 'admin.php?page=wpml-wcml', 'add_products_translation_link' ),
-			array( 'Translate shipping classes', 'admin.php?page=wpml-wcml&tab=product_shipping_class', 'add_shipping_classes_translation_link' ),
-		);
+		return [
+			[ 'Translate WooCommerce products', 'admin.php?page=wpml-wcml', 'add_products_translation_link' ],
+			[ 'Translate shipping classes', 'admin.php?page=wpml-wcml&tab=product_shipping_class', 'add_shipping_classes_translation_link' ],
+		];
 	}
 
 	private function get_expected_script( $link, $name, $callback ) {
@@ -236,11 +231,11 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 	 * @param string $callback
 	 */
 	function it_does_not_add_the_multi_currency_link( $name, $admin_url, $setting_id, $callback ) {
-		$link = rand_str( 10 );
+		$link = 'some_link';
 
-		WP_Mock::wpFunction( 'admin_url', array( 'times' => 1, 'return' => $link, 'args' => array( $admin_url ) ) );
-		WP_Mock::wpFunction( '__', array( 'times' => 1, 'return' => $name, 'args' => array( $name, 'woocommerce-multilingual' ) ) );
-		WP_Mock::wpFunction( 'wp_enqueue_style', array( 'times' => 1, 'args' => array( 'wcml-pointers' ) ) );
+		WP_Mock::userFunction( 'admin_url', [ 'times' => 1, 'return' => $link, 'args' => [ $admin_url ] ] );
+		WP_Mock::userFunction( '__', [ 'times' => 1, 'return' => $name, 'args' => [ $name, 'woocommerce-multilingual' ] ] );
+		WP_Mock::userFunction( 'wp_enqueue_style', [ 'times' => 1, 'args' => [ 'wcml-pointers' ] ] );
 
 		$settings = [
 			[
@@ -272,20 +267,20 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 	 * @param string $callback
 	 */
 	function it_adds_link_through_settings( $name, $admin_url, $setting_id, $callback ) {
-		$link = rand_str( 10 );
+		$link = 'some_link';
 
-		WP_Mock::wpFunction( 'admin_url', array( 'times' => 1, 'return' => $link, 'args' => array( $admin_url ) ) );
-		WP_Mock::wpFunction( '__', array( 'times' => 1, 'return' => $name, 'args' => array( $name, 'woocommerce-multilingual' ) ) );
-		WP_Mock::wpFunction( 'wp_enqueue_style', array( 'times' => 1, 'args' => array( 'wcml-pointers' ) ) );
+		WP_Mock::userFunction( 'admin_url', [ 'times' => 1, 'return' => $link, 'args' => [ $admin_url ] ] );
+		WP_Mock::userFunction( '__', [ 'times' => 1, 'return' => $name, 'args' => [ $name, 'woocommerce-multilingual' ] ] );
+		WP_Mock::userFunction( 'wp_enqueue_style', [ 'times' => 1, 'args' => [ 'wcml-pointers' ] ] );
 
-		$tested_key         = rand_str( 5 );
-		$tested_description = rand_str( 100 );
-		$settings           = array(
-			$tested_key => array(
+		$tested_key         = 'the_tested_key';
+		$tested_description = 'the_tested_description';
+		$settings           = [
+			$tested_key => [
 				'id'   => $setting_id,
 				'desc' => $tested_description,
-			),
-		);
+			],
+		];
 
 		$expected                        = $settings;
 		$expected[ $tested_key ]['desc'] = $this->get_description( $link, $name, $tested_description, $callback );
@@ -296,17 +291,17 @@ class Test_WCML_Pointers extends OTGS_TestCase {
 	}
 
 	function dp_descriptions() {
-		return array(
-			array( 'Configure multicurrency for multilingual sites', 'admin.php?page=wpml-wcml&tab=multi-currency', 'pricing_options', 'add_multi_currency_link' ),
-			array( 'Translate endpoints', 'admin.php?page=wpml-wcml&tab=slugs', 'checkout_endpoint_options', 'add_endpoints_translation_link' ),
-		);
+		return [
+			[ 'Configure multicurrency for multilingual sites', 'admin.php?page=wpml-wcml&tab=multi-currency', 'pricing_options', 'add_multi_currency_link' ],
+			[ 'Translate endpoints', 'admin.php?page=wpml-wcml&tab=slugs', 'checkout_endpoint_options', 'add_endpoints_translation_link' ],
+		];
 	}
 
 	private function get_description( $link, $name, $current_description, $callback ) {
-		$descriptions = array(
+		$descriptions = [
 			'add_multi_currency_link'        => '<a class="button button-small button-wpml wcml-pointer-multi_currency" href="' . $link . '">' . $name . '</a><br />' . $current_description,
 			'add_endpoints_translation_link' => '<a class="button button-small button-wpml wcml-pointer-endpoints_translation" href="' . $link . '">' . $name . '</a><br />' . $current_description,
-		);
+		];
 
 		return $descriptions[ $callback ];
 	}
