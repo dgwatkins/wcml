2d9efb0cdae8a79ddbe15fc74f102063ecc09659
pierre Sylvestre <pierre.s@onthegosystems.com>
2022-04-25 16:45:35 -0300

    WIP better FP lock

--- classes/Multicurrency/Transient/Hooks.php
+++ classes/Multicurrency/Transient/Hooks.php
@@ -3,7 +3,6 @@
 namespace WCML\Multicurrency\Transient;
 
 use WCML\MultiCurrency\Settings as McSettings;
-use WCML\Utilities\InMemoryLock;
 use WPML\FP\Fns;
 use WPML\FP\Str;
 use WPML\LIB\WP\Hooks as WpHooks;
@@ -22,28 +21,27 @@ class Hooks {
 			return $getKeyWithCurrency( getClientCurrency() );
 		};
 
-		$lock = new InMemoryLock();
+		$setTransient = function( $value ) use ( $key, $getKeyWithClientCurrency ) {
+			delete_transient( $key );
+			return set_transient( $getKeyWithClientCurrency(), $value );
+		};
+
+		$deleteTransient = function() use ( $getKeyWithCurrency ) {
+			foreach ( McSettings::getActiveCurrencyCodes() as $code ) {
+				delete_transient( $getKeyWithCurrency( $code ) );
+			}
+		};
+
+		$withLock = Fns::withNamedLock( __CLASS__ . "_$key", Fns::identity() );
 
 		WpHooks::onFilter( 'pre_transient_' . $key )
 			->then( compose( 'get_transient', $getKeyWithClientCurrency ) );
 
 		WpHooks::onAction( 'set_transient_' . $key )
-			->then( spreadArgs( function( $value ) use ( $key, $getKeyWithClientCurrency, &$lock ) {
-				$lock->lock();
-				delete_transient( $key );
-				$lock->release();
-
-				return set_transient( $getKeyWithClientCurrency(), $value );
-			} ) );
+			->then( $withLock( $setTransient ) );
 
 		WpHooks::onAction( 'delete_transient_' . $key )
-			->then( function() use ( $getKeyWithCurrency, &$lock ) {
-				if ( ! $lock->isLocked() ) {
-					foreach ( McSettings::getActiveCurrencyCodes() as $code ) {
-						delete_transient( $getKeyWithCurrency( $code ) );
-					}
-				}
-			} );
+			->then( spreadArgs( $withLock( $deleteTransient ) ) );
 	}
 
 }
