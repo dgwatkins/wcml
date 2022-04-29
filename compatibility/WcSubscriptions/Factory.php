5124db5d683361c4da89b6c7f4b339c92dcf1ae1
pierre Sylvestre <pierre.s@onthegosystems.com>
2022-04-29 12:02:16 -0300

    Load MC-related code for WC Subscriptions in standalone mode
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-4024

--- compatibility/WcSubscriptions/Factory.php
+++ compatibility/WcSubscriptions/Factory.php
@@ -3,15 +3,32 @@
 namespace WCML\Compatibility\WcSubscriptions;
 
 use WCML\Compatibility\ComponentFactory;
+use WCML\StandAlone\IStandAloneAction;
 use WCML_WC_Subscriptions;
 use function WCML\functions\getWooCommerceWpml;
+use function WCML\functions\isStandAlone;
 
-class Factory extends ComponentFactory {
+/**
+ * @see https://www.woocommerce.com/products/woocommerce-subscriptions/
+ */
+class Factory extends ComponentFactory implements IStandAloneAction {
 
 	/**
 	 * @inheritDoc
 	 */
 	public function create() {
-		return new WCML_WC_Subscriptions( getWooCommerceWpml(), self::getWpdb() );
+		$hooks = [
+			new SharedHooks(),
+		];
+
+		if ( wcml_is_multi_currency_on() ) {
+			$hooks[] = new MulticurrencyHooks( getWooCommerceWpml(), self::getWpdb() );
+		}
+
+		if ( ! isStandAlone() ) {
+			$hooks[] = new WCML_WC_Subscriptions( getWooCommerceWpml(), self::getWpdb() );
+		}
+
+		return $hooks;
 	}
 }
--- tests/phpunit/tests/classes/compatibility/WcSubscriptions/TestMulticurrencyHooks.php
+++ tests/phpunit/tests/classes/compatibility/WcSubscriptions/TestMulticurrencyHooks.php
@@ -1,123 +1,66 @@
 <?php
 
+namespace WCML\Compatibility\WcSubscriptions;
+
+use WC_Product_Subscription_Variation;
+use woocommerce_wpml;
+use WP_Mock;
+use wpdb;
 use function WPML\FP\tap as tap;
 
-class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
+/**
+ * @group compatibility
+ * @group wc-subscriptions
+ */
+class TestMulticurrencyHooks extends \OTGS_TestCase {
 
 	/** @var woocommerce_wpml */
 	private $woocommerce_wpml;
+
 	/** @var wpdb */
 	private $wpdb;
 
-	public function setUp()
-	{
+	public function setUp() {
 		parent::setUp();
 
-		$this->woocommerce_wpml = $this->getMockBuilder('woocommerce_wpml')
+		$this->woocommerce_wpml = $this->getMockBuilder( woocommerce_wpml::class )
 		                               ->disableOriginalConstructor()
 		                               ->getMock();
 
 		$this->wpdb = $this->stubs->wpdb();
 	}
 
-	private function get_subject(){
-		return new WCML_WC_Subscriptions( $this->woocommerce_wpml, $this->wpdb );
+	private function get_subject() {
+		return new MulticurrencyHooks( $this->woocommerce_wpml, $this->wpdb );
 	}
 
 	/**
 	 * @test
 	 */
-	public function actions_on_init_front_end() {
-		\WP_Mock::wpFunction(
-			'is_admin',
-			array(
-				'return' => false,
-			)
-		);
+	public function actions_on_init_frontend() {
+		WP_Mock::userFunction( 'is_admin' )->andReturn( false );
 
-		\WP_Mock::wpFunction(
-			'is_cart',
-			[
-				'return' => false,
-				'times' => 1,
-			]
-		);
+		WP_Mock::userFunction( 'is_cart', [
+			'return' => false,
+			'times' => 1,
+		] );
 
-		\WP_Mock::wpFunction(
-			'is_checkout',
-			[
-				'return' => false,
-				'times' => 1,
-			]
-		);
-
-		\WP_Mock::wpFunction(
-			'wcml_is_multi_currency_on',
-			array(
-				'return' => true,
-			)
-		);
+		WP_Mock::userFunction( 'is_checkout', [
+			'return' => false,
+			'times' => 1,
+		] );
 
 		$subject = $this->get_subject();
-
-		WP_Mock::expectActionAdded( 'woocommerce_before_calculate_totals', array( $subject, 'maybe_backup_recurring_carts' ), 1, 1 );
-		WP_Mock::expectActionAdded( 'woocommerce_after_calculate_totals', array( $subject, 'maybe_restore_recurring_carts' ), 200, 1 );
-
-		WP_Mock::expectFilterAdded( 'wcs_get_subscription', array( $subject, 'filter_subscription_items' ) );
 
 		WP_Mock::expectFilterAdded( 'wcs_switch_proration_new_price_per_day', tap( [ $subject, 'set_prorating_price' ] ) );
 
-		\WP_Mock::wpFunction( 'wcs_cart_contains_resubscribe', array(
-			'return' => false
-		) );
-
 		$subject->init();
-	}
-
-	/**
-	 * @test
-	 */
-	public function actions_on_init_back_end() {
-		\WP_Mock::wpFunction(
-			'is_admin',
-			array(
-				'return' => true,
-			)
-		);
-
-		$subject = $this->get_subject();
-
-		WP_Mock::expectActionNotAdded( 'woocommerce_before_calculate_totals', array( $subject, 'maybe_backup_recurring_carts' ), 1, 1 );
-		WP_Mock::expectActionNotAdded( 'woocommerce_after_calculate_totals', array( $subject, 'maybe_restore_recurring_carts' ), 200, 1 );
-
-		$subject->init();
-	}
-
-	/**
-	 * @test
-	 */
-	public function maybe_backup_recurring_carts() {
-
-		$subject = $this->get_subject();
-
-		$cart                  = new stdClass();
-		$cart->recurring_carts = rand_str();
-
-		$other_cart                  = new stdClass();
-		$other_cart->recurring_carts = false;
-
-		$subject->maybe_backup_recurring_carts( $cart );
-
-		$subject->maybe_restore_recurring_carts( $other_cart );
-
-		$this->assertEquals( $cart->recurring_carts, $other_cart->recurring_carts );
 	}
 
 	/**
 	 * @test
 	 */
 	public function woocommerce_subscription_price_from_custom_price() {
-
 		$price           = rand( 1, 100 );
 		$expected_price  = rand( 1, 100 );
 		$product_id      = rand( 1, 100 );
@@ -132,17 +75,17 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 
 		$this->woocommerce_wpml->multi_currency = $this->getMockBuilder( 'WCML_Multi_Currency' )
 		                                               ->disableOriginalConstructor()
-		                                               ->setMethods( array( 'get_client_currency' ) )
+		                                               ->setMethods( [ 'get_client_currency' ] )
 		                                               ->getMock();
 
 		$this->woocommerce_wpml->multi_currency->method( 'get_client_currency' )->willReturn( $client_currency );
 
-		\WP_Mock::wpFunction( 'get_post_meta', array(
-			'args'   => array( $product_id, '_wcml_custom_prices_status', true ),
+		WP_Mock::userFunction( 'get_post_meta', [
+			'args'   => [ $product_id, '_wcml_custom_prices_status', true ],
 			'return' => true
-		) );
+		] );
 
-		\WP_Mock::wpFunction( 'get_post_meta', array(
+		WP_Mock::userFunction( 'get_post_meta', array(
 			'args'   => array( $product_id, '_price_' . $client_currency, true ),
 			'return' => $expected_price
 		) );
@@ -158,24 +101,23 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 	 * @test
 	 */
 	public function woocommerce_subscription_price_from_auto_converted_price() {
+		$price          = 27;
+		$expected_price = 49;
+		$product_id     = 12;
 
-		$price          = rand( 1, 100 );
-		$expected_price = rand( 1, 100 );
-		$product_id = 12;
-
-		$product = $this->getMockBuilder( 'WC_Product_Subscription_Variation' )
+		$product = $this->getMockBuilder( WC_Product_Subscription_Variation::class )
 		                ->disableOriginalConstructor()
-		                ->setMethods( ['get_id'] )
+		                ->setMethods( [ 'get_id' ] )
 		                ->getMock();
 
 		$product->method( 'get_id' )->willReturn( $product_id );
 
-		\WP_Mock::wpFunction( 'get_post_meta', array(
-			'args'   => array( $product_id, '_wcml_custom_prices_status', true ),
+		WP_Mock::userFunction( 'get_post_meta', [
+			'args'   => [ $product_id, '_wcml_custom_prices_status', true ],
 			'return' => false
-		) );
+		] );
 
-		\WP_Mock::onFilter( 'wcml_raw_price_amount' )
+		WP_Mock::onFilter( 'wcml_raw_price_amount' )
 		        ->with( $price )
 		        ->reply( $expected_price );
 
@@ -220,7 +162,7 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 
 		$wcs_cart_contains_resubscribe['subscription_resubscribe']['subscription_id'] = $subscription_id;
 
-		\WP_Mock::wpFunction(
+		WP_Mock::wpFunction(
 			'is_cart',
 			[
 				'return' => true,
@@ -228,7 +170,7 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 			]
 		);
 
-		\WP_Mock::wpFunction( 'wcs_cart_contains_resubscribe', [
+		WP_Mock::wpFunction( 'wcs_cart_contains_resubscribe', [
 			'return' => $wcs_cart_contains_resubscribe,
 			'times' => 1,
 		] );
@@ -245,7 +187,7 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 
 		$early_renewal_cart_item['subscription_renewal']['subscription_renewal_early'] = $subscription_id;
 
-		\WP_Mock::wpFunction(
+		WP_Mock::wpFunction(
 			'is_cart',
 			[
 				'return' => true,
@@ -253,12 +195,12 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 			]
 		);
 
-		\WP_Mock::wpFunction( 'wcs_cart_contains_early_renewal', [
+		WP_Mock::wpFunction( 'wcs_cart_contains_early_renewal', [
 			'return' => $early_renewal_cart_item,
 			'times' => 1,
 		] );
 
-		\WP_Mock::wpFunction( 'wcs_cart_contains_resubscribe', [
+		WP_Mock::wpFunction( 'wcs_cart_contains_resubscribe', [
 			'return' => false,
 			'times' => 1,
 		] );
@@ -271,12 +213,12 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 		$subscription_currency = 'EUR';
 		$client_currency       = 'USD';
 
-		\WP_Mock::wpFunction( 'get_post_meta', [
+		WP_Mock::wpFunction( 'get_post_meta', [
 			'args'   => [ $subscription_id, '_order_currency', true ],
 			'return' => $subscription_currency
 		] );
 
-		\WP_Mock::wpFunction( 'wcml_is_multi_currency_on', [
+		WP_Mock::wpFunction( 'wcml_is_multi_currency_on', [
 			'return' => true
 		] );
 
@@ -296,11 +238,11 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 	 */
 	public function does_not_force_client_currency_for_resubscribe_subscription_when_MC_is_off() {
 
-		\WP_Mock::wpFunction( 'wcml_is_multi_currency_on', array(
+		WP_Mock::wpFunction( 'wcml_is_multi_currency_on', array(
 			'return' => false
 		) );
 
-		\WP_Mock::wpFunction( 'get_post_meta', array(
+		WP_Mock::wpFunction( 'get_post_meta', array(
 			'times' => 0
 		) );
 
@@ -325,7 +267,7 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 
 		$product->method( 'get_id' )->willReturn( $product_id );
 
-		\WP_Mock::wpFunction( 'wcml_is_multi_currency_on', array(
+		WP_Mock::wpFunction( 'wcml_is_multi_currency_on', array(
 			'return' => true
 		) );
 
@@ -337,23 +279,23 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 		$this->woocommerce_wpml->multi_currency->method( 'get_client_currency' )->willReturn( $client_currency );
 
 		$this->woocommerce_wpml->products = $this->getMockBuilder( 'WCML_Multi_Currency' )
-		                                               ->disableOriginalConstructor()
-		                                               ->setMethods( array( 'get_original_product_id' ) )
-		                                               ->getMock();
+		                                         ->disableOriginalConstructor()
+		                                         ->setMethods( array( 'get_original_product_id' ) )
+		                                         ->getMock();
 
 		$this->woocommerce_wpml->products->method( 'get_original_product_id' )->willReturn( $product_id );
 
-		\WP_Mock::wpFunction( 'get_post_meta', array(
+		WP_Mock::wpFunction( 'get_post_meta', array(
 			'args'   => array( $product_id, '_wcml_custom_prices_status', true ),
 			'return' => true
 		) );
 
-		\WP_Mock::wpFunction( 'get_post_meta', array(
+		WP_Mock::wpFunction( 'get_post_meta', array(
 			'args'   => array( $product_id, '_subscription_sign_up_fee_' . $client_currency, true ),
 			'return' => $expected_subscription_sign_up_fee
 		) );
 
-		\WP_Mock::wpFunction( 'wcml_get_woocommerce_currency_option', array(
+		WP_Mock::wpFunction( 'wcml_get_woocommerce_currency_option', array(
 			'return' => rand_str()
 		) );
 
@@ -382,7 +324,7 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 		$product->method( 'get_id' )->willReturn( 1 );
 		$product->method( 'get_meta' )->with( '_min_price_variation_id', true )->willReturn( $product_id );
 
-		\WP_Mock::wpFunction( 'wcml_is_multi_currency_on', array(
+		WP_Mock::wpFunction( 'wcml_is_multi_currency_on', array(
 			'return' => true
 		) );
 
@@ -394,23 +336,23 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 		$this->woocommerce_wpml->multi_currency->method( 'get_client_currency' )->willReturn( $client_currency );
 
 		$this->woocommerce_wpml->products = $this->getMockBuilder( 'WCML_Multi_Currency' )
-		                                               ->disableOriginalConstructor()
-		                                               ->setMethods( array( 'get_original_product_id' ) )
-		                                               ->getMock();
+		                                         ->disableOriginalConstructor()
+		                                         ->setMethods( array( 'get_original_product_id' ) )
+		                                         ->getMock();
 
 		$this->woocommerce_wpml->products->method( 'get_original_product_id' )->willReturn( $product_id );
 
-		\WP_Mock::wpFunction( 'get_post_meta', array(
+		WP_Mock::wpFunction( 'get_post_meta', array(
 			'args'   => array( $product_id, '_wcml_custom_prices_status', true ),
 			'return' => true
 		) );
 
-		\WP_Mock::wpFunction( 'get_post_meta', array(
+		WP_Mock::wpFunction( 'get_post_meta', array(
 			'args'   => array( $product_id, '_subscription_sign_up_fee_' . $client_currency, true ),
 			'return' => $expected_subscription_sign_up_fee
 		) );
 
-		\WP_Mock::wpFunction( 'wcml_get_woocommerce_currency_option', array(
+		WP_Mock::wpFunction( 'wcml_get_woocommerce_currency_option', array(
 			'return' => rand_str()
 		) );
 
@@ -451,20 +393,20 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 
 		$product->method( 'get_id' )->willReturn( $product_id );
 
-		\WP_Mock::wpFunction( 'wcml_is_multi_currency_on', array(
+		WP_Mock::wpFunction( 'wcml_is_multi_currency_on', array(
 			'return' => true
 		) );
 
-		\WP_Mock::wpFunction( 'get_post_meta', array(
+		WP_Mock::wpFunction( 'get_post_meta', array(
 			'args'   => array( $product_id, '_wcml_custom_prices_status', true ),
 			'return' => false
 		) );
 
-		\WP_Mock::wpFunction( 'wcml_get_woocommerce_currency_option', array(
+		WP_Mock::wpFunction( 'wcml_get_woocommerce_currency_option', array(
 			'return' => rand_str()
 		) );
 
-		\WP_Mock::onFilter( 'wcml_raw_price_amount' )
+		WP_Mock::onFilter( 'wcml_raw_price_amount' )
 		        ->with( $subscription_sign_up_fee )
 		        ->reply( $expected_subscription_sign_up_fee );
 
@@ -491,7 +433,7 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 
 		$this->woocommerce_wpml->multi_currency->method( 'get_client_currency' )->willReturn( $client_currency );
 
-		\WP_Mock::wpFunction( 'wcml_get_woocommerce_currency_option', array(
+		WP_Mock::wpFunction( 'wcml_get_woocommerce_currency_option', array(
 			'return' => $client_currency
 		) );
 
@@ -512,22 +454,22 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 		$client_currency          = 'EUR';
 
 		$product = $this->getMockBuilder( 'WC_Product' )
-			->disableOriginalConstructor()
-			->getMock();
+		                ->disableOriginalConstructor()
+		                ->getMock();
 
-		\WP_Mock::userFunction( 'wcml_is_multi_currency_on', [
+		WP_Mock::userFunction( 'wcml_is_multi_currency_on', [
 			'return' => true,
 		] );
 
 		$this->woocommerce_wpml->multi_currency = $this->getMockBuilder( 'WCML_Multi_Currency' )
-			->disableOriginalConstructor()
-			->setMethods( [ 'get_client_currency' ] )
-			->getMock();
+		                                               ->disableOriginalConstructor()
+		                                               ->setMethods( [ 'get_client_currency' ] )
+		                                               ->getMock();
 
 		$this->woocommerce_wpml->multi_currency->method( 'get_client_currency' )
-			->willReturn( $client_currency );
+		                                       ->willReturn( $client_currency );
 
-		\WP_Mock::userFunction( 'wcml_get_woocommerce_currency_option', [
+		WP_Mock::userFunction( 'wcml_get_woocommerce_currency_option', [
 			'return' => 'USD',
 		] );
 
@@ -537,18 +479,6 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 		$filtered_subscription_sign_up_fee = $subject->subscriptions_product_sign_up_fee_filter( $subscription_sign_up_fee, $product );
 
 		$this->assertSame( $subscription_sign_up_fee, $filtered_subscription_sign_up_fee );
-	}
-
-	/**
-	 * @test
-	 */
-	public function it_set_allowed_variations_types_in_xliff() {
-
-		$subject = $this->get_subject();
-
-		$filtered_types = $subject->set_allowed_variations_types_in_xliff( array() );
-
-		$this->assertSame( array( 'variable-subscription', 'subscription_variation' ), $filtered_types );
 	}
 
 	/**
@@ -601,40 +531,4 @@ class Test_WCML_WC_Subscriptions extends OTGS_TestCase {
 
 		unset( $_POST[ '_custom_variation_subscription_sign_up_fee' ] );
 	}
-
-	/**
-	 * @test
-	 */
-	public function it_should_filter_subscription_items() {
-
-		$items = [];
-
-		$subscription = $this->getMockBuilder( 'WC_Subscription' )
-		                     ->disableOriginalConstructor()
-		                     ->setMethods( [ 'get_items' ] )
-		                     ->getMock();
-		$subscription->expects( $this->once() )->method( 'get_items' )->willReturn( $items );
-		$this->woocommerce_wpml->orders = $this->getMockBuilder( 'WCML_Orders' )
-		                                       ->disableOriginalConstructor()
-		                                       ->setMethods( [ 'adjust_order_item_in_language' ] )
-		                                       ->getMock();
-
-		$this->woocommerce_wpml->orders->expects( $this->once() )->method( 'adjust_order_item_in_language' )->with( $items )->willReturn( true );
-		$subject = $this->get_subject();
-
-		$subject->filter_subscription_items( $subscription );
-
-	}
-
-	/**
-	 * @test
-	 */
-	public function it_should_not_filter_subscription_items_if_false() {
-
-		$subject = $this->get_subject();
-
-		$this->assertFalse( $subject->filter_subscription_items( false ) );
-
-	}
-
 }
