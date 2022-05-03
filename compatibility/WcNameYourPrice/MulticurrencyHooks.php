471af76483d5395f922f72ea42fba163b252281e
David Garcia Watkins <david.gw@onthegosystems.com>
2022-05-03 13:25:29 +0000

    Make name-your-price filters run also in standalone mode
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-4023

--- compatibility/WcNameYourPrice/MulticurrencyHooks.php
+++ compatibility/WcNameYourPrice/MulticurrencyHooks.php
@@ -1,67 +1,45 @@
 <?php
 
+namespace WCML\Compatibility\WcNameYourPrice;
+
+use WC_Name_Your_Price_Compatibility;
+
+use function WCML\functions\getClientCurrency;
+
 /**
  * WooCommerce Name Your Price compatibility.
  *
  * @version 4.12.2
  */
-class WCML_WC_Name_Your_Price implements \IWPML_Action {
-
-	/**
-	 * @var SitePress
-	 */
-	public $sitepress;
-	/**
-	 * @var woocommerce_wpml
-	 */
-	private $woocommerce_wpml;
-	/**
-	 * @var int
-	 */
-	private $multi_currency_mode;
-
-	/**
-	 * WCML_WC_Name_Your_Price constructor.
-	 *
-	 * @param SitePress        $sitepress
-	 * @param woocommerce_wpml $woocommerce_wpml
-	 */
-	public function __construct( SitePress $sitepress, woocommerce_wpml $woocommerce_wpml ) {
-		$this->sitepress           = $sitepress;
-		$this->woocommerce_wpml    = $woocommerce_wpml;
-		$this->multi_currency_mode = $woocommerce_wpml->settings['enable_multi_currency'];
-	}
+class MulticurrencyHooks implements \IWPML_Action {
 
 	public function add_hooks() {
 		// Price filters.
 		if ( ! is_admin() ) {
 			if ( is_callable( [ 'WC_Name_Your_Price_Compatibility', 'is_nyp_gte' ] ) && WC_Name_Your_Price_Compatibility::is_nyp_gte( '3.0' ) ) {
-				add_filter( 'wc_nyp_raw_suggested_price', [ $this, 'product_price_filter' ], 10, 2 );
-				add_filter( 'wc_nyp_raw_minimum_price', [ $this, 'product_price_filter' ], 10, 2 );
-				add_filter( 'wc_nyp_raw_maximum_price', [ $this, 'product_price_filter' ], 10, 2 );
+				add_filter( 'wc_nyp_raw_suggested_price', [ $this, 'product_price_filter' ] );
+				add_filter( 'wc_nyp_raw_minimum_price', [ $this, 'product_price_filter' ] );
+				add_filter( 'wc_nyp_raw_maximum_price', [ $this, 'product_price_filter' ] );
 			} else {
-				add_filter( 'woocommerce_raw_suggested_price', [ $this, 'product_price_filter' ], 10, 2 );
-				add_filter( 'woocommerce_raw_minimum_price', [ $this, 'product_price_filter' ], 10, 2 );
-				add_filter( 'woocommerce_raw_maximum_price', [ $this, 'product_price_filter' ], 10, 2 );
+				add_filter( 'woocommerce_raw_suggested_price', [ $this, 'product_price_filter' ] );
+				add_filter( 'woocommerce_raw_minimum_price', [ $this, 'product_price_filter' ] );
+				add_filter( 'woocommerce_raw_maximum_price', [ $this, 'product_price_filter' ] );
 			}
 		}
 
 		// Cart currency switching.
-		if ( $this->is_multi_currency_on() ) {
-			add_filter( 'woocommerce_add_cart_item_data', [ $this, 'add_initial_currency' ] );
-			add_filter( 'woocommerce_get_cart_item_from_session', [ $this, 'filter_woocommerce_get_cart_item_from_session' ], 20, 2 );
-		}
-	}
-
-	public function product_price_filter( $price, $product ) {
-		return apply_filters( 'wcml_raw_price_amount', $price );
+		add_filter( 'woocommerce_add_cart_item_data', [ $this, 'add_initial_currency' ] );
+		add_filter( 'woocommerce_get_cart_item_from_session', [ $this, 'filter_woocommerce_get_cart_item_from_session' ], 20, 2 );
 	}
 
 	/**
-	 * @return bool
+	 * @param float|int    $price
+	 * @param string|false $currency
+	 *
+	 * @return float|int
 	 */
-	private function is_multi_currency_on() {
-		return $this->multi_currency_mode === $this->sitepress->get_wp_api()->constant( 'WCML_MULTI_CURRENCIES_INDEPENDENT' );
+	public function product_price_filter( $price, $currency = false ) {
+		return apply_filters( 'wcml_raw_price_amount', $price, $currency );
 	}
 
 	/**
@@ -71,7 +49,6 @@ class WCML_WC_Name_Your_Price implements \IWPML_Action {
 	 *
 	 * @return array
 	 * @since 4.12.2
-	 *
 	 */
 	public function add_initial_currency( $cart_item_data ) {
 
@@ -107,14 +84,14 @@ class WCML_WC_Name_Your_Price implements \IWPML_Action {
 			$session_data['nyp_original'] = $values['nyp_original'];
 		}
 
-		$current_currency = $this->woocommerce_wpml->multi_currency->get_client_currency();
+		$current_currency = getClientCurrency();
 
 		if ( isset( $session_data['nyp_currency'] ) && $session_data['nyp_currency'] !== $current_currency ) {
 
 			// Product is in the 'data'.
 			$product = $session_data['data'];
 
-			$price_in_current_currency = $this->woocommerce_wpml->multi_currency->prices->convert_raw_woocommerce_price( $session_data['nyp'], $current_currency );
+			$price_in_current_currency = $this->product_price_filter( $session_data['nyp'], $current_currency );
 
 			// Set to price in current currency.
 			$product->set_price( $price_in_current_currency );
@@ -125,7 +102,6 @@ class WCML_WC_Name_Your_Price implements \IWPML_Action {
 			if ( $product->is_type( [ 'subscription', 'subscription_variation' ] ) ) {
 				$product->update_meta_data( '_subscription_price', $price_in_current_currency );
 			}
-
 		}
 
 		return $session_data;
