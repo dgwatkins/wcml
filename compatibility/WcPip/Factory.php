d7fbc89991eb6f43c47261dcda878cd12f08ab8c
pierre Sylvestre <pierre.s@onthegosystems.com>
2022-05-04 10:43:44 -0300

    Load MC-related code for Print and Invoices Packing Lists (PIP) in standalone mode
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-4017

--- compatibility/WcPip/Factory.php
+++ compatibility/WcPip/Factory.php
@@ -3,17 +3,29 @@
 namespace WCML\Compatibility\WcPip;
 
 use WCML\Compatibility\ComponentFactory;
+use WCML\StandAlone\IStandAloneAction;
 use WCML_Pip;
+use function WCML\functions\isStandAlone;
 
 /**
  * @see https://woocommerce.com/products/print-invoices-packing-lists/
  */
-class Factory extends ComponentFactory {
+class Factory extends ComponentFactory implements IStandAloneAction {
 
 	/**
 	 * @inheritDoc
 	 */
 	public function create() {
-		return new WCML_Pip();
+		$hooks = [];
+
+		if ( wcml_is_multi_currency_on() ) {
+			$hooks[] = new MulticurrencyHooks();
+		}
+
+		if ( ! isStandAlone() ) {
+			$hooks[] = new WCML_Pip();
+		}
+
+		return $hooks;
 	}
 }
