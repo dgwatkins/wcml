b0730a6a838295d41cef9cac004478178312a181
David Garcia Watkins <david.gw@onthegosystems.com>
2022-09-27 12:35:29 +0200

    Move the condition from WHERE to JOIN so that missing records are included
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-4179/Analytics-orders-page-does-not-show-orders-when-they-are-missing-the-wpmllanguage-postmeta-key

--- classes/Reports/Orders/Hooks.php
+++ classes/Reports/Orders/Hooks.php
@@ -22,9 +22,6 @@ class Hooks implements \IWPML_Backend_Action, \IWPML_REST_Action, \IWPML_DIC_Act
 		add_filter( "woocommerce_analytics_clauses_join_orders_subquery", [ $this, 'addJoin' ] );
 		add_filter( "woocommerce_analytics_clauses_join_orders_stats_total", [ $this, 'addJoin' ] );
 		add_filter( "woocommerce_analytics_clauses_join_orders_stats_interval", [ $this, 'addJoin' ] );
-		add_filter( "woocommerce_analytics_clauses_where_orders_subquery", [ $this, 'addWhere' ] );
-		add_filter( "woocommerce_analytics_clauses_where_orders_stats_total", [ $this, 'addWhere' ] );
-		add_filter( "woocommerce_analytics_clauses_where_orders_stats_interval", [ $this, 'addWhere' ] );
 		add_filter( "woocommerce_analytics_clauses_select_orders_subquery", [ $this, 'addSelect' ] );
 		add_filter( "woocommerce_analytics_clauses_select_orders_stats_total", [ $this, 'addSelect' ] );
 		add_filter( "woocommerce_analytics_clauses_select_orders_stats_interval", [ $this, 'addSelect' ] );
@@ -51,18 +48,13 @@ class Hooks implements \IWPML_Backend_Action, \IWPML_REST_Action, \IWPML_DIC_Act
 	 * @return array
 	 */
 	public function addJoin( $clauses ) {
-		$clauses[] = "LEFT JOIN {$this->wpdb->postmeta} wcml_language_postmeta ON wcml_language_postmeta.post_id = IF( {$this->wpdb->prefix}wc_order_stats.parent_id = 0, {$this->wpdb->prefix}wc_order_stats.order_id, {$this->wpdb->prefix}wc_order_stats.parent_id )";
-
-		return $clauses;
-	}
-
-	/**
-	 * @param array $clauses
-	 *
-	 * @return array
-	 */
-	public function addWhere( $clauses ) {
-		$clauses[] = "AND wcml_language_postmeta.meta_key = 'wpml_language'";
+		$clauses[] = "LEFT JOIN {$this->wpdb->postmeta} wcml_language_postmeta
+			ON wcml_language_postmeta.post_id = IF(
+				{$this->wpdb->prefix}wc_order_stats.parent_id = 0,
+				{$this->wpdb->prefix}wc_order_stats.order_id,
+				{$this->wpdb->prefix}wc_order_stats.parent_id
+			)
+			AND wcml_language_postmeta.meta_key = 'wpml_language'";
 
 		return $clauses;
 	}
