44b6ae992773894e6daeabc583f36da1ce5df96d
David Garcia Watkins <david.gw@onthegosystems.com>
2022-10-27 13:40:50 +0000

    Show all reviews in Store API, when configured to do so
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-4078/All-Reviews-block-widget-is-not-displaying-reviews-from-all-languages

--- tests/phpunit/tests/classes/Rest/Store/TestHooks.php
+++ tests/phpunit/tests/classes/Rest/Store/TestHooks.php
@@ -2,6 +2,8 @@
 
 namespace WCML\Rest\Store;
 
+use WPML\FP\Fns;
+
 /**
  * @group rest/store
  */
@@ -33,11 +35,17 @@ class TestHooks extends \OTGS_TestCase {
 			'return' => true,
 		] );
 
+		\WP_Mock::userFunction( 'WCML\functions\getSetting', [
+			'args'   => [ 'reviews_in_all_languages', false ],
+			'return' => false,
+		] );
+
 		$_SERVER['REQUEST_URI'] = 'wp-json/wc/store/cart';
 
 		$subject = $this->getSubject();
 
 		\WP_Mock::expectActionAdded( 'init', [ $subject, 'initializeSession' ], 0 );
+		\WP_Mock::expectActionNotAdded( 'wpml_is_comment_query_filtered', Fns::always( false ) );
 
 		$subject->add_hooks();
 	}
@@ -48,13 +56,59 @@ class TestHooks extends \OTGS_TestCase {
 	 * @return void 
 	 */
 	public function itDoesNotAddHooksWhenNoMulticurrency() {
+		\WP_Mock::userFunction( 'rest_get_url_prefix', [
+			'return' => 'wp-json/',
+		] );
+
+		\WP_Mock::userFunction( 'trailingslashit', [
+			'return' => function ( $url ) {
+				return rtrim( $url, '/' ) . '/';
+			},
+		] );
+
 		\WP_Mock::userFunction( 'wcml_is_multi_currency_on', [
 			'return' => false,
 		] );
 
+		$_SERVER['REQUEST_URI'] = 'wp-json/wc/store/cart';
+
 		$subject = $this->getSubject();
 
 		\WP_Mock::expectActionNotAdded( 'init', [ $subject, 'initializeSession' ], 0 );
+
+		$subject->add_hooks();
+	}
+
+	/**
+	 * @test
+	 *
+	 * @return void 
+	 */
+	public function itAddsReviewsHookWhenConfigured() {
+		\WP_Mock::userFunction( 'rest_get_url_prefix', [
+			'return' => 'wp-json/',
+		] );
+
+		\WP_Mock::userFunction( 'trailingslashit', [
+			'return' => function ( $url ) {
+				return rtrim( $url, '/' ) . '/';
+			},
+		] );
+
+		\WP_Mock::userFunction( 'wcml_is_multi_currency_on', [
+			'return' => false,
+		] );
+
+		\WP_Mock::userFunction( 'WCML\functions\getSetting', [
+			'args'   => [ 'reviews_in_all_languages', false ],
+			'return' => true,
+		] );
+
+		$_SERVER['REQUEST_URI'] = 'wp-json/wc/store/cart';
+
+		$subject = $this->getSubject();
+
+		\WP_Mock::expectActionAdded( 'wpml_is_comment_query_filtered', Fns::always( false ) );
 
 		$subject->add_hooks();
 	}
