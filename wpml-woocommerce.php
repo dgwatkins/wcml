613363affe06854866d1a5fabdf2880902732be7
Pierre Sylvestre <pierre.s@onthegosystems.com>
2023-07-11 09:06:37 -0300

    [wcml-4161] Raise 'WC tested up to:' to 7.9

--- wpml-woocommerce.php
+++ wpml-woocommerce.php
@@ -11,7 +11,7 @@
  * Version: 5.2.0
  * Plugin Slug: woocommerce-multilingual
  * WC requires at least: 3.9
- * WC tested up to: 7.8
+ * WC tested up to: 7.9
  *
  * @package WCML
  * @author  OnTheGoSystems
