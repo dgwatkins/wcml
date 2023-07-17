be0bcdd9a9bcf9ff8f356e589ba0eb19e228fcd2
David Garcia Watkins <david.gw@onthegosystems.com>
2023-07-17 13:53:17 +0200

    [Blocks] Translate gateway settings in PaymentRegistry
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-4275/WooCommerce-checkout-block-does-not-translate-some-values

---
+++ classes/PaymentGateways/BlockHooks.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace WCML\PaymentGateways;
+
+use Automattic\WooCommerce\Blocks\Payments\PaymentMethodRegistry;
+use WPML\LIB\WP\Hooks;
+
+use function WPML\FP\spreadArgs;
+
+class BlockHooks implements \IWPML_Frontend_Action, \IWPML_DIC_Action {
+
+	/**
+	 * @var \woocommerce_wpml
+	 */
+	private $woocommerce_wpml;
+
+	public function __construct( \woocommerce_wpml $woocommerce_wpml ) {
+		$this->woocommerce_wpml = $woocommerce_wpml;
+	}
+
+	public function add_hooks() {
+		Hooks::onAction( 'woocommerce_blocks_payment_method_type_registration', PHP_INT_MAX )
+			->then( spreadArgs( [ $this, 'translateSettings' ] ) );
+	}
+
+	/**
+	 * @param PaymentMethodRegistry $registry
+	 */
+	public function translateSettings( $registry ) {
+		foreach ( $registry->get_all_registered() as $gatewayId => $gateway ) {
+			Hooks::onFilter( 'option_woocommerce_' . $gatewayId . '_settings' )
+				->then( spreadArgs( function( $settings ) use ( $gatewayId ) {
+					foreach ( [ 'title', 'description' ] as $name ) {
+						$settings[ $name ] = $this->woocommerce_wpml->gateways->get_translated_gateway_string( $settings[ $name ], $gatewayId, $name );
+					}
+
+					return $settings;
+				} ) );
+		}
+	}
+
+}
--- wpml-woocommerce.php
+++ wpml-woocommerce.php
@@ -104,6 +104,7 @@ function wcml_loader() {
 		\WCML\AdminNotices\Review::class,
 		\WCML\Multicurrency\UI\Factory::class,
 		\WCML\PaymentGateways\Hooks::class,
+		\WCML\PaymentGateways\BlockHooks::class,
 		\WCML\CLI\Hooks::class,
 		\WCML\Reports\Hooks::class,
 		\WCML\Reports\Products\Query::class,
