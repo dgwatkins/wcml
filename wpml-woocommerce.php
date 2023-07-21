94076f04c9bdeba6af0f013fdac5138e006fe6f2
David Garcia Watkins <david.gw@onthegosystems.com>
2023-07-21 17:09:34 +0000

    Defer until WCML_WC_Gateways is initialized
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-4411/Call-to-a-member-function-gettranslatedgatewaystring-on-null-in-BlockHooks.php35

--- wpml-woocommerce.php
+++ wpml-woocommerce.php
@@ -104,7 +104,7 @@ function wcml_loader() {
 		\WCML\AdminNotices\Review::class,
 		\WCML\Multicurrency\UI\Factory::class,
 		\WCML\PaymentGateways\Hooks::class,
-		\WCML\PaymentGateways\BlockHooks::class,
+		\WCML\PaymentGateways\BlockHooksFactory::class,
 		\WCML\CLI\Hooks::class,
 		\WCML\Reports\Hooks::class,
 		\WCML\Reports\Products\Query::class,
